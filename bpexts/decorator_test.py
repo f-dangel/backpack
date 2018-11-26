423d673bc2b62e8cdfd0228402eec5b1bdcf58ed
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2018-11-26 12:17:45 +0100

    Add test for naming/docstring modification of decorator

---
+++ bpexts/decorator_test.py
@@ -0,0 +1,11 @@
+"""Test decorator of torch.nn.Module subclasses."""
+
+import torch.nn
+from decorator import decorate
+
+
+def test_decorated_linear_properties():
+    """Test name and docstring of decorated torch.nn.Linear."""
+    DecoratedAffine = decorate(torch.nn.Linear)
+    assert DecoratedAffine.__name__ == 'DecoratedLinear'
+    assert DecoratedAffine.__doc__.startswith('[Decorated by bpexts]')
