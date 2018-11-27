390bec2ad69bbcb5f61d30d918641621a2c57638
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2018-11-27 18:07:21 +0100

    Add tests for weight batch gradient computation of linear

--- bpexts/gradient/linear_test.py
+++ bpexts/gradient/linear_test.py
@@ -29,11 +29,14 @@ def loss_function(tensor):
 
 
 # input (1 sample)
-in1 = Tensor([1, 1, 1]).float()
-out1 = Tensor([6 + 7, 15 + 8]).float()
+in1 = Tensor([[1, 1, 1]]).float()
+out1 = Tensor([[6 + 7, 15 + 8]]).float()
 loss1 = 13**2 + 23**2
 bias_grad1 = Tensor([2 * 13, 2 * 23]).float()
 bias_grad_batch1 = bias_grad1
+weight_grad1 = Tensor([[26, 26, 26],
+                       [46, 46, 46]]).float()
+weight_grad_batch1 = weight_grad1
 
 # input (2 samples)
 in2 = Tensor([[1, 0, 1],
@@ -44,14 +47,21 @@ loss2 = 11**2 + 18**2 + 9**2 + 13**2
 bias_grad2 = Tensor([2 * (11 + 9), 2 * (18 + 13)])
 bias_grad_batch2 = Tensor([[2 * 11, 2 * 18],
                            [2 * 9, 2 * 13]]).float()
+weight_grad2 = Tensor([[22, 18, 22],
+                       [36, 26, 36]]).float()
+weight_grad_batch2 = Tensor([[[22, 0, 22],
+                              [36, 0, 36]],
+                             [[0, 18, 0],
+                              [0, 26, 0]]]).float()
 
-
-# inputs, results, losses, bias_grads and bias_batch_grads
+# as lists for zipping
 inputs = [in1, in2]
 results = [out1, out2]
 losses = [loss1, loss2]
 bias_grads = [bias_grad1, bias_grad2]
 bias_grads_batch = [bias_grad_batch1, bias_grad_batch2]
+weight_grads = [weight_grad1, weight_grad2]
+weight_grads_batch = [weight_grad_batch1, weight_grad_batch2]
 
 
 def test_forward():
@@ -72,21 +82,31 @@ def test_losses():
         assert loss_val.item() == loss
 
 
-def test_bias_grad():
-    """Test computation of bias gradients."""
-    for input, bias_grad in zip(inputs, bias_grads):
+def test_grad():
+    """Test computation of bias/weight gradients."""
+    for input, b_grad, w_grad in zip(inputs,
+                                     bias_grads,
+                                     weight_grads):
         out = g_lin(input)
         loss = loss_function(out)
         loss.backward()
-        assert torch_allclose(g_lin.bias.grad, bias_grad)
+        assert torch_allclose(g_lin.bias.grad, b_grad)
+        assert torch_allclose(g_lin.weight.grad, w_grad)
         g_lin.zero_grad()
+        g_lin.remove_exts_buffers()
 
 
-def test_bias_grad_batch():
-    """Test computation of bias batch gradients."""
-    for input, bias_grad_batch in zip(inputs, bias_grads_batch):
+def test_grad_batch():
+    """Test computation of bias/weight batch gradients."""
+    for input, b_grad_batch, w_grad_batch in zip(inputs,
+                                                 bias_grads_batch,
+                                                 weight_grads_batch):
         out = g_lin(input)
         loss = loss_function(out)
         loss.backward()
-        assert torch_allclose(g_lin.bias.grad_batch, bias_grad_batch)
+        assert torch_allclose(g_lin.bias.grad_batch, b_grad_batch)
+        print(g_lin.weight.grad_batch)
+        print(w_grad_batch)
+        assert torch_allclose(g_lin.weight.grad_batch, w_grad_batch)
         g_lin.zero_grad()
+        g_lin.remove_exts_buffers()
