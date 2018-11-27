c1fbed2a718a8154cf72cae001a53eca3b1a459d
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2018-11-27 09:25:24 +0100

    Add tests for hook removing

--- bpexts/decorator_test.py
+++ bpexts/decorator_test.py
@@ -15,22 +15,28 @@ def test_decorated_linear_properties():
     assert DecoratedAffine.__doc__.startswith('[Decorated by bpexts]')
 
 
+def dummy_backward_hook(module, grad_input, grad_output):
+    """Dummy backward hook."""
+    pass
+
+
+def dummy_forward_hook(module, input, output):
+    """Dummy forward hook."""
+    pass
+
+
+def dummy_pre_forward_hook(module, input):
+    """Dummy pre forward hook."""
+    pass
+
+
 def test_exts_hooking():
-    """Test tracking of hooks registrated with exts functions."""
+    """Test tracking of hooks registrated with exts functions.
+
+    Return module with hooks (6 in total, 3 registered by torch.nn,
+    3 registered by exts.
+    """
     layer = DecoratedAffine(in_features=5, out_features=5)
-
-    # dummy hooks
-    def dummy_backward_hook(module, grad_input, grad_output):
-        """Dummy backward hook."""
-        pass
-
-    def dummy_forward_hook(module, input, output):
-        """Dummy forward hook."""
-        pass
-
-    def dummy_pre_forward_hook(module, input):
-        """Dummy pre forward hook."""
-        pass
     # exts and nn hook registration functions
     hooks = [dummy_backward_hook,
              dummy_forward_hook,
@@ -50,6 +56,36 @@ def test_exts_hooking():
         # usual hook registration should not affect extension hooks
         nn_func(hook)
         assert len(layer.exts_hooks) == i
+    # each hook type occurs twice (once from nn, once from exts)
+    for hook_type in [layer._backward_hooks,
+                      layer._forward_hooks,
+                      layer._forward_pre_hooks]:
+        assert len(hook_type) == 2
+    return layer
+
+
+def module_with_hooks():
+    """Return module with 6 hooks.
+
+    Module contains a backward, a forward and a pre forward hook.
+    The hooks are registered using the torch.nn method and the
+    exts method, resultin in 2*3 = 6 hooks.
+    """
+    return test_exts_hooking()
+
+
+def test_hook_removing():
+    """Test disabling of hooks tracked by exts."""
+    # 6 hooks, 3 tracked by exts, 3 tracked by torch.nn
+    layer = module_with_hooks()
+    # remove tracked hooks
+    layer.remove_exts_hooks()
+    assert len(layer.exts_hooks) == 0
+    # module has one forward, pre forward and backward hook each.
+    for hook_type in [layer._backward_hooks,
+                      layer._forward_hooks,
+                      layer._forward_pre_hooks]:
+        assert len(hook_type) == 1
 
 
 def module_with_buffers():
