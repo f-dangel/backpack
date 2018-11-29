f55132545f9eb680f8432ee18a74003bd3b3586f
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2018-11-29 09:56:23 +0100

    Use super().forward in conv2d, remove random forward tests

--- bpexts/gradient/conv2d.py
+++ bpexts/gradient/conv2d.py
@@ -33,39 +33,6 @@ class G_Conv2d(DecoratedConv2d):
             raise ValueError('Expecting 4D input (batch, channel, x, y)')
         module.register_exts_buffer('input', input[0].clone().detach())
 
-    def forward(self, input):
-        """Apply 2d convolution using unfold.
-
-        More info on how to do this manually:
-            https://discuss.pytorch.org/t/custom-convolution-dot-
-            product/14992/7
-        """
-        out_size = self.output_size(input.size()[1:])
-        out_shape = (-1, self.out_channels) + out_size
-        # expand patches
-        im2col = self.unfold(input)
-        # perform convolution by matrix multiplication
-        kernel_matrix = self.weight.view(self.out_channels, -1)
-        # j: output image size, k: output channels
-        convoluted = einsum('ki,bij->bkj', (kernel_matrix, im2col))
-        # reshape into output image
-        col2im = convoluted.view(out_shape)
-        if self.bias is not None:
-            bias = self.bias.view(1, -1, 1, 1)
-            col2im.add_(bias.expand_as(col2im))
-        return col2im
-
-    def output_size(self, input_size):
-        """Compute size of the output channels from random input.
-
-        Taken from
-            https://discuss.pytorch.org/t/convolution-and-pooling-
-            layers-need-a-method-to-calculate-output-size/21895/2
-        """
-        x = randn(input_size).unsqueeze(0)
-        output = super().forward(x)
-        return output.size()[2:]
-
     @staticmethod
     def compute_grad_batch(module, grad_input, grad_output):
         """Backward hook for computing batch gradients.
