282799fc83a5d65a7d4cf243f252ea141dd4dcb1
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2018-11-29 15:17:01 +0100

    Add test of exact full Hessian computation with involved function

--- bpexts/hessian/test_exact.py
+++ bpexts/hessian/test_exact.py
@@ -21,6 +21,25 @@ def simple_function_with_parameters():
     return f, parameters
 
 
+def involved_function_with_parameters():
+    """Return complicated example function with parameters."""
+    # parameters
+    x = tensor([[1, -2],
+                [3, -4]], requires_grad=True).float()
+    y = tensor([5, 6], requires_grad=True).float()
+    parameters = [x, y]
+    # input (no parameter)
+    input = tensor([1, 2]).float()
+    # composition of functions
+    temp = x.matmul(input) + y
+    temp_result = tensor([2, 1]).float()
+    assert torch_allclose(temp, temp_result)
+    f = (temp**2).sum()
+    f_result = tensor([5])
+    assert torch_allclose(f, f_result)
+    return f, parameters
+
+
 def test_exact_hessian_simple():
     """Test exact Hessian computation for a simple function."""
     f, parameters = simple_function_with_parameters()
@@ -30,4 +49,21 @@ def test_exact_hessian_simple():
                      [5, 6, 3]]).float()
     # compute and compare results
     hessian = exact_hessian(f, parameters, show_progress=True)
+    assert torch_allclose(hessian, result)
+
+
+def test_exact_hessian_involved():
+    """Test exact Hessian computation for a complicated function."""
+    f, parameters = involved_function_with_parameters()
+    # expected outcome
+    result = tensor([[2, 4, 0, 0, 2, 0],
+                     [4, 8, 0, 0, 4, 0],
+                     [0, 0, 2, 4, 0, 2],
+                     [0, 0, 4, 8, 0, 4],
+                     [2, 4, 0, 0, 2, 0],
+                     [0, 0, 2, 4, 0, 2]]).float()
+    # compute and compare results
+    print(result)
+    hessian = exact_hessian(f, parameters, show_progress=True)
+    print(hessian)
     assert torch_allclose(hessian, result)
