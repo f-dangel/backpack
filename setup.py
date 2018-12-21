1519f1a680bc2fba478daf13c1ab2a1f136e9aea
Frederik Kunstner <frederik.kunstner@gmail.com>
2018-12-21 08:57:56 +0100

    Make backprop-extended pip-installable

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='backprop-extended',
+	version='0.1',
+	description='',
+	url='https://gitlab.tuebingen.mpg.de/fdangel/backprop-extended',
+	author='',
+	author_email='',
+	license='MIT',
+	packages=['bpexts'],
+	zip_safe=False
+)
