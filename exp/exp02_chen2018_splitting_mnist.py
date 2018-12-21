6d955e86a43fcdd828f5cb10a1d84bcbcb297900
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2018-12-21 14:26:02 +0100

    Add experiment with block splitting on Chen MNIST model

---
+++ exp/exp02_chen2018_splitting_mnist.py
@@ -0,0 +1,102 @@
+"""
+Experiments performed in Chen et al.: BDA-PCH, figure 1,
+with different parameter splitting.
+
+Link to the reference:
+    https://arxiv.org/pdf/1802.06502v2.pdf
+"""
+
+import torch
+from torch.nn import CrossEntropyLoss
+from os import path
+from models_chen2018 import original_mnist_model, separated_mnist_model
+from load_mnist import MNISTLoader
+from training import SecondOrderTraining
+from utils import (directory_in_data,
+                   dirname_from_params,
+                   tensorboard_instruction,
+                   run_directory_exists)
+import enable_import_bpexts
+from bpexts.optim.cg_newton import CGNewton
+from bpexts.hbp.parallel.parallel_sequential\
+        import HBPParallelIdenticalSequential
+
+
+def mnist_cgnewton(modify_2nd_order_terms, num_blocks):
+    """
+    # --------------
+    # MNIST CGNewton
+    # --------------
+
+    Run will be skipped if logging directory already exists.
+
+    Parameters:
+    -----------
+    modify_2nd_order_terms : (str)
+        Strategy for treating 2nd-order effects of module functions:
+        * `'zero'`: Yields the Generalizes Gauss Newton matrix
+        * `'abs'`: BDA-PCH approximation
+        * `'clip'`: Different BDA-PCH approximation
+    """
+    device = torch.device('cuda:0' if torch.cuda.is_available()
+                          else 'cpu')
+
+    # hyper parameters
+    # ----------------
+    batch = 500
+    epochs = 30
+    lr = 0.1
+    alpha = 0.02
+    cg_maxiter = 50
+    cg_tol = 0.1
+    cg_atol = 0
+
+    # logging directory
+    # -----------------
+    directory_name = 'exp02_chen_splitting/mnist'
+    data_dir = directory_in_data(directory_name)
+    print(tensorboard_instruction(data_dir))
+    # directory of run
+    run_name = dirname_from_params(opt='cgn',
+                                   batch=batch,
+                                   lr=lr,
+                                   alpha=alpha,
+                                   maxiter=cg_maxiter,
+                                   tol=cg_tol,
+                                   atol=cg_atol,
+                                   mod2nd=modify_2nd_order_terms,
+                                   blocks=num_blocks)
+    logdir = path.join(data_dir, run_name)
+
+    # training procedure
+    # ------------------
+    if not run_directory_exists(logdir):
+        # set up training and run
+        model = original_mnist_model()
+        # split into parallel modules
+        model = HBPParallelIdenticalSequential.from_sequential(model)
+        model = model.split_into_blocks(num_blocks)
+        loss_function = CrossEntropyLoss()
+        data_loader = MNISTLoader(train_batch_size=batch)
+        optimizer = CGNewton(model.parameters(),
+                             lr=lr,
+                             alpha=alpha,
+                             cg_atol=cg_atol,
+                             cg_tol=cg_tol,
+                             cg_maxiter=cg_maxiter)
+        # initialize training
+        train = SecondOrderTraining(model,
+                                    loss_function,
+                                    optimizer,
+                                    data_loader,
+                                    logdir)
+        train.run(num_epochs=epochs,
+                  modify_2nd_order_terms=modify_2nd_order_terms,
+                  device=device)
+
+
+if __name__ == '__main__':
+    num_blocks = [1, 2, 3, 4, 5, 256, 512]
+
+    for num in num_blocks:
+        mnist_cgnewton('abs', num)
