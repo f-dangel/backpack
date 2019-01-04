b9932077e3b1dc1be311950259c7979bd0f3cfe3
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-01-04 18:24:13 +0100

    Dirty strategy/implementation for handling Combined in parallel mode

--- bpexts/hbp/parallel/combined.py
+++ bpexts/hbp/parallel/combined.py
@@ -28,10 +28,10 @@ class HBPParallelCompositionActivationLinear(HBPParallel):
         self.out_features_list = [c.linear.out_features
                                   for c in self.children()]
 
-        # disable hooks in linear layers except for first layer
+        # disable hooks in layers except for first layer
         for i, mod in enumerate(self.children()):
             if i != 0:
-                mod.linear.disable_exts()
+                mod.disable_exts()
 
         # try to copy already existing duplicate buffers from HBP
         try:
@@ -40,10 +40,43 @@ class HBPParallelCompositionActivationLinear(HBPParallel):
                     'mean_input', mean_input)
             self._reference_mean_input_in_children()
         except AttributeError as e:
-            warn('Could not copy/find buffer mean_input.\n{}'
-                 .format(e))
+            warn('Could not copy/find buffer mean_input.\n{}'.format(e))
 
-        # buffers from activation will be kept (no redundancy)
+        # Try to save memory
+        #####################
+        has_bias = set(mod.linear.bias is not None
+                       for mod in self.children())
+        if not len(has_bias) == 1:
+            raise ValueError('Expect simultaneous presence/absence'
+                             ' of bias, got {}'.format(has_bias))
+        has_bias = has_bias.pop()
+
+        # concatenate weight matrix
+        weight = cat([mod.linear.weight.data
+                      for mod in self.children()])
+        self.register_buffer('weight', weight)
+
+        # concatenate bias
+        bias = None if not has_bias else\
+            cat([mod.linear.bias.data for mod in self.children()])
+        self.register_buffer('bias', bias)
+
+        # assign slices to children parameters
+        idx = [0] + list(cumsum(self.out_features_list))
+        idx = [(idx[i], idx[i + 1]) for i in range(len(idx) - 1)]
+        for (i, j), child in zip(idx, self.children()):
+            child.linear.weight.data = self.weight.data[i:j, :]
+            if has_bias:
+                child.linear.bias.data = self.bias.data[i:j]
+
+    # override
+    def forward(self, input):
+        """Only use first activation layer, split activation output and
+        apply each linear module separately."""
+        activation_out = self.get_submodule(0).activation(input)
+        split_output = [layer.linear(activation_out)
+                        for layer in self.children()]
+        return self.concatenate_output(split_output)
 
     # override
     def hbp_hooks(self):
@@ -100,17 +133,24 @@ class HBPParallelCompositionActivationLinear(HBPParallel):
         layer = self.contained_class(in_features=in_features,
                                      out_features=out_features,
                                      bias=has_bias)
+        layer.linear.weight.data = self.weight
+        # concatenate bias and assign
+        if has_bias:
+            layer.linear.bias.data = self.bias.data
 
-        # unite linear layers and replace
-        united_linear = HBPParallelLinear(
-                *[c.linear for c in self.children()]
-                ).unite().get_submodule(0)
-        layer.linear = united_linear
+        # try to copy mean_input from linear
+        try:
+            mean_input = self.get_submodule(0).linear.mean_input
+            layer.linear.register_exts_buffer('mean_input',
+                                              mean_input)
+        except AttributeError:
+            warn('Could not copy/find buffer mean_input')
 
+        # copy buffers from activation
+        ##############################
         # buffer grad_output
         try:
-            grad_output = cat([c.activation.grad_output
-                               for c in self.children()])
+            grad_output = self.get_submodule(0).activation.grad_output
             layer.activation.register_exts_buffer('grad_output',
                                                   grad_output)
         except AttributeError:
@@ -118,8 +158,7 @@ class HBPParallelCompositionActivationLinear(HBPParallel):
 
         # buffer grad_phi
         try:
-            grad_phi = cat([c.activation.grad_phi
-                            for c in self.children()])
+            grad_phi = self.get_submodule(0).activation.grad_phi
             layer.activation.register_exts_buffer('grad_phi',
                                                   grad_phi)
         except AttributeError:
@@ -127,20 +166,13 @@ class HBPParallelCompositionActivationLinear(HBPParallel):
 
         # buffer gradgrad_phi
         try:
-            gradgrad_phi = cat([c.activation.gradgrad_phi
-                                for c in self.children()])
+            gradgrad_phi = self.get_submodule(0).activation.gradgrad_phi
             layer.activation.register_exts_buffer('gradgrad_phi',
                                                   gradgrad_phi)
         except AttributeError:
             warn('Could not copy/find buffer gradgrad_phi')
 
-        # HBPParallelLinear version with single child
-        parallel = self.__class__(layer)
-
-        # out_features_list
-        parallel.out_features_list = [out_features]
-
-        return parallel
+        return self.__class__(layer)
 
     def split(self, out_features_list):
         """Split into parallel series of HBPCompositionActivationLinear.
@@ -172,50 +204,49 @@ class HBPParallelCompositionActivationLinear(HBPParallel):
 
         idx = [0] + list(cumsum(out_features_list))
         idx = [(idx[i], idx[i + 1]) for i in range(len(idx) - 1)]
-        # parallel layers split
-        parallel_linear = HBPParallelLinear(combined.linear).split(
-                out_features_list)
-        for out, (i, j), mod in zip(out_features_list, idx,
-                                    parallel_linear.children()):
+        for out, (i, j) in zip(out_features_list, idx):
             # create HBPCompositionActivationLinear
             child = self.contained_class(in_features=in_features,
                                          out_features=out,
                                          bias=has_bias)
-            # replace linear layer
-            child.linear = mod
-
-            # split and register buffers grad_output, grad_phi, gradgrad_phi
-            # buffer grad_output
-            activation = combined.activation
-            try:
-                grad_output = activation.grad_output[:, i:j]
-                child.activation.register_exts_buffer('grad_output',
-                                                      grad_output)
-            except AttributeError:
-                warn('Could not copy/find buffer grad_output')
-
-            # buffer grad_phi
-            try:
-                grad_phi = activation.grad_phi
-                child.activation.register_exts_buffer('grad_phi',
-                                                      grad_phi)
-            except AttributeError:
-                warn('Could not copy/find buffer grad_phi')
-
-            # buffer gradgrad_phi
-            try:
-                gradgrad_phi = activation.gradgrad_phi
-                child.activation.register_exts_buffer('gradgrad_phi',
-                                                      gradgrad_phi)
-            except AttributeError:
-                warn('Could not copy/find buffer gradgrad_phi')
-
+            child.linear.weight.data = united.weight.data[i:j, :]
+            if has_bias:
+                child.linear.bias.data = united.bias.data[i:j]
             layers.append(child)
 
-        # HBPParallelCompositionActivationLinear version with single child
-        parallel = self.__class__(*layers)
 
-        # out_features_list
-        parallel.out_features_list = out_features_list
+        # register buffers grad_output, grad_phi, gradgrad_phi
+        # buffer grad_output
+        activation = combined.activation
+        try:
+            grad_output = activation.grad_output
+            layers[0].activation.register_exts_buffer('grad_output',
+                                                      grad_output)
+        except AttributeError:
+            warn('Could not copy/find buffer grad_output')
 
-        return parallel
+        # buffer grad_phi
+        try:
+            grad_phi = activation.grad_phi
+            layers[0].activation.register_exts_buffer('grad_phi',
+                                                      grad_phi)
+        except AttributeError:
+            warn('Could not copy/find buffer grad_phi')
+
+        # buffer gradgrad_phi
+        try:
+            gradgrad_phi = activation.gradgrad_phi
+            layers[0].activation.register_exts_buffer('gradgrad_phi',
+                                                      gradgrad_phi)
+        except AttributeError:
+            warn('Could not copy/find buffer gradgrad_phi')
+
+        # try to copy mean_input from linear
+        try:
+            mean_input = self.get_submodule(0).linear.mean_input
+            layers[0].linear.register_exts_buffer('mean_input',
+                                                  mean_input)
+        except AttributeError:
+            warn('Could not copy/find buffer mean_input')
+
+        return self.__class__(*layers)
--- bpexts/hbp/parallel/combined_test.py
+++ bpexts/hbp/parallel/combined_test.py
@@ -1,6 +1,6 @@
 """Test conversion/splitting HBPCompositionActivation to parallel."""
 
-from torch import randn, Tensor
+from torch import randn, Tensor, eye
 from ..combined_relu_test import test_input_hessian\
         as hbp_relulinear_after_hessian_backward
 from ..combined_sigmoid_test import test_input_hessian\
@@ -9,7 +9,8 @@ from ..combined_relu import HBPReLULinear
 from ..combined_sigmoid import HBPSigmoidLinear
 from .combined import HBPParallelCompositionActivationLinear
 from ...utils import (torch_allclose,
-                      set_seeds)
+                      set_seeds,
+                      memory_report)
 
 
 in_features = 20
@@ -17,11 +18,6 @@ out_features_list = [2, 3, 4]
 out_features_list2 = [1, 8]
 num_layers = len(out_features_list)
 input = randn(1, in_features)
-
-
-def random_input():
-    """Return random input copy."""
-    return input.clone()
 
 
 def test_forward_pass_hbp_relulinear():
@@ -82,18 +78,28 @@ def test_buffer_split_hbp_relulinear_unite():
     assert isinstance(mod.activation.grad_output, Tensor)
 
     parallel2 = parallel.split_into_blocks(2)
-    for mod in parallel2.children():
+    for i, mod in enumerate(parallel2.children()):
         assert isinstance(mod.linear.mean_input, Tensor)
-        assert isinstance(mod.activation.grad_phi, Tensor)
-        assert isinstance(mod.activation.gradgrad_phi, Tensor)
-        assert isinstance(mod.activation.grad_output, Tensor)
-
+        if i == 0:
+            assert hasattr(mod.activation, 'grad_phi')
+            assert hasattr(mod.activation, 'gradgrad_phi')
+            assert hasattr(mod.activation, 'grad_output')
+        else:
+            assert not hasattr(mod.activation, 'grad_phi')
+            assert not hasattr(mod.activation, 'gradgrad_phi')
+            assert not hasattr(mod.activation, 'grad_output')
+ 
     parallel3 = parallel.unite()
-    for mod in parallel3.children():
+    for i, mod in enumerate(parallel3.children()):
         assert isinstance(mod.linear.mean_input, Tensor)
-        assert isinstance(mod.activation.grad_phi, Tensor)
-        assert isinstance(mod.activation.gradgrad_phi, Tensor)
-        assert isinstance(mod.activation.grad_output, Tensor)
+        if i == 0:
+            assert hasattr(mod.activation, 'grad_phi')
+            assert hasattr(mod.activation, 'gradgrad_phi')
+            assert hasattr(mod.activation, 'grad_output')
+        else:
+            assert not hasattr(mod.activation, 'grad_phi')
+            assert not hasattr(mod.activation, 'gradgrad_phi')
+            assert not hasattr(mod.activation, 'grad_output')
 
 
 def test_buffer_split_hbp_sigmoidlinear_unite():
@@ -108,15 +114,89 @@ def test_buffer_split_hbp_sigmoidlinear_unite():
     assert isinstance(mod.activation.grad_output, Tensor)
 
     parallel2 = parallel.split_into_blocks(2)
-    for mod in parallel2.children():
+    for i, mod in enumerate(parallel2.children()):
         assert isinstance(mod.linear.mean_input, Tensor)
-        assert isinstance(mod.activation.grad_phi, Tensor)
-        assert isinstance(mod.activation.gradgrad_phi, Tensor)
-        assert isinstance(mod.activation.grad_output, Tensor)
-
+        if i == 0:
+            assert hasattr(mod.activation, 'grad_phi')
+            assert hasattr(mod.activation, 'gradgrad_phi')
+            assert hasattr(mod.activation, 'grad_output')
+        else:
+            assert not hasattr(mod.activation, 'grad_phi')
+            assert not hasattr(mod.activation, 'gradgrad_phi')
+            assert not hasattr(mod.activation, 'grad_output')
+ 
     parallel3 = parallel.unite()
-    for mod in parallel3.children():
+    for i, mod in enumerate(parallel3.children()):
         assert isinstance(mod.linear.mean_input, Tensor)
-        assert isinstance(mod.activation.grad_phi, Tensor)
-        assert isinstance(mod.activation.gradgrad_phi, Tensor)
-        assert isinstance(mod.activation.grad_output, Tensor)
+        if i == 0:
+            assert hasattr(mod.activation, 'grad_phi')
+            assert hasattr(mod.activation, 'gradgrad_phi')
+            assert hasattr(mod.activation, 'grad_output')
+        else:
+            assert not hasattr(mod.activation, 'grad_phi')
+            assert not hasattr(mod.activation, 'gradgrad_phi')
+            assert not hasattr(mod.activation, 'grad_output')
+
+
+def test_memory_consumption():
+    """Check memory consumption during splitting."""
+    layer = hbp_sigmoidlinear_after_hessian_backward()
+    # delete Hessian quantities
+    del layer.linear.bias.hessian
+    del layer.linear.weight.hessian
+    del layer.linear.weight.hvp
+    print('Before conversion')
+    mem_stat1 = memory_report()
+
+    layer = HBPParallelCompositionActivationLinear(layer)
+    print('After conversion')
+    mem_stat2 = memory_report()
+
+    layer = layer.split_into_blocks(4)
+    print('After splitting')
+    mem_stat3 = memory_report()
+    assert mem_stat1 == mem_stat2 == mem_stat3
+
+
+def random_input():
+    """Return random input copy."""
+    new_input = input.clone()
+    new_input.requires_grad = True
+    return new_input
+
+
+def example_loss(tensor):
+    """Sum all square entries of a tensor."""
+    return (tensor**2).view(-1).sum(0)
+
+
+def forward(layer, input):
+    """Feed input through layers and loss. Return output and loss."""
+    out = layer(input)
+    return out, example_loss(out)
+
+
+def test_memory_consumption_during_hbp():
+    """Check for constant memory consumption during HBP."""
+    sigma_linear = HBPSigmoidLinear(in_features=in_features,
+                                    out_features=sum(out_features_list),
+                                    bias=True)
+    sigma_linear = HBPParallelCompositionActivationLinear(
+            sigma_linear).split_into_blocks(4)
+
+    mem_stat = None
+    mem_stat_after = None
+    
+    for i in range(10):
+        input = random_input()
+        out, loss = forward(sigma_linear, input)
+        loss_hessian = 2 * eye(out.numel())
+        loss.backward()
+        out_h = loss_hessian
+        sigma_linear.backward_hessian(out_h)
+        if i == 0:
+            mem_stat = memory_report()
+        if i == 9:
+            mem_stat_after = memory_report()
+
+    assert mem_stat == mem_stat_after
