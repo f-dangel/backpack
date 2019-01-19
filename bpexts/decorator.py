9ada86d4e5ac89301bc2cdda16928c8f7c389e90
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-01-19 14:28:35 +0100

    Improve docstrings of decorator.py

--- bpexts/decorator.py
+++ bpexts/decorator.py
@@ -8,17 +8,45 @@ from torch.nn import Module
 
 
 def decorate(module_subclass):
-    """Add functionality to torch.nn.Module subclass.
+    """Extend functionality of a module.
+
+    The functionality allows to install
+
+    - hooks whose book-keeping decoupled from the normal hooks and
+      allows them to be removed if desired.
+
+    - buffers whose book-keeping is decoupled from the normal buffers
 
     Implemented in this way, each subclass of nn.Module is decorated
     separately, thereby avoiding double-inheritance from torch.nn.Module
     in a diamond-like pattern.
+
+    Parameters
+    ----------
+    module_subclass : torch.nn.Module subclass
+        A subclass of torch.nn.Module that will be extended with the
+        aforementioned functionality
+
+    Returns
+    -------
+    class
+        Class which can be used exactly the same as the original class
+        but has additional methods to implement extensions of
+        backpropagation
     """
     if not issubclass(module_subclass, Module):
         raise ValueError('Can onĺy wrap subclasses of torch.nn.Module')
 
     class DecoratedModule(module_subclass):
-        """Module decorated for backpropagation extension."""
+        """Module decorated for backpropagation extension.
+        
+        Attributes
+        ----------
+        exts_hooks : :obj:`list`
+            Stores handles of hooks that allow for easy removal
+        exts_buffers : :obj:`set`
+            Store buffers of backprop extension
+        """
         __doc__ = '[Decorated by bpexts] {}'.format(module_subclass.__doc__)
 
         def __init__(self, *args, **kwargs):
@@ -51,7 +79,17 @@ def decorate(module_subclass):
 
         # --- buffer tracking ---
         def register_exts_buffer(self, name, tensor=None):
-            """Register tracked buffer for extended backpropagation."""
+            """Register tracked buffer for extended backpropagation.
+
+            If no tensor is specified, `None` is used as a placeholder.
+            
+            Parameters
+            ----------
+            name : :obj:`str`
+                Name of the buffer
+            tensor : torch.Tensor, optional
+                Tensor to store as buffer
+            """
             self.register_buffer(name, tensor)
             self._track_exts_buffer(name)
 
@@ -61,7 +99,13 @@ def decorate(module_subclass):
 
         # --- disable exts ---
         def disable_exts(self, keep_buffers=False):
-            """Disable exts behavior, make module behave like torch.nn."""
+            """Disable exts behavior, make module behave like torch.nn.
+            
+            Parameters
+            ----------
+            keep_buffers: :obj:`bool`
+                Also remove buffers stored by backpropagation extension
+            """
             self.remove_exts_hooks()
             if not keep_buffers:
                 self.remove_exts_buffers()
@@ -69,13 +113,13 @@ def decorate(module_subclass):
                 module.disable_exts(keep_buffers=keep_buffers)
 
         def remove_exts_hooks(self):
-            """Remove all hooks tracked by exts."""
+            """Remove all hooks tracked by bpexts."""
             while self.exts_hooks:
                 handle = self.exts_hooks.pop()
                 handle.remove()
 
         def remove_exts_buffers(self):
-            """Remove all buffers introduced by exts."""
+            """Remove all buffers introduced by bpexts."""
             while self.exts_buffers:
                 name = self.exts_buffers.pop()
                 self._buffers.pop(name)
