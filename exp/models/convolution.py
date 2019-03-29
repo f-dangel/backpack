dab848d2ef9fce626f7d27c3ad888990d7059e6a
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-03-29 16:06:39 +0100

    Add c1d1 support for arbitrary dataset inputs

--- exp/models/convolution.py
+++ exp/models/convolution.py
@@ -1,4 +1,4 @@
-"""Convolutional neural networks for testing/experiments."""
+"""CNNs for testing/experiments."""
 
 import torch.nn as nn
 from numpy import prod
@@ -7,23 +7,20 @@ from bpexts.hbp.view import HBPViewBatchFlat
 from bpexts.hbp.conv2d import HBPConv2d
 from bpexts.hbp.combined_sigmoid import HBPSigmoidLinear
 from bpexts.hbp.sequential import HBPSequential
-from bpexts.utils import set_seeds
 
 
 def c1d1(
         # dimension of input and output of the net
         input_size=(1, 28, 28),
         num_outputs=10,
-        # random initialization
-        seed=None,
         # convolution parameters
         conv_channels=8,
         kernel_size=4,
         padding=0,
         stride=1):
-    """A very simple CNN for image classification without pooling.
+    """Simple CNN with one convolution and one linear layer.
 
-    The architecture uses the following structure:
+    The architecture is as follows:
 
       - Convolution (aka c1)
       - Sigmoid activation
@@ -32,24 +29,15 @@ def c1d1(
     Parameters:
     -----------
     input_size : tuple(int)
-        Shape of the input
+        Shape of the input without batch dimension
     num_outputs : int
         Number of outputs (classes)
-    seed : int
-        Random seed used for initialization process of layers,
-        No seed will be set if left `None`
     conv_channels : int
         Number of feature channels created by the convolution layer
 
     The remaining arguments define the properties of the convolution layer.
-
-    Returns:
-    --------
-    HBPSequential:
-        `PyTorch` module behaving like `nn.Sequential` with HBP
-        functionality
     """
-    # determine output features from convolution layer on inputs
+    # determine output features from convolution
     output_size = HBPConv2d.output_shape(
         input_size=(1, ) + input_size,
         out_channels=conv_channels,
@@ -57,10 +45,9 @@ def c1d1(
         stride=stride)
     output_numel = prod(output_size)
     # create the model
-    set_seeds(seed)
     model = HBPSequential(
         HBPConv2d(
-            in_channels=1,
+            in_channels=input_size[0],
             out_channels=conv_channels,
             kernel_size=kernel_size,
             padding=padding,
--- exp/models/convolution_test.py
+++ exp/models/convolution_test.py
@@ -4,6 +4,8 @@ from torch import rand
 from torch.nn import (Module, Sigmoid, Linear, Conv2d)
 from .convolution import c1d1
 from bpexts.utils import torch_allclose, set_seeds
+from bpexts.hbp.conv2d import HBPConv2d
+from numpy import prod
 
 
 def test_c1d1_output_size():
@@ -22,45 +24,67 @@ def test_c1d1_output_size():
     assert tuple(out.size()) == (3, 8)
 
 
-def torch_c1d1(seed=None):
+def torch_c1d1(
+        # dimension of input and output of the net
+        input_size=(1, 28, 28),
+        num_outputs=10,
+        # convolution parameters
+        conv_channels=8,
+        kernel_size=4,
+        padding=0,
+        stride=1):
     """Torch implementation of the simple CNN for fixed hyperparameters."""
 
-    class SimplestCNN(Module):
-        """PyTorch implementation of the simple CNN for MNIST."""
+    class c1d1Torch(Module):
+        """PyTorch implementation of c1d1."""
 
         def __init__(self):
             super().__init__()
+            # determine output size of convolution layer
+            output_size = HBPConv2d.output_shape(
+                input_size=(1, ) + input_size,
+                out_channels=conv_channels,
+                kernel_size=kernel_size,
+                stride=stride)
+            output_numel = prod(output_size)
+            # create layers
             self.conv = Conv2d(
-                in_channels=1, out_channels=8, kernel_size=(4, 4), stride=1)
+                in_channels=input_size[0],
+                out_channels=conv_channels,
+                kernel_size=kernel_size,
+                padding=padding,
+                stride=stride)
             self.sigmoid = Sigmoid()
             self.fc = Linear(
-                in_features=8 * 25 * 25, out_features=10, bias=True)
+                in_features=output_numel, out_features=num_outputs, bias=True)
 
         def forward(self, input):
             x = self.conv(input)
             x = self.sigmoid(x)
-            # reshape into 2d tensor for fully-connected layer
+            # flatten for fully-connected layer
             x = x.reshape(x.size()[0], -1)
             return self.fc(x)
 
-    # create the model
-    set_seeds(seed)
-    return SimplestCNN()
+    return c1d1Torch()
 
 
 def test_compare_c1d1_parameters():
-    """Compare the parameters of the HBP and PyTorch simplest MNIST CNNs."""
-    hbp_cnn = c1d1(seed=1)
-    torch_cnn = torch_c1d1(seed=1)
+    """Compare the parameters of the HBP and PyTorch c1d1."""
+    set_seeds(0)
+    hbp_cnn = c1d1()
+    set_seeds(0)
+    torch_cnn = torch_c1d1()
     assert len(list(hbp_cnn.parameters())) == len(list(torch_cnn.parameters()))
     for p1, p2 in zip(hbp_cnn.parameters(), torch_cnn.parameters()):
         assert torch_allclose(p1, p2)
 
 
 def test_c1d1_forward():
-    """Compare forward pass of HBP model and PyTorch implementation."""
-    hbp_cnn = c1d1(seed=0)
-    torch_cnn = torch_c1d1(seed=0)
+    """Compare forward pass of HBP model and PyTorch c1d1."""
+    set_seeds(0)
+    hbp_cnn = c1d1()
+    set_seeds(0)
+    torch_cnn = torch_c1d1()
     x = rand(12, 1, 28, 28)
     out_torch = torch_cnn(x)
     out_hbp = hbp_cnn(x)
