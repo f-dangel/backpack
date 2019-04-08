1ca8ef2987cb3cb643332e5d0d6495195df1ada2
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-04-08 18:14:04 +0200

    Move hardcoded test of HBP linear to tests directory

--- tests/hbp/linear_test.py
+++ tests/hbp/linear_test.py
@@ -1,8 +1,10 @@
 """Test HBP of linear layer."""
 
+import unittest
 import torch
 from torch.nn import Linear
 from bpexts.hbp.linear import HBPLinear
+from bpexts.hbp.loss import batch_summed_hessian
 from bpexts.utils import set_seeds
 from hbp_test import hbp_test
 
@@ -54,3 +56,88 @@ if torch.cuda.is_available():
                 num_hvp=num_hvp)):
         """Compare torch Linear and HBPLinear on GPU."""
         pass
+
+
+class HBPLinearHardcodedTest(unittest.TestCase):
+    """Hardcoded test of ``HBPLinear``."""
+
+    @staticmethod
+    def example_layer():
+        """Return example linear layer."""
+        layer = HBPLinear(in_features=3, out_features=2)
+        w = torch.tensor([[1., 2., 3.], [4., 5., 6.]], requires_grad=True)
+        b = torch.tensor([7., 8.], requires_grad=True)
+        layer.weight.data = w
+        layer.bias.data = b
+        return layer
+
+    @staticmethod
+    def example_input():
+        """Example input for the linear layer."""
+        x = torch.tensor([[1., 3., 5.]], requires_grad=True)
+        return x
+
+    @staticmethod
+    def example_loss(tensor):
+        """Sum all square entries of a tensor."""
+        return (tensor**2).view(-1).sum(0)
+
+    def test_mean_input_hook(self):
+        """Check storing of mean_input hook."""
+        layer = self.example_layer()
+        x = torch.tensor([[2., 4., 6.], [8., 10., 12.], [14., 16., 18.]])
+        mean_x = torch.tensor([[8., 10., 12.]])
+        # forward, calls hook
+        layer(x)
+        assert torch.allclose(layer.mean_input, mean_x)
+
+    def test_input_hessian(self):
+        """Return layer after backward_hessian, check input Hessian."""
+        layer, x = self.example_layer(), self.example_input()
+        out = layer(x)
+        loss = self.example_loss(out)
+        # Hessian of loss function w.r.t layer output
+        output_hessian = batch_summed_hessian(loss, out)
+        loss_hessian = torch.tensor([[2., 0.], [0., 2.]])
+        assert torch.allclose(loss_hessian, output_hessian)
+        # Hessian with respect to layer inputs
+        # call hessian backward
+        input_hessian = layer.backward_hessian(loss_hessian)
+        # result: W^T * output_hessian * W
+        h_in_result = torch.tensor([[34., 44., 54.], [44., 58., 72.],
+                                    [54., 72., 90.]])
+        assert torch.allclose(input_hessian, h_in_result)
+        return layer
+
+    def test_bias_hessian(self, random_vp=10):
+        """Check correct backpropagation of bias Hessian and HVP."""
+        layer = self.test_input_hessian()
+        # Hessian with respect to layer bias
+        bias_hessian = torch.tensor([[2., 0.], [0., 2.]])
+        assert torch.allclose(layer.bias.hessian, bias_hessian)
+        # check Hessian-vector product
+        for _ in range(random_vp):
+            v = torch.randn(2)
+            vp = layer.bias.hvp(v)
+            result = bias_hessian.matmul(v)
+            assert torch.allclose(vp, result, atol=1E-5)
+
+    def test_weight_hessian(self, random_vp=10):
+        """Check correct weight Hessian/HVP backpropagation."""
+        layer = self.test_input_hessian()
+        # Hessian with respect to layer weights
+        # x * x^T \otimes output_hessian
+        weight_hessian = torch.tensor([[2., 6., 10., 0., 0., 0.],
+                                       [6., 18., 30., 0., 0., 0.],
+                                       [10., 30., 50., 0., 0., 0.],
+                                       [0., 0., 0., 2., 6., 10.],
+                                       [0., 0., 0., 6., 18., 30.],
+                                       [0., 0., 0., 10., 30., 50.]])
+        print(layer.weight.hessian())
+        assert torch.allclose(layer.weight.hessian(), weight_hessian)
+        # check Hessian-vector product
+        for _ in range(random_vp):
+            v = torch.randn(6)
+            vp = layer.weight.hvp(v)
+            result = weight_hessian.matmul(v)
+            assert torch.allclose(vp, result, atol=1E-5)
