ac1036767aa025c10c475902b5325e28514b00f3
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-06-03 14:07:42 +0200

    Move test of HBPModule

--- tests/hbp/module_test.py
+++ tests/hbp/module_test.py
@@ -1,6 +1,6 @@
 """Test HBP decoration of torch.nn.Module subclasses."""
 
-from .module import hbp_decorate
+from bpexts.module import hbp_decorate
 from torch.nn import Linear
 
 
@@ -9,4 +9,4 @@ def test_hbp_decorate():
     modifiedLinear = hbp_decorate(Linear)
     inputs, outputs = 5, 2
     hbp_linear = modifiedLinear(in_features=inputs, out_features=outputs)
-    assert(hbp_linear.__class__.__name__) == 'HBPLinear'
+    assert (hbp_linear.__class__.__name__) == 'HBPLinear'
