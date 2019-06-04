675203ae1d785f40226a57619b9a529f7fc77e0f
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-06-04 10:40:43 +0200

    Fix wrong assignment of average_param_jac

--- bpexts/hbp/module.py
+++ bpexts/hbp/module.py
@@ -49,8 +49,9 @@ def hbp_decorate(module_subclass):
             # hooks might be specific to the mode, remove the old ones
             self.disable_hbp()
             self.average_input_jac = average_input_jacobian
-            self.average_param_jac = (
-                None if not self.has_trainable_parameters() else True)
+            self.average_param_jac = (None
+                                      if not self.has_trainable_parameters()
+                                      else average_parameter_jacobian)
             # enable hooks
             self.enable_hbp()
 
