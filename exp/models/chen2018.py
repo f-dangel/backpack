ea30de4faf8f5eb90a67703ca42d11393e3f81d2
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-06-09 18:25:32 +0200

    Only formulate Chen models as torch Sequentials

--- exp/models/chen2018.py
+++ exp/models/chen2018.py
@@ -1,17 +1,14 @@
 """Model architectures used in Chen et al.: BDA-PCH (2018).
 
-The same initialization method for the parameters was chosen.
+The same initialization method for the parameters is chosen.
 """
 
+import torch
 import torch.nn as nn
-from bpexts.hbp.linear import HBPLinear
-from bpexts.hbp.sigmoid import HBPSigmoid
-from bpexts.hbp.combined_sigmoid import HBPSigmoidLinear
-from bpexts.hbp.sequential import HBPSequential
-from bpexts.utils import set_seeds
+from torch.nn import Sequential, Linear, Sigmoid
 
 
-def original_mnist_model(seed=None):
+def mnist_model():
     """FCNN architecture used by Chen et al on MNIST.
 
     The architecture uses the following structure:
@@ -19,63 +16,15 @@ def original_mnist_model(seed=None):
         (sigmoid)->(32->10)
 
     Use Xavier initialization method for weights, set bias to 0.
-
-    Parameters:
-    -----------
-    seed : int
-        Random seed used for initialization process of layers,
-        No seed will be set if left `None`
-
-    Returns:
-    --------
-    HBPSequential:
-        `PyTorch` module behaving like `nn.Sequential` with HBP
-        functionality
-
-    Examples:
-    ---------
-    The equivalent `PyTorch` model architecture looks as follows
-    >>> import torch.nn as nn
-    >>> nn.Sequential(nn.Linear(784, 512),
-                      nn.Sigmoid(),
-                      nn.Linear(512, 128),
-                      nn.Sigmoid(),
-                      nn.Linear(128, 32),
-                      nn.Sigmoid(),
-                      nn.Linear(32, 10))
     """
-    set_seeds(seed)
-    model = HBPSequential(
-        HBPLinear(784, 512), HBPSigmoidLinear(512, 128),
-        HBPSigmoidLinear(128, 32), HBPSigmoidLinear(32, 10))
-    xavier_init(model)
-    # make sure it's using the right HBP approximations
-    for idx, mod in enumerate(model.children()):
-        if idx == 0:
-            assert mod.average_input_jac == None
-        else:
-            assert mod.average_input_jac == True
-        assert mod.average_param_jac == True
-    return model
-
-
-def separated_mnist_model(seed=None):
-    """Original MNIST model with activations treated separately in HBP.
-
-    Parameters:
-    -----------
-    seed : (int)
-        Set seed before weight initialization, no reset if left `None`
-    """
-    set_seeds(seed)
-    model = HBPSequential(
-        HBPLinear(784, 512), HBPSigmoid(), HBPLinear(512, 128), HBPSigmoid(),
-        HBPLinear(128, 32), HBPSigmoid(), HBPLinear(32, 10))
+    model = Sequential(
+        Linear(784, 512), Sigmoid(), Linear(512, 128), Sigmoid(),
+        Linear(128, 32), Sigmoid(), Linear(32, 10))
     xavier_init(model)
     return model
 
 
-def original_cifar10_model(seed=None):
+def cifar10_model():
     """FCNN architecture used by Chen et al on CIFAR-10.
 
     The architecture uses the following neuron structure:
@@ -83,69 +32,12 @@ def original_cifar10_model(seed=None):
     with sigmoid activation functions and linear outputs.
 
     Use Xavier initialization method for weights, set bias to 0.
-
-    Parameters:
-    -----------
-    seed : (int)
-        Random seed used for initialization process of layers,
-        no reset if left `None`
-
-    Returns:
-    --------
-    HBPSequential: Fully-connected deep network with described architecture
-             and positive curvature Hessian backpropagation
-
-    Examples:
-    ---------
-    The equivalent `PyTorch` model architecture looks as follows
-    >>> import torch.nn as nn
-    >>> nn.Sequential(nn.Linear(3072, 1024),
-                      nn.Sigmoid(),
-                      nn.Linear(1024, 512),
-                      nn.Sigmoid(),
-                      nn.Linear(512, 256),
-                      nn.Sigmoid(),
-                      nn.Linear(256, 128),
-                      nn.Sigmoid(),
-                      nn.Linear(128, 64),
-                      nn.Sigmoid(),
-                      nn.Linear(64, 32),
-                      nn.Sigmoid(),
-                      nn.Linear(32, 16),
-                      nn.Sigmoid(),
-                      nn.Linear(16, 10))
     """
-    set_seeds(seed)
-    model = HBPSequential(
-        HBPLinear(3072, 1024), HBPSigmoidLinear(1024, 512),
-        HBPSigmoidLinear(512, 256), HBPSigmoidLinear(256, 128),
-        HBPSigmoidLinear(128, 64), HBPSigmoidLinear(64, 32),
-        HBPSigmoidLinear(32, 16), HBPSigmoidLinear(16, 10))
-    xavier_init(model)
-    # make sure it's using the right HBP approximations
-    for idx, mod in enumerate(model.children()):
-        if idx == 0:
-            assert mod.average_input_jac == None
-        else:
-            assert mod.average_input_jac == True
-        assert mod.average_param_jac == True
-    return model
-
-
-def separated_cifar10_model(seed=None):
-    """Original CIFAR-10 model with activations treated separately in HBP.
-
-    Parameters:
-    -----------
-    seed : (int)
-        Set random seed before layer initialization, no reset if left `None`
-    """
-    set_seeds(seed)
-    model = HBPSequential(
-        HBPLinear(3072, 1024), HBPSigmoid(), HBPLinear(1024, 512),
-        HBPSigmoid(), HBPLinear(512, 256), HBPSigmoid(), HBPLinear(256, 128),
-        HBPSigmoid(), HBPLinear(128, 64), HBPSigmoid(), HBPLinear(64, 32),
-        HBPSigmoid(), HBPLinear(32, 16), HBPSigmoid(), HBPLinear(16, 10))
+    model = Sequential(
+        Linear(3072, 1024), Sigmoid(), Linear(1024, 512), Sigmoid(),
+        Linear(512, 256), Sigmoid(), Linear(256, 128), Sigmoid(),
+        Linear(128, 64), Sigmoid(), Linear(64, 32), Sigmoid(), Linear(32, 16),
+        Sigmoid(), Linear(16, 10))
     xavier_init(model)
     return model
 
--- exp/models/chen2018_test.py
+++ exp/models/chen2018_test.py
@@ -1,25 +1,28 @@
 """Test of model architectures from Chen et al.: BDA-PCH (2018)."""
 
-from bpexts.utils import set_seeds, torch_allclose
-from .chen2018 import (original_mnist_model,
-                             separated_mnist_model,
-                             original_cifar10_model,
-                             separated_cifar10_model)
-
-from torch import randn
+import torch
+from bpexts.hbp.sequential import convert_torch_to_hbp
+from bpexts.hbp.parallel.sequential import HBPParallelSequential
+from .chen2018 import (mnist_model, cifar10_model)
 
 
 def test_forward_mnist_models():
-    """Check same behaviour of original and separated MNIST model."""
-    input = randn(2, 784)
-    original = original_mnist_model(seed=13)
-    separated = separated_mnist_model(seed=13)
-    assert torch_allclose(original(input), separated(input), atol=1E-5)
+    """Check same behaviour of original and HBP/split MNIST model."""
+    max_blocks = 5
+    input = torch.randn(2, 784)
+    original = mnist_model()
+    hbp = convert_torch_to_hbp(original)
+    hbp_parallel = HBPParallelSequential(max_blocks, *list(hbp.children()))
+    assert torch.allclose(original(input), hbp(input), atol=1E-5)
+    assert torch.allclose(original(input), hbp_parallel(input), atol=1E-5)
 
 
 def test_forward_cifar10_models():
-    """Check same behaviour of original and separated CIFAR-10 model."""
-    input = randn(2, 3072)
-    original = original_cifar10_model(seed=11)
-    separated = separated_cifar10_model(seed=11)
-    assert torch_allclose(original(input), separated(input), atol=1E-5)
+    """Check same behaviour of original and HBP/split CIFAR-10 model."""
+    max_blocks = 5
+    input = torch.randn(2, 3072)
+    original = cifar10_model()
+    hbp = convert_torch_to_hbp(original)
+    hbp_parallel = HBPParallelSequential(max_blocks, *list(hbp.children()))
+    assert torch.allclose(original(input), hbp(input), atol=1E-5)
+    assert torch.allclose(original(input), hbp_parallel(input), atol=1E-5)
