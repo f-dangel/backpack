80db189836714fb0c0d4bf784dead6f19dd27465
Frederik Kunstner <frederik.kunstner@gmail.com>
2019-06-25 08:32:12 +0200

    Try to clean the tests a bit

--- tests/gradient/conv2d_test.py
+++ tests/gradient/conv2d_test.py
@@ -11,18 +11,42 @@ from torch.nn import Conv2d
 from random import (randint, choice)
 from bpexts.gradient.conv2d import Conv2d as G_Conv2d
 import bpexts.gradient.config as config
-from bpexts.utils import torch_allclose
+from bpexts.utils import torch_allclose as allclose
 
-# convolution parameters
-in_channels = 3
-out_channels = 2
-kernel_size = (2, 2)
-stride = (1, 1)
-padding = (0, 0)
-dilation = (1, 1)
-bias = False
+TEST_ATOL = 1e-4
 
-# predefined kernel matrix
+###
+# Problem settings
+###
+
+
+def make_conv_params(in_channels, out_channels, kernel_size, stride, padding, dilatation, bias, kernel):
+    return {
+        "in_channels": in_channels,
+        "out_channels": out_channels,
+        "kernel_size": kernel_size,
+        "stride": stride,
+        "padding": padding,
+        "dilatation": dilatation,
+        "bias": bias,
+        "kernel": kernel,
+    }
+
+
+def make_conv_layer(LayerClass, conv_params):
+    layer = LayerClass(
+        in_channels=conv_params["in_channels"],
+        out_channels=conv_params["out_channels"],
+        kernel_size=conv_params["kernel_size"],
+        stride=conv_params["stride"],
+        padding=conv_params["padding"],
+        dilation=conv_params["dilatation"],
+        bias=conv_params["bias"]
+    )
+    layer.weight.data = conv_params["kernel"]
+    return layer
+
+
 kernel11 = [[1, 1], [2, 2]]
 kernel12 = [[1, 1], [1, 1]]
 kernel13 = [[0, 1], [1, 0]]
@@ -31,6 +55,17 @@ kernel22 = [[2, 1], [2, 1]]
 kernel23 = [[1, 2], [2, 0]]
 kernel = Tensor([[kernel11, kernel12, kernel13],
                  [kernel21, kernel22, kernel23]]).float()
+
+CONV_PARAMS = make_conv_params(
+    in_channels=3,
+    out_channels=2,
+    kernel_size=(2, 2),
+    stride=(1, 1),
+    padding=(0, 0),
+    dilatation=(1, 1),
+    bias=False,
+    kernel=kernel
+)
 
 # input (1 sample)
 in_feature1 = [[1, 2, 0], [1, 1, 3], [0, 2, 2]]
@@ -41,29 +76,10 @@ result1 = [[14, 20], [15, 24]]
 result2 = [[12, 24], [17, 26]]
 out1 = Tensor([[result1, result2]]).float()
 
-# convolution layer from torch.nn
-conv2d = Conv2d(
-    in_channels=in_channels,
-    out_channels=out_channels,
-    kernel_size=kernel_size,
-    stride=stride,
-    padding=padding,
-    dilation=dilation,
-    bias=bias)
-conv2d.weight.data = kernel
 
-# extended convolution layer
-g_conv2d = G_Conv2d(
-    in_channels=in_channels,
-    out_channels=out_channels,
-    kernel_size=kernel_size,
-    stride=stride,
-    padding=padding,
-    dilation=dilation,
-    bias=bias)
-g_conv2d.weight.data = kernel
+conv2d = make_conv_layer(Conv2d, CONV_PARAMS)
+g_conv2d = make_conv_layer(G_Conv2d, CONV_PARAMS)
 
-# as lists for zipping
 inputs = [in1]
 results = [out1]
 
@@ -80,93 +96,66 @@ def test_forward():
     """
     for input, result in zip(inputs, results):
         out_conv2d = conv2d(input)
-        assert torch_allclose(out_conv2d, result)
+        assert allclose(out_conv2d, result)
         out_g_conv2d = g_conv2d(input)
-        assert torch_allclose(out_g_conv2d, result)
+        assert allclose(out_g_conv2d, result)
 
 
-def random_convolutions_and_inputs(in_channels=None,
-                                   out_channels=None,
-                                   kernel_size=None,
-                                   stride=None,
-                                   padding=None,
-                                   dilation=None,
-                                   bias=None,
-                                   kernel_shape=None,
-                                   batch_size=None,
-                                   in_size=None):
+def random_convolutions_and_inputs(in_channels=randint(1, 3),
+                                   out_channels=randint(1, 3),
+                                   kernel_size=(randint(1, 3), randint(1, 3)),
+                                   stride=(randint(1, 3), randint(1, 3)),
+                                   padding=(randint(0, 2), randint(0, 2)),
+                                   dilatation=(randint(1, 3), randint(1, 3)),
+                                   bias=choice([True, False]),
+                                   batch_size=randint(1, 3),
+                                   in_size=(randint(8, 12), randint(8, 12))):
     """Return same torch/exts 2d conv modules and random inputs.
 
     Arguments can be fixed by handing them over.
     """
-    # random convolution parameters
-    in_channels = in_channels if in_channels is not None else randint(1, 3)
-    out_channels = out_channels if out_channels is not None else randint(1, 3)
-    kernel_size = kernel_size if kernel_size is not None\
-        else (randint(1, 3), randint(1, 3))
-    stride = stride if stride is not None else (randint(1, 3), randint(1, 3))
-    padding = padding if padding is not None\
-        else (randint(0, 2), randint(0, 2))
-    dilation = dilation if dilation is not None\
-        else (randint(1, 3), randint(1, 3))
-    bias = bias if bias is not None else choice([True, False])
-    # random kernel
     kernel_shape = (out_channels, in_channels) + kernel_size
     kernel = randn(kernel_shape)
-    # random input
-    batch_size = batch_size if batch_size is not None else randint(1, 3)
-    in_size = in_size if in_size is not None\
-        else (randint(8, 12), randint(8, 12))
     in_shape = (batch_size, in_channels) + in_size
     input = randn(in_shape)
-    # torch.nn convolution
-    conv2d = Conv2d(
+
+    conv_params = make_conv_params(
         in_channels=in_channels,
         out_channels=out_channels,
         kernel_size=kernel_size,
         stride=stride,
         padding=padding,
-        dilation=dilation,
-        bias=bias)
-    conv2d.weight.data = kernel
-    # extended convolution layer
-    g_conv2d = G_Conv2d(
-        in_channels=in_channels,
-        out_channels=out_channels,
-        kernel_size=kernel_size,
-        stride=stride,
-        padding=padding,
-        dilation=dilation,
-        bias=bias)
-    g_conv2d.weight.data = kernel
-    # random bias
+        dilatation=dilatation,
+        bias=bias,
+        kernel=kernel
+    )
+
+    conv2d = make_conv_layer(Conv2d, conv_params)
+    g_conv2d = make_conv_layer(G_Conv2d, conv_params)
+
     if bias is True:
         bias_vals = randn(out_channels)
         conv2d.bias.data = bias_vals
         g_conv2d.bias.data = bias_vals
-        assert torch_allclose(conv2d.bias, g_conv2d.bias)
-    assert torch_allclose(conv2d.weight, g_conv2d.weight)
+        assert allclose(conv2d.bias, g_conv2d.bias)
+    assert allclose(conv2d.weight, g_conv2d.weight)
+
     return conv2d, g_conv2d, input
 
 
 def compare_grads(conv2d, g_conv2d, input):
     """Feed input through nn and exts conv2d, compare bias/weight grad."""
-    # backward for torch conv2d
-    out_conv2d = conv2d(input)
-    loss = loss_function(out_conv2d)
+    loss = loss_function(conv2d(input))
     loss.backward()
-    # backward for exts conv2d
-    out_g_conv2d = g_conv2d(input)
-    loss_g = loss_function(out_g_conv2d)
+
+    loss_g = loss_function(g_conv2d(input))
     with config.bpexts(config.BATCH_GRAD):
         loss_g.backward()
-    # need to choose lower precision for some reason
-    assert torch_allclose(g_conv2d.bias.grad, conv2d.bias.grad, atol=1E-4)
-    assert torch_allclose(g_conv2d.weight.grad, conv2d.weight.grad, atol=1E-4)
-    assert torch_allclose(
-        g_conv2d.bias.grad_batch.sum(0), conv2d.bias.grad, atol=1E-4)
-    assert torch_allclose(
-        g_conv2d.weight.grad_batch.sum(0), conv2d.weight.grad, atol=1E-4)
+
+    assert allclose(g_conv2d.bias.grad, conv2d.bias.grad, atol=TEST_ATOL)
+    assert allclose(g_conv2d.weight.grad, conv2d.weight.grad, atol=TEST_ATOL)
+    assert allclose(g_conv2d.bias.grad_batch.sum(0), conv2d.bias.grad, atol=TEST_ATOL)
+    assert allclose(g_conv2d.weight.grad_batch.sum(0), conv2d.weight.grad, atol=TEST_ATOL)
 
 
 def test_random_grad(random_runs=10):
--- tests/gradient/linear_test.py
+++ tests/gradient/linear_test.py
@@ -4,23 +4,30 @@ from torch import Tensor
 from torch.nn import Linear
 from bpexts.gradient.linear import Linear as G_Linear
 import bpexts.gradient.config as config
-from bpexts.utils import torch_allclose, set_seeds
+from bpexts.utils import torch_allclose as allclose, set_seeds
 from .gradient_test import set_up_gradient_tests
+
+
+##
+# Example problems definitions
+##
+
 
 # predefined weight matrix and bias
 weight = Tensor([[1, 2, 3], [4, 5, 6]]).float()
 bias = Tensor([7, 8]).float()
 in_features, out_features = 3, 2
 
-# linaer layer with fixed weight and bias
-lin = Linear(in_features=in_features, out_features=out_features)
-lin.weight.data = weight
-lin.bias.data = bias
 
-# extended linear layer with fixed weight and bias
-g_lin = G_Linear(in_features=in_features, out_features=out_features)
-g_lin.weight.data = weight
-g_lin.bias.data = bias
+def make_lin_layer(LayerClass, in_features, out_features, weight, bias):
+    layer = LayerClass(in_features=in_features, out_features=out_features)
+    layer.weight.data = weight
+    layer.bias.data = bias
+    return layer
+
+
+lin = make_lin_layer(Linear, in_features, out_features, weight, bias)
+g_lin = make_lin_layer(G_Linear, in_features, out_features, weight, bias)
 
 
 def loss_function(tensor):
@@ -28,101 +35,114 @@ def loss_function(tensor):
     return ((tensor.view(-1))**2).sum()
 
 
-# input (1 sample)
-in1 = Tensor([[1, 1, 1]]).float()
-out1 = Tensor([[6 + 7, 15 + 8]]).float()
-loss1 = 13**2 + 23**2
-bias_grad1 = Tensor([2 * 13, 2 * 23]).float()
-bias_grad_batch1 = bias_grad1
-weight_grad1 = Tensor([[26, 26, 26], [46, 46, 46]]).float()
-weight_grad_batch1 = weight_grad1
+EXAMPLE_1 = {
+    "in": Tensor([[1, 1, 1]]).float(),
+    "out": Tensor([[6 + 7, 15 + 8]]).float(),
+    "loss": 13**2 + 23**2,
+    "bias_grad": Tensor([2 * 13, 2 * 23]).float(),
+    "bias_grad_batch": Tensor([2 * 13, 2 * 23]).float(),
+    "weight_grad": Tensor([[26, 26, 26], [46, 46, 46]]).float(),
+    "weight_grad_batch": Tensor([[26, 26, 26], [46, 46, 46]]).float(),
+}
 
-# input (2 samples)
-in2 = Tensor([[1, 0, 1], [0, 1, 0]]).float()
-out2 = Tensor([[4 + 7, 10 + 8], [2 + 7, 5 + 8]]).float()
-loss2 = 11**2 + 18**2 + 9**2 + 13**2
-bias_grad2 = Tensor([2 * (11 + 9), 2 * (18 + 13)])
-bias_grad_batch2 = Tensor([[2 * 11, 2 * 18], [2 * 9, 2 * 13]]).float()
-weight_grad2 = Tensor([[22, 18, 22], [36, 26, 36]]).float()
-weight_grad_batch2 = Tensor([[[22, 0, 22], [36, 0, 36]],
-                             [[0, 18, 0], [0, 26, 0]]]).float()
+EXAMPLE_2 = {
+    "in": Tensor([[1, 0, 1], [0, 1, 0]]).float(),
+    "out": Tensor([[4 + 7, 10 + 8], [2 + 7, 5 + 8]]).float(),
+    "loss": 11**2 + 18**2 + 9**2 + 13**2,
+    "bias_grad": Tensor([2 * (11 + 9), 2 * (18 + 13)]),
+    "bias_grad_batch": Tensor([[2 * 11, 2 * 18], [2 * 9, 2 * 13]]).float(),
+    "weight_grad": Tensor([[22, 18, 22], [36, 26, 36]]).float(),
+    "weight_grad_batch": Tensor([[[22, 0, 22], [36, 0, 36]],
+                                 [[0, 18, 0], [0, 26, 0]]]).float(),
+}
 
-# as lists for zipping
-inputs = [in1, in2]
-results = [out1, out2]
-losses = [loss1, loss2]
-bias_grads = [bias_grad1, bias_grad2]
-bias_grads_batch = [bias_grad_batch1, bias_grad_batch2]
-weight_grads = [weight_grad1, weight_grad2]
-weight_grads_batch = [weight_grad_batch1, weight_grad_batch2]
+EXAMPLES = [EXAMPLE_1, EXAMPLE_2]
+
+
+##
+# Tests
+##
 
 
 def test_forward():
     """Compare forward of torch.nn.Linear and exts.gradient.G_Linear.
 
     Handles single-instance and batch mode."""
-    for input, result in zip(inputs, results):
+    for ex in EXAMPLES:
+        input, result = ex["in"], ex["out"]
+
         out_lin = lin(input)
-        assert torch_allclose(out_lin, result)
+        assert allclose(out_lin, result)
+
         out_g_lin = g_lin(input)
-        assert torch_allclose(out_g_lin, result)
+        assert allclose(out_g_lin, result)
 
 
 def test_losses():
     """Test output of loss function."""
-    for input, loss in zip(inputs, losses):
-        out = g_lin(input)
-        loss_val = loss_function(out)
-        assert loss_val.item() == loss
+    for ex in EXAMPLES:  # input, loss in zip(inputs, losses):
+        loss_val = loss_function(g_lin(ex["in"]))
+        assert loss_val.item() == ex["loss"]
 
 
 def test_grad():
     """Test computation of bias/weight gradients."""
-    for input, b_grad, w_grad in zip(inputs, bias_grads, weight_grads):
-        out = g_lin(input)
-        loss = loss_function(out)
+    for ex in EXAMPLES:
+        input, b_grad, w_grad = ex["in"], ex["bias_grad"], ex["weight_grad"]
+
+        loss = loss_function(g_lin(input))
         with config.bpexts(config.GRAD):
             loss.backward()
-        assert torch_allclose(g_lin.bias.grad, b_grad)
-        assert torch_allclose(g_lin.weight.grad, w_grad)
+
+        assert allclose(g_lin.bias.grad, b_grad)
+        assert allclose(g_lin.weight.grad, w_grad)
+
         g_lin.zero_grad()
         g_lin.clear_grad_batch()
 
 
 def test_grad_batch():
     """Test computation of bias/weight batch gradients."""
-    for input, b_grad_batch, w_grad_batch in zip(inputs, bias_grads_batch,
-                                                 weight_grads_batch):
-        out = g_lin(input)
-        loss = loss_function(out)
+    for ex in EXAMPLES:
+        input, b_grad_batch, w_grad_batch = ex["in"], ex["bias_grad_batch"], ex["weight_grad_batch"]
+
+        loss = loss_function(g_lin(input))
         with config.bpexts(config.BATCH_GRAD):
             loss.backward()
-        assert torch_allclose(g_lin.bias.grad_batch, b_grad_batch)
-        print(g_lin.weight.grad_batch)
-        print(w_grad_batch)
-        assert torch_allclose(g_lin.weight.grad_batch, w_grad_batch)
+
+        assert allclose(g_lin.bias.grad_batch, b_grad_batch), "{} ≠ {}".format(g_lin.bias.grad_batch, b_grad_batch)
+        assert allclose(g_lin.weight.grad_batch, w_grad_batch), "{} ≠ {}".format(g_lin.weight.grad_batch, w_grad_batch)
+
         g_lin.zero_grad()
         g_lin.clear_grad_batch()
 
 
-# automated test
-# hyper-parameters
-in_features = 20
-out_features = 10
-bias = True
-batch = 13
-input_size = (batch, in_features)
-atol = 1e-5
-rtol = 1e-5
+TEST_SETTINGS = {
+    "in_features": 20,
+    "out_features": 10,
+    "bias": True,
+    "batch": 13,
+    "rtol": 1e-5,
+    "atol": 1e-5
+}
 
 
 def layer_fn():
     set_seeds(0)
     return G_Linear(
-        in_features=in_features, out_features=out_features, bias=bias)
+        in_features=TEST_SETTINGS["in_features"],
+        out_features=TEST_SETTINGS["out_features"],
+        bias=TEST_SETTINGS["bias"]
+    )
 
 
-for name, test_cls in set_up_gradient_tests(
-        layer_fn, 'Linear', input_size=input_size, atol=atol, rtol=rtol):
+gradient_tests = set_up_gradient_tests(
+    layer_fn, 'Linear',
+    input_size=(TEST_SETTINGS["batch"], TEST_SETTINGS["in_features"]),
+    atol=TEST_SETTINGS["atol"],
+    rtol=TEST_SETTINGS["rtol"]
+)
+
+for name, test_cls in gradient_tests:
     exec('{} = test_cls'.format(name))
     del test_cls
