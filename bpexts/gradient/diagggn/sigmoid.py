5bfd4e1e511e71529550d7ad73d6926ceb4d4db6
Frederik Kunstner <frederik.kunstner@gmail.com>
2019-06-28 16:20:14 +0200

    Fix Diag GGN sigmoid

--- bpexts/gradient/diagggn/sigmoid.py
+++ bpexts/gradient/diagggn/sigmoid.py
@@ -9,7 +9,7 @@ def diag_ggn(module, grad_output):
 
 
 def backpropagate_sqrt_ggn(module, grad_output, sqrt_ggn_out):
-    d_sigma = module.output0 * (1. - module.output0)
+    d_sigma = module.output * (1. - module.output)
     sqrt_ggn_in = einsum('bi,bic->bic', (d_sigma, sqrt_ggn_out))
     CTX._backpropagated_sqrt_ggn = sqrt_ggn_in
 
