f97ef214789f6736d6d42069aae0ebcee5500e99
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-06-28 15:18:56 +0200

    diag_ggn for Conv2d, tested for parameters, but not for backpropagated tensor

--- bpexts/gradient/diagggn/conv2d.py
+++ bpexts/gradient/diagggn/conv2d.py
@@ -1,30 +1,120 @@
 import torch.nn
 from torch import einsum
 from ..config import CTX
+from ..batchgrad.conv2d import unfold_func
 
 
 def diag_ggn(module, grad_output):
     sqrt_ggn_out = CTX._backpropagated_sqrt_ggn
     if module.bias is not None and module.bias.requires_grad:
-        module.bias.diag_ggn = bias_diag_ggn(module, grad_output)
+        module.bias.diag_ggn = bias_diag_ggn(module, grad_output, sqrt_ggn_out)
     if module.weight.requires_grad:
-        module.weight.diag_ggn = weight_diag_ggn(module, grad_output)
-
-    backpropagate_sqrt_ggn(module, grad_output, sqrt_ggn_out)
+        module.weight.diag_ggn = weight_diag_ggn(module, grad_output,
+                                                 sqrt_ggn_out)
+    CTX._backpropagated_sqrt_ggn = backpropagate_sqrt_ggn(
+        module, grad_output, sqrt_ggn_out)
 
 
 def bias_diag_ggn(module, grad_output, sqrt_ggn_out):
-    return einsum('bic->i', (sqrt_ggn_out**2))
+    # separate channel and spatial coordinates of features
+    batch = module.input0.size(0)
+    num_classes = sqrt_ggn_out.size(2)
+    out_pixels = module.output_shape[2] * module.output_shape[3]
+    # apply Jacobian
+    sqrt_ggn = sqrt_ggn_out.view(batch, module.out_channels, out_pixels,
+                                 num_classes)
+    sqrt_ggn = einsum('bijc->bic', sqrt_ggn)
+    # extract diag
+    return einsum('bic->i', (sqrt_ggn**2))
 
 
+# TODO: Move the axis-merging trick to a separate method
 def weight_diag_ggn(module, grad_output, sqrt_ggn_out):
-    # TODO need access to dimensions of input and output
-    raise NotImplementedError
+    # TODO: Figure out how to do this for multiple inputs
+
+    # checks for debugging
+    batch, out_channels, out_x, out_y = module.output_shape
+    in_features = module.input0.numel() / batch
+    out_features = out_channels * out_x * out_y
+    num_classes = sqrt_ggn_out.size(2)
+    assert tuple(sqrt_ggn_out.size())[:2] == (batch, out_features)
+
+    ##############################################################
+    # trick to process batch and class dimensions without for loop
+    # shape of sqrt_ggn_out: (batch, output, class)
+    # merge batch and class axes (einsum for convenience)
+    # TODO: This can be formulated more efficiently
+    sqrt_ggn = einsum('boc->cbo', (sqrt_ggn_out, )).contiguous()
+    sqrt_ggn = sqrt_ggn.view(num_classes * batch, out_features)
+
+    # separate channel and spatial dimensions
+    sqrt_ggn = sqrt_ggn.view(num_classes * batch, out_channels, out_x * out_y)
+    # end TODO
+
+    # unfolded input
+    X = unfold_func(module)(module.input0)
+    # repeat for each class, according to the docs, no way around copying
+    X = X.repeat(num_classes, 1, 1)
+
+    # TODO: This can be formulated more efficiently
+    # apply Jacobian with merged (batch, class) axes
+    sqrt_ggn = einsum('bml,bkl->bmk', (sqrt_ggn, X)).contiguous()
+    # result of shape (batch * class, kernel, channel)
+
+    # unmerge batch and class axes
+    sqrt_ggn = sqrt_ggn.view(num_classes, batch, module.weight.numel())
+    sqrt_ggn = einsum('cbk->bkc', (sqrt_ggn, ))
+    # end TODO
+    ##############################################################
+
+    # compute the diagonal of the flattened kernel
+    w_diag_ggn = einsum('bkc->k', (sqrt_ggn**2, ))
+
+    # reshape into kernel dimensions
+    return w_diag_ggn.view_as(module.weight)
 
 
 def backpropagate_sqrt_ggn(module, grad_output, sqrt_ggn_out):
-    # TODO need access to dimensions of input and output
-    raise NotImplementedError
+    # TODO: Figure out how to do this for multiple inputs
+
+    # checks for debugging
+    batch, out_channels, out_x, out_y = module.output_shape
+    in_features = module.input0.numel() / batch
+    out_features = out_channels * out_x * out_y
+    num_classes = sqrt_ggn_out.size(2)
+    assert tuple(sqrt_ggn_out.size())[:2] == (batch, out_features)
+
+    ##############################################################
+    # trick to process batch and class dimensions without for loop
+    # shape of sqrt_ggn_out: (batch, output, class)
+    # merge batch and class axes (einsum for convenience)
+    # TODO: This can be formulated more efficiently
+    sqrt_ggn = einsum('boc->cbo', (sqrt_ggn_out, )).contiguous()
+    sqrt_ggn = sqrt_ggn.view(num_classes * batch, out_features)
+
+    # separate channel and spatial dimensions
+    sqrt_ggn = sqrt_ggn.view(num_classes * batch, out_channels, out_x, out_y)
+    # end TODO
+
+    # apply Jacobian w.r.t. input
+    sqrt_ggn = torch.nn.functional.conv_transpose2d(
+        sqrt_ggn,
+        module.weight.data,
+        stride=module.stride,
+        padding=module.padding,
+        dilation=module.dilation,
+        groups=module.groups)
+    if tuple(sqrt_ggn.size())[1:] != tuple(module.input0.size())[1:]:
+        raise ValueError(
+            "Size after conv_transpose does not match",
+            "Got {}, and {}. Expecting all dimensions to match, except for the first."
+            .format(sqrt_ggn.size(), module.input0.size()))
+
+    # unmerge batch and class axes
+    sqrt_ggn = sqrt_ggn.view(num_classes, batch, in_features)
+    return einsum('cbi->bic', (sqrt_ggn, ))
+    # end TODO
+    ##############################################################
 
 
 SIGNATURE = [(torch.nn.Conv2d, "DIAG_GGN", diag_ggn)]
