f97612a8b40ba17e914d5c760a36453cabe27dad
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-06-28 18:30:21 +0200

    Avoid copy of pool_idx in backpropagation of sqrt_ggn

--- bpexts/gradient/diagggn/maxpool2d.py
+++ bpexts/gradient/diagggn/maxpool2d.py
@@ -28,28 +28,15 @@ def backpropagate_sqrt_ggn(module, grad_output, sqrt_ggn_out):
     num_classes = sqrt_ggn_out.size(2)
     assert tuple(sqrt_ggn_out.size())[:2] == (batch, out_features)
 
-    ##############################################################
-    # trick to process batch and class dimensions without for loop
-    # shape of sqrt_ggn_out: (batch, output, class)
-    # merge batch and class axes (einsum for convenience)
-    # TODO: This can be formulated more efficiently
-    sqrt_ggn = einsum('boc->cbo', (sqrt_ggn_out, )).contiguous()
-    sqrt_ggn = sqrt_ggn.view(num_classes * batch, out_features)
-
     # separate channel and spatial dimensions
-    sqrt_ggn = sqrt_ggn.view(num_classes * batch, channels, out_x * out_y)
-    # end TODO
-
+    sqrt_ggn = sqrt_ggn_out.view(batch, channels, out_x * out_y, num_classes)
     # apply Jacobian w.r.t. input
     result = torch.zeros(
-        num_classes * batch, channels, in_x * in_y, device=sqrt_ggn.device)
+        batch, channels, in_x * in_y, num_classes, device=sqrt_ggn.device)
     pool_idx = pool_idx.view(batch, channels, out_x * out_y)
-    pool_idx = pool_idx.repeat(num_classes, 1, 1)
+    pool_idx = pool_idx.unsqueeze(-1).expand(-1, -1, -1, num_classes)
     result.scatter_add_(2, pool_idx, sqrt_ggn)
-
-    # unmerge batch and class axes
-    result = result.view(num_classes, batch, in_features)
-    return einsum('cbi->bic', (result, ))
+    return result.view(batch, in_features, num_classes)
 
 
 SIGNATURE = [(torch.nn.MaxPool2d, "DIAG_GGN", diag_ggn)]
