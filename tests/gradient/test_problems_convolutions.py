aa12bf0a594fbb834e3646c813cdb6a2d1c2469b
Frederik Kunstner <frederik.kunstner@gmail.com>
2019-06-28 17:29:51 +0200

    Add 2-layer convs and linear tests

--- tests/gradient/test_problems_convolutions.py
+++ tests/gradient/test_problems_convolutions.py
@@ -6,7 +6,7 @@ from .test_problem import TestProblem
 
 TEST_SETTINGS = {
     "in_features": (3, 4, 5),
-    "out_channels": 4,
+    "out_channels": 3,
     "kernel_size": (3, 2),
     "padding": (1, 1),
     "bias": True,
@@ -17,30 +17,46 @@ TEST_SETTINGS = {
 
 
 set_seeds(0)
-convlayer = config.extend(torch.nn.Conv2d(
-    in_channels=TEST_SETTINGS["in_features"][0],
-    out_channels=TEST_SETTINGS["out_channels"],
-    kernel_size=TEST_SETTINGS["kernel_size"],
-    padding=TEST_SETTINGS["padding"],
-    bias=TEST_SETTINGS["bias"]
-))
+
+
+def convlayer():
+    return config.extend(torch.nn.Conv2d(
+        in_channels=TEST_SETTINGS["in_features"][0],
+        out_channels=TEST_SETTINGS["out_channels"],
+        kernel_size=TEST_SETTINGS["kernel_size"],
+        padding=TEST_SETTINGS["padding"],
+        bias=TEST_SETTINGS["bias"]
+    ))
+
+
+def convlayer2():
+    return config.extend(torch.nn.Conv2d(
+        in_channels=TEST_SETTINGS["in_features"][0],
+        out_channels=TEST_SETTINGS["out_channels"],
+        kernel_size=TEST_SETTINGS["kernel_size"],
+        padding=TEST_SETTINGS["padding"],
+        bias=TEST_SETTINGS["bias"]
+    ))
+
 
 input_size = (TEST_SETTINGS["batch"], ) + TEST_SETTINGS["in_features"]
 X = torch.randn(size=input_size)
 
-linearlayer = config.extend(torch.nn.Linear(
-    in_features=np.prod(
-        [f - TEST_SETTINGS["padding"][0] for f in TEST_SETTINGS["in_features"]]
-    ) * TEST_SETTINGS["out_channels"],
-    out_features=1
-))
+
+def linearlayer():
+    return config.extend(torch.nn.Linear(
+        in_features=np.prod(
+            [f - TEST_SETTINGS["padding"][0] for f in TEST_SETTINGS["in_features"]]
+        ) * TEST_SETTINGS["out_channels"],
+        out_features=1
+    ))
 
 
 def make_regression_problem():
     model = torch.nn.Sequential(
-        convlayer,
+        convlayer(),
         Flatten(),
-        linearlayer
+        linearlayer()
     )
 
     Y = torch.randn(size=(model(X).shape[0], 1))
@@ -52,7 +68,21 @@ def make_regression_problem():
 
 def make_classification_problem():
     model = torch.nn.Sequential(
-        convlayer,
+        convlayer(),
+        Flatten()
+    )
+
+    Y = torch.randint(high=X.shape[1], size=(model(X).shape[0],))
+
+    lossfunc = config.extend(torch.nn.CrossEntropyLoss())
+
+    return TestProblem(X, Y, model, lossfunc)
+
+
+def make_2layer_classification_problem():
+    model = torch.nn.Sequential(
+        convlayer(),
+        convlayer2(),
         Flatten()
     )
 
@@ -66,4 +96,5 @@ def make_classification_problem():
 TEST_PROBLEMS = {
     "conv-regression": make_regression_problem(),
     "conv-classification": make_classification_problem(),
+    "conv-classification-2layer": make_2layer_classification_problem(),
 }
--- tests/gradient/test_problems_linear.py
+++ tests/gradient/test_problems_linear.py
@@ -1,5 +1,5 @@
 import torch
-from bpexts.utils import set_seeds
+from bpexts.utils import set_seeds, Flatten
 import bpexts.gradient.config as config
 from .test_problem import TestProblem
 
@@ -16,6 +16,11 @@ set_seeds(0)
 linearlayer = config.extend(torch.nn.Linear(
     in_features=TEST_SETTINGS["in_features"],
     out_features=TEST_SETTINGS["out_features"],
+    bias=TEST_SETTINGS["bias"],
+))
+linearlayer2 = config.extend(torch.nn.Linear(
+    in_features=TEST_SETTINGS["out_features"],
+    out_features=3,
     bias=TEST_SETTINGS["bias"],
 ))
 summationLinearLayer = config.extend(torch.nn.Linear(
@@ -41,16 +46,22 @@ def make_regression_problem():
 
 
 def make_classification_problem():
-    class To2D(torch.nn.Module):
-        def __init__(self):
-            super().__init__()
-
-        def forward(self, input):
-            return input.view(input.shape[0], -1)
-
     model = torch.nn.Sequential(
         linearlayer,
-        To2D()
+        Flatten()
+    )
+
+    Y = torch.randint(high=model(X).shape[1], size=(X.shape[0],))
+
+    lossfunc = config.extend(torch.nn.CrossEntropyLoss())
+
+    return TestProblem(X, Y, model, lossfunc)
+
+def make_2layer_classification_problem():
+    model = torch.nn.Sequential(
+        linearlayer,
+        linearlayer2,
+        Flatten()
     )
 
     Y = torch.randint(high=model(X).shape[1], size=(X.shape[0],))
@@ -63,4 +74,5 @@ def make_classification_problem():
 TEST_PROBLEMS = {
     "linear-regression": make_regression_problem(),
     "linear-classification": make_classification_problem(),
+    "linear-2layer-classification": make_2layer_classification_problem(),
 }
