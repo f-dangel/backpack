63bb232a8b856353efa78e17a26547ccc6c9de92
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-06-29 14:34:45 +0200

    Refactor docstrings/variables

--- bpexts/gradient/batchgrad/conv2d.py
+++ bpexts/gradient/batchgrad/conv2d.py
@@ -3,10 +3,10 @@ from torch import einsum
 
 
 def grad_batch(module, grad_input, grad_output):
-    """Compute individual gradients for module parameters.
+    """Compute individual gradients of `torch.nn.Conv2d` parameters.
 
-    Store bias batch gradients in `module.bias.batch_grad` and
-    weight batch gradients in `module.weight.batch_grad`.
+    - Store bias batch gradients in `module.bias.batch_grad`
+    - Store kernel batch gradients in `module.weight.batch_grad`
     """
     if module.bias is not None and module.bias.requires_grad:
         module.bias.grad_batch = bias_grad_batch(module, grad_output)
@@ -17,10 +17,11 @@ def grad_batch(module, grad_input, grad_output):
 def bias_grad_batch(module, grad_output):
     """Compute bias batch gradients.
 
-    The batchwise gradient of a linear layer is simply given
-    by the gradient with respect to the layer's output, summed over
-    the spatial dimensions (each number in the bias vector is added.
-    to the spatial output of an entire channel).
+    - The batchwise gradient of a `torch.nn.Linear` layer is given
+      by the gradient with respect to the output, `grad_output`
+    - In `torch.nn.Conv2d` one bias element is added to all spatial locations
+      of an output channel. Consequently, `grad_output` has to be summed
+      over the spatial dimensions
     """
     return grad_output[0].sum(3).sum(2)
 
@@ -30,28 +31,29 @@ def unfold_func(module):
         kernel_size=module.kernel_size,
         dilation=module.dilation,
         padding=module.padding,
-        stride=module.stride
-    )
+        stride=module.stride)
 
 
 def weight_grad_batch(module, grad_output):
     """Compute weight batch gradients.
 
-    The linear layer applies
+    The convolution layer applies
+
     Y = W * X
+
     (neglecting the bias) to a single sample X, where W denotes the
     matrix view of the kernel, Y is a view of the output and X denotes
     the unfolded input matrix.
 
     Note on shapes/dims:
     --------------------
-    original input x: (batch_size, in_channels, x_dim, y_dim)
-    original kernel w: (out_channels, in_channels, k_x, k_y)
-    im2col input X: (batch_size, num_patches, in_channels * k_x * k_y)
-    kernel matrix W: (out_channels, in_channels *  k_x * k_y)
-    matmul result Y: (batch_size, out_channels, num_patches)
-                   = (batch_size, out_channels, x_out_dim * y_out_dim)
-    col2im output y: (batch_size, out_channels, x_out_dim, y_out_dim)
+    - original input x: (batch, in_channels, x_dim, y_dim)
+    - original kernel w: (out_channels, in_channels, k_x, k_y)
+    - im2col input X: (batch, num_patches, in_channels * k_x * k_y)
+    - kernel matrix W: (out_channels, in_channels *  k_x * k_y)
+    - matmul result Y: (batch, out_channels, num_patches)
+                   = (batch, out_channels, x_out_dim * y_out_dim)
+    - col2im output y: (batch, out_channels, x_out_dim, y_out_dim)
 
     Forward pass: (pseudo)
     -------------
@@ -62,22 +64,22 @@ def weight_grad_batch(module, grad_output):
 
     Backward pass: (pseudo)
     --------------
-    Given: dE/dy    (same shape as y)
-    dE/dY = view(dE/dy) (same shape as Y)
+    Given: dE / dy          (same shape as y)
+    dE / dY = view(dE / dy) (same shape as Y)
 
-    dE/dW[b,k,l]    (batch-wise gradient)
-    dE/dW[b,k,l] = (dY[b,i,j]/dW[k,l]) * dE/dY[b,i,j]
-                 = delta(i,k) * delta(m,l) * X[b,m,j] * dE/dY[b,i,j]
-                 = delta(m,l) * X[b,m,j] * dE/dY[b,k,j]
+    dE / dW[b,k,l]          (batch-wise gradient)
+    dE / dW[b,k,l] = (dY[b,i,j] / dW[k,l]) * dE / dY[b,i,j]
+                 = delta(i,k) * delta(m,l) * X[b,m,j] * dE / dY[b,i,j]
+                 = delta(m,l) * X[b,m,j] * dE / dY[b,k,j]
 
     Result:
     -------
-    dE/dw = view(dE/dW)
+    dE / dw = view(dE / dW)
     """
-    batch_size = grad_output[0].size(0)
-    dE_dw_shape = (batch_size, ) + module.weight.size()
+    batch = module.input0.size(0)
+    dE_dw_shape = (batch, ) + module.weight.size()
     X = unfold_func(module)(module.input0)
-    dE_dY = grad_output[0].view(batch_size, module.out_channels, -1)
+    dE_dY = grad_output[0].view(batch, module.out_channels, -1)
     dE_dW = einsum('bml,bkl->bmk', (dE_dY, X))
     return dE_dW.view(dE_dw_shape)
 
--- bpexts/gradient/batchgrad/linear.py
+++ bpexts/gradient/batchgrad/linear.py
@@ -3,10 +3,10 @@ from torch import einsum
 
 
 def grad_batch(module, grad_input, grad_output):
-    """Compute batchwise gradients of module parameters.
+    """Compute batchwise gradients of `torch.nn.Linear` parameters.
 
-    Store bias batch gradients in `module.bias.batch_grad` and
-    weight batch gradients in `module.weight.batch_grad`.
+    - Store bias batch gradients in `module.bias.batch_grad`
+    - Store weight batch gradients in `module.weight.batch_grad`.
     """
     if module.bias is not None and module.bias.requires_grad:
         module.bias.grad_batch = bias_grad_batch(module, grad_output)
@@ -15,16 +15,12 @@ def grad_batch(module, grad_input, grad_output):
 
 
 def bias_grad_batch(module, grad_output):
-    """Compute bias batch gradients from grad w.r.t. layer outputs.
-
-    The batchwise gradient of a linear layer is simply given
-    by the gradient with respect to the layer's output.
-    """
+    """Compute bias batch gradients."""
     return grad_output[0]
 
 
 def weight_grad_batch(module, grad_output):
-    r"""Compute weight batch gradients from grad w.r.t layer outputs.
+    r"""Compute weight batch gradients.
 
     The linear layer applies
     y = W x + b.
@@ -33,23 +29,24 @@ def weight_grad_batch(module, grad_output):
     Result:
     -------
     Finally, this yields
+
     dE / d vec(W) = (dy / d vec(W))^T dy  (vec denotes row stacking)
                   = dy \otimes x^T
 
-    Derivation:
-    -----------
+    Details:
+    --------
     The Jacobian for W is given by
     (matrix derivative notation)
-    dy / d vec(W) = x^T \otimes I    (vec denotes column stacking),
-    dy / d vec(W) = I \otimes x      (vec denotes row stacking),
+    dy / d vec(W) = x^T \otimes I      (vec denotes column stacking),
+    dy / d vec(W) = I \otimes x        (vec denotes row stacking),
     (dy / d vec(W))^T = I \otimes x^T  (vec denotes row stacking)
     or
     (index notation)
-    dy[i] / dW[j,k] = delta(i,j) x[k]    (index notation).
+    dy[i] / dW[j,k] = delta(i,j) x[k]
     """
-    batch_size = grad_output[0].size(0)
-    weight_grad_batch = einsum('bi,bj->bij', (grad_output[0], module.input0))
-    return weight_grad_batch.view(batch_size, module.out_features, module.in_features)
+    batch = module.input0.size(0)
+    w_grad_batch = einsum('bi,bj->bij', (grad_output[0], module.input0))
+    return w_grad_batch.view(batch, module.out_features, module.in_features)
 
 
 SIGNATURE = [(torch.nn.Linear, "BATCH_GRAD", grad_batch)]
