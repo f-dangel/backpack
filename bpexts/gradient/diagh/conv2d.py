060b62c03099f8b89ad2cf3603fec81a854a2afc
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-07-02 21:47:43 +0200

    (diagh) Conv2d layer prototype, fix some typos in Linear

--- bpexts/gradient/diagh/conv2d.py
+++ bpexts/gradient/diagh/conv2d.py
@@ -3,13 +3,14 @@ import torch.nn
 from ..config import CTX
 from ...utils import einsum
 from ..backpropextension import BackpropExtension
-from ..jmp.linear import jac_mat_prod
+from ..jmp.conv2d import jac_mat_prod
+from ..utils import unfold_func
 from ..extensions import DIAG_H
 
 
-class DiagHLinear(BackpropExtension):
+class DiagHConv2d(BackpropExtension):
     def __init__(self):
-        super().__init__(torch.nn.Linear, DIAG_H, req_inputs=[0])
+        super().__init__(torch.nn.Conv2d, DIAG_H, req_inputs=[0])
 
     def apply(self, module, grad_input, grad_output):
         sqrt_h_outs = CTX._backpropagated_sqrt_h
@@ -25,19 +26,44 @@ class DiagHLinear(BackpropExtension):
             backpropagate_sqrt_h(module, grad_input, grad_output, sqrt_h_outs,
                                  sqrt_h_outs_signs)
 
-    # TODO: Reuse code in ..diaggn.linear to extract the diagonal
+    # TODO: Reuse code in ..diaggn.conv2d to extract the diagonal
     def bias_diagH(self, module, sqrt_h_outs, sqrt_h_outs_signs):
         h_diag = torch.zeros_like(module.bias)
         for h_sqrt, sign in zip(sqrt_h_outs, sqrt_h_outs_signs):
-            h_diag.add_(sign * einsum('bic->i', (h_sqrt**2, )))
+            h_sqrt_view = self.separate_channels_and_pixels(module, h_sqrt)
+            h_diag.add_(sign * einsum('bijc,bikc->i',
+                                      (h_sqrt_view, h_sqrt_view)))
         return h_diag
 
-    # TODO: Reuse code in ..diaggn.linear to extract the diagonal
+    # TODO: Reuse code in ..diaggn.conv2d to extract the diagonal
     def weight_diagH(self, module, sqrt_h_outs, sqrt_h_outs_signs):
+        # unfolded input, repeated for each class
+        num_classes = sqrt_h_outs[0].size(2)
+        X = unfold_func(module)(module.input0).unsqueeze(0).expand(
+            num_classes, -1, -1, -1)
+
         h_diag = torch.zeros_like(module.weight)
         for h_sqrt, sign in zip(sqrt_h_outs, sqrt_h_outs_signs):
-            h_diag.add_(sign * einsum('bic->i', (h_sqrt**2, )))
+            h_sqrt_view = self.separate_channels_and_pixels(module, h_sqrt)
+            h_diag.add_(
+                einsum('bmlc,cbkl,bmic,cbki->mk',
+                       (q_sqrt_view, X, h_sqrt_view, X)).view_as(
+                           module.weight))
         return h_diag
+
+    # TODO: Reuse code in ..diaggn.conv2d
+    def separate_channels_and_pixels(self, module, sqrt_ggn_out):
+        """Reshape (batch, out_features, classes)
+        into
+                   (batch, out_channels, pixels, classes).
+        """
+        batch, channels, pixels, classes = (
+            module.input0.size(0),
+            module.out_channels,
+            module.output_shape[2] * module.output_shape[3],
+            sqrt_ggn_out.size(2),
+        )
+        return sqrt_ggn_out.view(batch, channels, pixels, classes)
 
     def backpropagate_sqrt_h(self, module, grad_input, grad_output,
                              sqrt_h_outs, sqrt_h_outs_signs):
@@ -47,4 +73,4 @@ class DiagHLinear(BackpropExtension):
         CTX._backpropagated_sqrt_h = sqrt_h_outs
 
 
-EXTENSIONS = [DiagHLinear()]
+EXTENSIONS = [DiagHConv2d()]
