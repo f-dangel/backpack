aa9777c3bc5acec1fb40994cf6af38a1478a4098
Frederik Kunstner <frederik.kunstner@gmail.com>
2019-07-03 11:01:36 +0200

    Forgot to add the hf change

--- bpexts/hessian/free.py
+++ bpexts/hessian/free.py
@@ -134,8 +134,13 @@ def hessian_vector_product(f, x, v):
 
 def ggn_vector_product(loss, output, model, vp):
     """Compute GGN-vector product G loss(x) * v."""
+    plist = list(model.parameters())
+    return ggn_vector_product_from_plist(loss, output, plist, vp)
 
-    Jv = R_op(output, list(model.parameters()), vp)
+
+def ggn_vector_product_from_plist(loss, output, plist, vp):
+
+    Jv = R_op(output, plist, vp)
     batch, dims = output.size(0), output.size(1)
     if loss.grad_fn.__class__.__name__ == 'NllLossBackward':
         outputsoftmax = torch.nn.functional.softmax(output, dim=1)
@@ -147,5 +152,5 @@ def ggn_vector_product(loss, output, model, vp):
         HJv = [torch.squeeze(H @ torch.unsqueeze(Jv[0], -1)) / batch]
     else:
         HJv = hessian_vector_product(loss, output, Jv)
-    JHJv = L_op(output, list(model.parameters()), HJv)
+    JHJv = L_op(output, plist, HJv)
     return JHJv
