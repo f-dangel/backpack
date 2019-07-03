3946c64cf8c6168fded1b27da05b7eefe8353c97
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-07-03 11:37:34 +0200

    Extend pooling tests to allow for multiple types

--- tests/gradient/test_problems_pooling.py
+++ tests/gradient/test_problems_pooling.py
@@ -12,25 +12,26 @@ TEST_SETTINGS = {
     "bias": True,
     "batch": 5,
     "rtol": 1e-5,
-    "atol": 5e-4
+    "atol": 5e-4,
+    "pool_size": 3,
 }
 
+POOLINGS = {
+    'MaxPool2d': torch.nn.MaxPool2d,
+    # 'AvgPool2d': torch.nn.AvgPool2d,
+}
 
 set_seeds(0)
 
 
 def convlayer():
-    return extend(torch.nn.Conv2d(
-        in_channels=TEST_SETTINGS["in_features"][0],
-        out_channels=TEST_SETTINGS["out_channels"],
-        kernel_size=TEST_SETTINGS["kernel_size"],
-        padding=TEST_SETTINGS["padding"],
-        bias=TEST_SETTINGS["bias"]
-    ))
-
-
-def pooling():
-    return extend(torch.nn.MaxPool2d(3))
+    return extend(
+        torch.nn.Conv2d(
+            in_channels=TEST_SETTINGS["in_features"][0],
+            out_channels=TEST_SETTINGS["out_channels"],
+            kernel_size=TEST_SETTINGS["kernel_size"],
+            padding=TEST_SETTINGS["padding"],
+            bias=TEST_SETTINGS["bias"]))
 
 
 input_size = (TEST_SETTINGS["batch"], ) + TEST_SETTINGS["in_features"]
@@ -38,19 +39,16 @@ X = torch.randn(size=input_size)
 
 
 def linearlayer():
-    return extend(torch.nn.Linear(
-        in_features=6,
-        out_features=1
-    ))
+    return extend(torch.nn.Linear(in_features=6, out_features=1))
 
 
-def make_regression_problem():
-    model = torch.nn.Sequential(
-        convlayer(),
-        pooling(),
-        Flatten(),
-        linearlayer()
-    )
+def pooling(pooling_cls):
+    return extend(pooling_cls(TEST_SETTINGS["pool_size"]))
+
+
+def make_regression_problem(pooling_cls):
+    model = torch.nn.Sequential(convlayer(), pooling(pooling_cls), Flatten(),
+                                linearlayer())
 
     Y = torch.randn(size=(model(X).shape[0], 1))
 
@@ -59,38 +57,32 @@ def make_regression_problem():
     return TestProblem(X, Y, model, lossfunc)
 
 
-def make_classification_problem():
-    model = torch.nn.Sequential(
-        convlayer(),
-        pooling(),
-        Flatten()
-    )
+def make_classification_problem(pooling_cls):
+    model = torch.nn.Sequential(convlayer(), pooling(pooling_cls), Flatten())
 
-    Y = torch.randint(high=X.shape[1], size=(model(X).shape[0],))
+    Y = torch.randint(high=X.shape[1], size=(model(X).shape[0], ))
 
     lossfunc = extend(torch.nn.CrossEntropyLoss())
 
     return TestProblem(X, Y, model, lossfunc)
 
 
-def make_2layer_classification_problem():
-    model = torch.nn.Sequential(
-        convlayer(),
-        pooling(),
-        convlayer(),
-        pooling(),
-        Flatten()
-    )
+def make_2layer_classification_problem(pooling_cls):
+    model = torch.nn.Sequential(convlayer(), pooling(pooling_cls), convlayer(),
+                                pooling(pooling_cls), Flatten())
 
-    Y = torch.randint(high=X.shape[1], size=(model(X).shape[0],))
+    Y = torch.randint(high=X.shape[1], size=(model(X).shape[0], ))
 
     lossfunc = extend(torch.nn.CrossEntropyLoss())
 
     return TestProblem(X, Y, model, lossfunc)
 
 
-TEST_PROBLEMS = {
-    "conv+pooling-regression": make_regression_problem(),
-    "conv+pooling-classification": make_classification_problem(),
-    "conv+pooling-classification-2layer": make_2layer_classification_problem(),
-}
+TEST_PROBLEMS = {}
+for pool_name, pool_cls in POOLINGS.items():
+    TEST_PROBLEMS["conv+{}-regression".format(
+        pool_name)] = make_regression_problem(pool_cls)
+    TEST_PROBLEMS["conv+{}-classification".format(
+        pool_name)] = make_classification_problem(pool_cls)
+    TEST_PROBLEMS["conv+{}-classification-2layer".format(
+        pool_name)] = make_2layer_classification_problem(pool_cls)
