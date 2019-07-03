0596d0bb735e0e7a77f5aa67ee37aeeba797caa8
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-07-03 10:38:22 +0200

    (diagh) Add ReLU

--- bpexts/gradient/diagh/__init__.py
+++ bpexts/gradient/diagh/__init__.py
@@ -1,3 +1,3 @@
-from . import conv2d, linear, crossentropyloss, mseloss
+from . import conv2d, linear, sigmoid, relu, crossentropyloss, mseloss
 
-EXTENSIONS = conv2d.EXTENSIONS + linear.EXTENSIONS + crossentropyloss.EXTENSIONS + mseloss.EXTENSIONS
+EXTENSIONS = conv2d.EXTENSIONS + linear.EXTENSIONS + crossentropyloss.EXTENSIONS + mseloss.EXTENSIONS + sigmoid.EXTENSIONS + relu.EXTENSIONS
--- bpexts/gradient/diagh/relu.py
+++ bpexts/gradient/diagh/relu.py
@@ -1,9 +1,11 @@
 import torch.nn
-from ..config import CTX
+from ..context import CTX
 from ...utils import einsum
 from ..backpropextension import BackpropExtension
 from ..jmp.relu import jac_mat_prod
 from ..extensions import DIAG_H
+
+DETACH_INPUTS = True
 
 
 class DiagHReLU(BackpropExtension):
@@ -14,9 +16,9 @@ class DiagHReLU(BackpropExtension):
         sqrt_h_outs = CTX._backpropagated_sqrt_h
         sqrt_h_outs_signs = CTX._backpropagated_sqrt_h_signs
 
-        if module.input0.requires_grad:
-            backpropagate_sqrt_h(module, grad_input, grad_output, sqrt_h_outs,
-                                 sqrt_h_outs_signs)
+        if module.input0.requires_grad or DETACH_INPUTS:
+            self.backpropagate_sqrt_h(module, grad_input, grad_output,
+                                      sqrt_h_outs, sqrt_h_outs_signs)
 
     def backpropagate_sqrt_h(self, module, grad_input, grad_output,
                              sqrt_h_outs, sqrt_h_outs_signs):
