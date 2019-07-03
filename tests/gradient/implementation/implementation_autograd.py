12b909800735a4af3da039c1d55f00c0391ae7e6
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-07-03 13:39:43 +0200

    (test) Add missing self in plist_like

--- tests/gradient/implementation/implementation_autograd.py
+++ tests/gradient/implementation/implementation_autograd.py
@@ -4,7 +4,6 @@ import bpexts.hessian.free as HF
 
 
 class AutogradImpl(Implementation):
-
     def gradient(self):
         return list(torch.autograd.grad(self.loss(), self.model.parameters()))
 
@@ -15,7 +14,8 @@ class AutogradImpl(Implementation):
         ]
 
         for b in range(self.N):
-            gradients = torch.autograd.grad(self.loss(b), self.model.parameters())
+            gradients = torch.autograd.grad(
+                self.loss(b), self.model.parameters())
             for idx, g in enumerate(gradients):
                 batch_grads[idx][b, :] = g.detach() / self.N
 
@@ -23,7 +23,8 @@ class AutogradImpl(Implementation):
 
     def batch_l2(self):
         batch_grad = self.batch_gradients()
-        batch_l2 = [(g**2).sum(list(range(1, len(g.shape)))) for g in batch_grad]
+        batch_l2 = [(g**2).sum(list(range(1, len(g.shape))))
+                    for g in batch_grad]
         return batch_l2
 
     def variance(self):
@@ -35,9 +36,10 @@ class AutogradImpl(Implementation):
         sgs = self.plist_like(self.model.parameters())
 
         for b in range(self.N):
-            gradients = torch.autograd.grad(self.loss(b), self.model.parameters())
+            gradients = torch.autograd.grad(
+                self.loss(b), self.model.parameters())
             for idx, g in enumerate(gradients):
-                sgs[idx] += (g.detach() / self.N) ** 2
+                sgs[idx] += (g.detach() / self.N)**2
 
         return sgs
 
@@ -58,7 +60,8 @@ class AutogradImpl(Implementation):
             diag_ggn_p = torch.zeros_like(p).view(-1)
 
             for parameter_index in range(p.numel()):
-                diag_value = extract_ith_element_of_diag_ggn(parameter_index, p)
+                diag_value = extract_ith_element_of_diag_ggn(
+                    parameter_index, p)
                 diag_ggn_p[parameter_index] = diag_value
 
             diag_ggns.append(diag_ggn_p.view(p.size()))
@@ -66,7 +69,8 @@ class AutogradImpl(Implementation):
         return diag_ggns
 
     def diag_h(self):
-        loss = self.problem.lossfunc(self.model(self.problem.X), self.problem.Y)
+        loss = self.problem.lossfunc(
+            self.model(self.problem.X), self.problem.Y)
 
         def extract_ith_element_of_diag_h(i, p):
             v = torch.zeros(p.numel()).to(self.device)
@@ -88,8 +92,5 @@ class AutogradImpl(Implementation):
 
         return diag_hs
 
-    def plist_like(plist):
-        return list([
-            torch.zeros(*p.size()).to(self.device)
-            for p in plist
-        ])
+    def plist_like(self, plist):
+        return list([torch.zeros(*p.size()).to(self.device) for p in plist])
