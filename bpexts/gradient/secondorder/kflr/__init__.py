6b7e0fdd966f63292e5c0fc46a124b8ef80dff77
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-07-05 20:41:04 +0200

    (KFLR) Add conv2d layer

--- bpexts/gradient/secondorder/kflr/__init__.py
+++ bpexts/gradient/secondorder/kflr/__init__.py
@@ -1,8 +1,9 @@
-from . import linear, activations, pooling, dropout
+from . import linear, activations, pooling, dropout, conv2d
 
 EXTENSIONS = [
     *linear.EXTENSIONS,
     *activations.EXTENSIONS,
     *pooling.EXTENSIONS,
     *dropout.EXTENSIONS,
+    *conv2d.EXTENSIONS,
 ]
