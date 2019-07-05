b5565ac70fc400502f1ff6657b5352bf44541864
Frederik Kunstner <frederik.kunstner@gmail.com>
2019-07-05 17:25:29 +0200

    [Refactor] Extract MaxPool2d as a Jacobian, use it in diagggn/maxpool2d

--- bpexts/gradient/diagggn/maxpool2d.py
+++ bpexts/gradient/diagggn/maxpool2d.py
@@ -1,15 +1,9 @@
-import torch.nn
-from ..jmp.maxpool2d import jac_mat_prod
+from ..jacobians.maxpool2d import MaxPool2dJacobian
 from .base import DiagGGNBase
 
 
-class DiagGGNMaxpool2d(DiagGGNBase):
-
-    def get_module(self):
-        return torch.nn.MaxPool2d
-
-    def jac_mat_prod(self, module, grad_input, grad_output, sqrt_ggn_out):
-        return jac_mat_prod(module, grad_input, grad_output, sqrt_ggn_out)
+class DiagGGNMaxpool2d(DiagGGNBase, MaxPool2dJacobian):
+    pass
 
 
 EXTENSIONS = [DiagGGNMaxpool2d()]
---
+++ bpexts/gradient/jacobians/maxpool2d.py
@@ -0,0 +1,64 @@
+from torch import zeros
+from torch.nn import MaxPool2d
+from torch.nn.functional import max_pool2d
+from .basejacobian import BaseJacobian
+
+
+class MaxPool2dJacobian(BaseJacobian):
+
+    def get_module(self):
+        return MaxPool2d
+
+    def jac_mat_prod(self, module, grad_input, grad_output, mat):
+        self.__check_sizes_input(mat, module)
+        mat_as_pool = self.__reshape_for_pooling(mat, module)
+        jmp_as_pool = self.__apply_jacobian_of(module, mat_as_pool)
+        return self.__reshape_for_matmul(jmp_as_pool, module)
+
+    def __check_sizes_input(self, mat, module):
+        batch, out_channels, out_x, out_y = module.output_shape
+        assert tuple(mat.size())[:2] == (batch, out_channels * out_x * out_y)
+
+    def __reshape_for_pooling(self, mat, module):
+        batch, channels, out_x, out_y = module.output_shape
+        _, _, in_x, in_y = module.input0.size()
+        num_classes = mat.size(-1)
+
+        batch, channels, out_x, out_y = module.output_shape
+        return mat.view(batch, channels, out_x * out_y, num_classes)
+
+    def __reshape_for_matmul(self, mat, module):
+        batch, channels, out_x, out_y = module.output_shape
+        _, _, in_x, in_y = module.input0.size()
+        in_features = module.input0.numel() / batch
+        batch = mat.size(0)
+        num_classes = mat.size(-1)
+
+        return mat.view(batch, in_features, num_classes)
+
+    def __apply_jacobian_of(self, module, mat):
+        batch, channels, out_x, out_y = module.output_shape
+        _, _, in_x, in_y = module.input0.size()
+        num_classes = mat.shape[-1]
+
+        result = zeros(
+            batch, channels, in_x * in_y, num_classes, device=mat.device
+        )
+
+        pool_idx = self.get_pooling_idx(module)
+        pool_idx = pool_idx.view(batch, channels, out_x * out_y)
+        pool_idx = pool_idx.unsqueeze(-1).expand(-1, -1, -1, num_classes)
+        result.scatter_add_(2, pool_idx, mat)
+        return result
+
+    def get_pooling_idx(self, module):
+        _, pool_idx = max_pool2d(
+            module.input0,
+            kernel_size=module.kernel_size,
+            stride=module.stride,
+            padding=module.padding,
+            dilation=module.dilation,
+            return_indices=True,
+            ceil_mode=module.ceil_mode
+        )
+        return pool_idx
