3e6087aad041813819324de80f69af511a075f3f
Frederik Kunstner <frederik.kunstner@gmail.com>
2019-07-05 15:41:44 +0200

    [Refactor] Move apply in base backpropextension

--- bpexts/gradient/backpropextension.py
+++ bpexts/gradient/backpropextension.py
@@ -2,12 +2,23 @@
 
 class BackpropExtension():
 
-    def __init__(self, module, extension, req_inputs=[], req_output=False):
+    def __init__(self, module, extension, params=[], req_inputs=[], req_output=False):
         # TODO: req_inputs and req_output are currently unused
         self.__module = module
         self.__extension = extension
         self.__req_inputs = req_inputs
         self.__req_output = req_output
+        self.params = params
+        self.savefield = extension.savefield
+
+        for param in self.params:
+            extFunc = getattr(self, param, None)
+            if extFunc is None:
+                raise ValueError(
+                    "Extension creation for " +
+                    "[{},{}] ".format(module, extension) +
+                    "failed: no function called {}".format(param)
+                )
 
     def __get_key(self):
         return tuple([self.__module, self.__extension])
@@ -16,4 +27,8 @@ class BackpropExtension():
         return self.__extension
 
     def apply(self, module, grad_input, grad_output):
-        raise NotImplementedError
+        for param in self.params:
+            if (getattr(module, param) is not None) and (getattr(module, param).requires_grad):
+                extFunc = getattr(self, param)
+                extValue = extFunc(module, grad_input, grad_output)
+                setattr(getattr(module, param), self.savefield, extValue)
