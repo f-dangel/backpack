47abf22919361d94ea28c3121bbb48ec23c0e556
Frederik Kunstner <frederik.kunstner@gmail.com>
2019-07-05 17:22:51 +0200

    [Refactor] Make diaggn/maxpool2d use the bpext apply function

--- bpexts/gradient/diagggn/maxpool2d.py
+++ bpexts/gradient/diagggn/maxpool2d.py
@@ -1,22 +1,15 @@
 import torch.nn
-from ..context import CTX
 from ..jmp.maxpool2d import jac_mat_prod
-from ..backpropextension import BackpropExtension
-from ..extensions import DIAG_GGN
+from .base import DiagGGNBase
 
 
-class DiagGGNMaxpool2d(BackpropExtension):
+class DiagGGNMaxpool2d(DiagGGNBase):
 
-    def __init__(self):
-        super().__init__(
-            torch.nn.MaxPool2d, DIAG_GGN,
-            req_inputs=[0], req_output=True
-        )
+    def get_module(self):
+        return torch.nn.MaxPool2d
 
-    def apply(self, module, grad_input, grad_output):
-        sqrt_ggn_out = CTX._backpropagated_sqrt_ggn
-        sqrt_ggn_in = jac_mat_prod(module, grad_input, grad_output, sqrt_ggn_out)
-        CTX._backpropagated_sqrt_ggn = sqrt_ggn_in
+    def jac_mat_prod(self, module, grad_input, grad_output, sqrt_ggn_out):
+        return jac_mat_prod(module, grad_input, grad_output, sqrt_ggn_out)
 
 
 EXTENSIONS = [DiagGGNMaxpool2d()]
