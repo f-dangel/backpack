b5f60e9529f624cfd25187e53260e53a7468e631
Frederik Kunstner <frederik.kunstner@gmail.com>
2019-07-05 17:08:09 +0200

    [Refactor] Make diagggn/conv use the bpext apply function

--- bpexts/gradient/diagggn/conv2d.py
+++ bpexts/gradient/diagggn/conv2d.py
@@ -10,27 +10,15 @@ from ..extensions import DIAG_GGN
 class DiagGGNConv2d(BackpropExtension):
 
     def __init__(self):
-        super().__init__(
-            torch.nn.Conv2d, DIAG_GGN,
-            req_inputs=[0], req_output=True
-        )
+        super().__init__(torch.nn.Conv2d, DIAG_GGN, params=["bias", "weight"])
 
-    def apply(self, module, grad_input, grad_output):
+    def bias(self, module, grad_output, sqrt_ggn_out):
         sqrt_ggn_out = CTX._backpropagated_sqrt_ggn
-
-        if module.bias is not None and module.bias.requires_grad:
-            module.bias.diag_ggn = self.bias_diag_ggn(module, grad_output, sqrt_ggn_out)
-        if module.weight.requires_grad:
-            module.weight.diag_ggn = self.weight_diag_ggn(module, grad_output,
-                                                          sqrt_ggn_out)
-
-        self.backpropagate_sqrt_ggn(module, grad_input, grad_output, sqrt_ggn_out)
-
-    def bias_diag_ggn(self, module, grad_output, sqrt_ggn_out):
         sqrt_ggn = convUtils.separate_channels_and_pixels(module, sqrt_ggn_out)
         return einsum('bijc,bikc->i', (sqrt_ggn, sqrt_ggn))
 
-    def weight_diag_ggn(self, module, grad_output, sqrt_ggn_out):
+    def weight(self, module, grad_output, sqrt_ggn_out):
+        sqrt_ggn_out = CTX._backpropagated_sqrt_ggn
         sqrt_ggn = convUtils.separate_channels_and_pixels(module, sqrt_ggn_out)
 
         # unfolded input, repeated for each class
@@ -41,7 +29,8 @@ class DiagGGNConv2d(BackpropExtension):
         return einsum('bmlc,cbkl,bmic,cbki->mk',
                       (sqrt_ggn, X, sqrt_ggn, X)).view_as(module.weight)
 
-    def backpropagate_sqrt_ggn(self, module, grad_input, grad_output, sqrt_ggn_out):
+    def backpropagate(self, module, grad_input, grad_output):
+        sqrt_ggn_out = CTX._backpropagated_sqrt_ggn
         sqrt_ggn_in = jac_mat_prod(module, grad_input, grad_output, sqrt_ggn_out)
         CTX._backpropagated_sqrt_ggn = sqrt_ggn_in
 
