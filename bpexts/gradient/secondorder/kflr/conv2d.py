9878245184a24c144c0691c0619f90932cfff156
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-07-06 15:26:31 +0200

    (KFLR) Remove CTX dependency

--- bpexts/gradient/secondorder/kflr/conv2d.py
+++ bpexts/gradient/secondorder/kflr/conv2d.py
@@ -5,7 +5,6 @@ approximation as in the KFAC paper for convolutions (KFC) to obtain curvature
 estimated for the convolution layer.
 """
 
-from ...context import CTX
 from ...utils import conv as convUtils
 from ...derivatives.conv2d import Conv2DDerivatives
 from ....utils import einsum
@@ -33,7 +32,7 @@ class KFLRConv2d(KFLRBase, Conv2DDerivatives):
         return einsum('bik,bjk->ij', (X_expanded, X_expanded)) / batch
 
     def Gamma(self, module, grad_input, grad_output):
-        kflr_sqrt_ggn_out = self.get_sqrt_matrix()
+        kflr_sqrt_ggn_out = self.get_from_ctx()
         kflr_sqrt_ggn = convUtils.separate_channels_and_pixels(
             module, kflr_sqrt_ggn_out)
         # NOTE: Normalization might be different from KFC
