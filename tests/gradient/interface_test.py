42c61cf8958e4dd32defb6420263a08adf0e79d9
Frederik Kunstner <frederik.kunstner@gmail.com>
2019-07-08 14:28:34 +0200

    Add conv interface test

--- tests/gradient/interface_test.py
+++ tests/gradient/interface_test.py
@@ -4,6 +4,9 @@ Test of the interface - calls every method that needs implementation
 
 import torch
 from torch.nn import Linear, ReLU, CrossEntropyLoss
+from torch.nn import Sequential
+from torch.nn import Conv2d
+from bpexts.gradient.layers import Flatten
 from bpexts.gradient import extend, bpexts
 import bpexts.gradient.extensions as ext
 
@@ -15,7 +18,7 @@ def dummy_forward_pass():
     D_OUT = 2
 
     X = torch.randn(N, D_IN)
-    Y = torch.randint(2, size=(N, ))
+    Y = torch.randint(2, size=(N,))
 
     lin1 = extend(Linear(in_features=D_IN, out_features=D_H, bias=True))
     act = extend(ReLU())
@@ -31,7 +34,23 @@ def dummy_forward_pass():
     return forward, (lin1.weight, lin2.weight), (lin1.bias, lin2.bias)
 
 
+def dummy_forward_pass_conv():
+    N, C, H, W = 2, 3, 4, 4
+    X = torch.randn(N, C, H, W)
+    Y = torch.randint(high=5, size=(N,))
+    conv = extend(Conv2d(3, 2, 2))
+    lin = extend(Linear(18, 5))
+    model = Sequential(conv, Flatten(), lin)
+    loss = extend(CrossEntropyLoss())
+
+    def forward():
+        return loss(model(X), Y)
+
+    return forward, (conv.weight, lin.weight), (conv.bias, lin.bias)
+
+
 forward_func, weights, bias = dummy_forward_pass()
+forward_func_conv, weights_conv, bias_conv = dummy_forward_pass_conv()
 
 FEATURES_TO_ATTRIBUTES = {
     ext.GRAD: "grad",
@@ -43,12 +62,17 @@ FEATURES_TO_ATTRIBUTES = {
 }
 
 
-def interface_test(feature, weight_has_attr=True, bias_has_attr=True):
+def interface_test(feature, weight_has_attr=True, bias_has_attr=True, use_conv=False):
+    if use_conv:
+        f, ws, bs = forward_func_conv, weights_conv, bias_conv
+    else:
+        f, ws, bs = forward_func, weights, bias
+
     with bpexts(feature):
-        forward_func().backward()
-    for w in weights:
+        f().backward()
+    for w in ws:
         assert weight_has_attr == hasattr(w, FEATURES_TO_ATTRIBUTES[feature])
-    for b in bias:
+    for b in bs:
         assert bias_has_attr == hasattr(b, FEATURES_TO_ATTRIBUTES[feature])
 
 
@@ -74,3 +98,27 @@ def test_interface_kflr():
 
 def test_interface_kfac():
     interface_test(ext.KFAC, weight_has_attr=True, bias_has_attr=False)
+
+
+def test_interface_grad_conv():
+    interface_test(ext.GRAD, use_conv=True)
+
+
+def test_interface_batch_grad_conv():
+    interface_test(ext.BATCH_GRAD, use_conv=True)
+
+
+def test_interface_sum_grad_squared_conv():
+    interface_test(ext.SUM_GRAD_SQUARED, use_conv=True)
+
+
+def test_interface_diag_ggn_conv():
+    interface_test(ext.DIAG_GGN, use_conv=True)
+
+
+def test_interface_kflr_conv():
+    interface_test(ext.KFLR, weight_has_attr=True, bias_has_attr=False, use_conv=True)
+
+
+def test_interface_kfac_conv():
+    interface_test(ext.KFAC, weight_has_attr=True, bias_has_attr=False, use_conv=True)
