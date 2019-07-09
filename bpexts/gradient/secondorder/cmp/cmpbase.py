4ee3348eaa582a4d17e688b31f76ddab53f82d1d
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-07-09 14:13:53 +0200

    (CMP) Incorporate residual term of the Hessian

--- bpexts/gradient/secondorder/cmp/cmpbase.py
+++ bpexts/gradient/secondorder/cmp/cmpbase.py
@@ -1,6 +1,7 @@
 from ...backpropextension import BackpropExtension
 from ...ctxinteract import ActOnCTX
 from ...extensions import CMP
+from ....utils import einsum
 
 BACKPROPAGATED_MP_NAME = "_cmp_backpropagated_mp"
 EXTENSION = CMP
@@ -20,15 +21,31 @@ class CMPBase(BackpropExtension, ActOnCTX):
     def backpropagate(self, module, grad_input, grad_output):
         CMP_out = self.get_from_ctx()
 
+        # second-order module effects
+        D = self._compute_residual_diag_if_nonzero(module, grad_input,
+                                                   grad_output)
+
         def CMP_in(mat):
-            print(mat.size())
             Jmat = self.jac_mat_prod(module, grad_input, grad_output, mat)
-            print(Jmat.size())
             CJmat = CMP_out(Jmat)
-            print(CJmat.size())
             JTCJmat = self.jac_t_mat_prod(module, grad_input, grad_output,
                                           CJmat)
-            print(JTCJmat.size())
+
+            if D is not None:
+                JTCJmat.add_(einsum('bi,bic->bic', (D, mat)))
+
             return JTCJmat
 
         self.set_in_ctx(CMP_in)
+
+    def _compute_residual_diag_if_nonzero(self, module, grad_input,
+                                          grad_output):
+        if self.hessian_is_zero():
+            return None
+
+        if not self.hessian_is_diagonal():
+            raise AttributeError(
+                "Residual terms are only supported for elementwise functions")
+
+        # second order module effects
+        return self.hessian_diagonal(module, grad_input, grad_output)
