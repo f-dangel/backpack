756a0f8650f007f64b560bb019c29705a3f2705d
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-07-11 13:16:57 +0200

    (Cleanup) Modify setup file and edit author list

--- setup.py
+++ setup.py
@@ -1,16 +1,39 @@
+from os import path
 from setuptools import setup
 
-# extract dependencies from 'requirements.txt' file
-with open('requirements.txt') as f:
+# META
+##############################################################################
+AUTHORS = "F. Dangel, F. Künstner"
+NAME = "BackPACK"
+# TODO: Change into backpack
+PACKAGES = ["bpexts"]
+
+DESCRIPTION = r"""BACKpropagation PACKage - A backpack for PyTorch that
+extends the backward pass of feedforward networks to compute quantities
+beyond the gradient.
+""".replace("\n", " ")
+
+VERSION = "0.1"
+# TODO: Change into backpack
+URL = "https://github.com/f-dangel/bpexts"
+LICENSE = "MIT"
+
+# DEPENDENCIES
+##############################################################################
+REQUIREMENTS_FILE = "requirements.txt"
+REQUIREMENTS_PATH = path.join(path.abspath(__file__), REQUIREMENTS_FILE)
+
+with open(REQUIREMENTS_FILE) as f:
     requirements = f.read().splitlines()
 
 setup(
-    author='F. Dangel',
-    name='bpexts',
-    version='0.2',
-    description='Extended backward pass for feedforward networks in PyTorch',
+    author=AUTHORS,
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
     install_requires=requirements,
-    url='https://github.com/f-dangel/bpexts',
-    license='MIT',
-    packages=['bpexts'],
-    zip_safe=False)
+    url=URL,
+    license=LICENSE,
+    packages=PACKAGES,
+    zip_safe=False,
+)
