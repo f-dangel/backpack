c422401addfedef28171d5cef3189ae7307f3af2
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-07-12 12:20:25 +0200

    (Refactor) Replace context getters and setters

--- backpack/matbackprop.py
+++ backpack/matbackprop.py
@@ -1,19 +1,41 @@
 from .backpropextension import BackpropExtension
 from .ctxinteract import ActOnCTX
+from .context import get_from_ctx, set_in_ctx
 
 
 class MatToJacMat(BackpropExtension, ActOnCTX):
     """Backpropagate `M` to `J^T M`."""
 
-    def __init__(self, ctx_name, extension, params=None):
+    def __init__(self, mat_name_in_ctx, extension, params=None):
         if params is None:
             params = []
-        ActOnCTX.__init__(self, ctx_name)
+        self.M_NAME_IN_CTX = mat_name_in_ctx
+        ActOnCTX.__init__(self, mat_name_in_ctx)
         BackpropExtension.__init__(
             self, self.get_module(), extension, params=params)
 
     def backpropagate(self, module, grad_input, grad_output):
-        sqrt_out = self.get_from_ctx()
-        sqrt_in = self.jac_t_mat_prod(module, grad_input, grad_output,
-                                      sqrt_out)
-        self.set_in_ctx(sqrt_in)
+        M = self.get_M_from_ctx()
+
+        if isinstance(M, list):
+            JT_M = self.apply_jac_t_on_list(module, grad_input, grad_output, M)
+        else:
+            JT_M = self.jac_t_mat_prod(module, grad_input, grad_output, M)
+
+        self.set_JT_M_in_ctx(JT_M)
+
+    def apply_jac_t(self, module, grad_input, grad_output, M):
+        return self.jac_t_mat_prod(module, grad_input, grad_output, M)
+
+    def apply_jac_t_on_list(self, module, grad_input, grad_output, M_list):
+        mat_list = [
+            self.apply_jac_t(module, grad_input, grad_output, M)
+            for M in M_list
+        ]
+        return M_list
+
+    def get_M_from_ctx(self):
+        return get_from_ctx(self.M_NAME_IN_CTX)
+
+    def set_JT_M_in_ctx(self, JT_M):
+        set_in_ctx(self.M_NAME_IN_CTX, JT_M)
