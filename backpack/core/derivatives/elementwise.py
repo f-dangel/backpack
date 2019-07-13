b2692e00e0ecbb477d6dca5856d8b732ae8c1fb1
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-07-13 16:45:26 +0200

    (EA) jac_t_mat_jac elementwise functions

--- backpack/core/derivatives/elementwise.py
+++ backpack/core/derivatives/elementwise.py
@@ -4,14 +4,23 @@ from ...utils.utils import einsum
 
 class ElementwiseDerivatives(BaseDerivatives):
     def jac_t_mat_prod(self, df, mat):
-        batch = df.size(0)
-        return einsum('bi,bic->bic', (df.view(batch, -1), mat))
+        _, df_flat = self.batch_flat(df)
+        return einsum('bi,bic->bic', (df_flat, mat))
 
     def jac_mat_prod(self, df, mat):
-        # the Jacobian is diagonal, so J^T = J
-        batch = df.size(0)
-        return einsum('bi,bic->bic', (df.view(batch, -1), mat))
+        _, df_flat = self.batch_flat(df)
+        return einsum('bi,bic->bic', (df_flat, mat))
 
     def hessian_diagonal(self, ddf, mat):
-        batch = mat.shape[0]
-        return ddf.view(batch, -1) * mat.view(batch, -1)
+        _, ddf_flat = self.batch_flat(ddf)
+        _, mat_flat = self.batch_flat(mat)
+        return ddf_flat * mat_flat
+
+    def ea_jac_t_mat_jac(self, df, mat):
+        batch, df_flat = self.batch_flat(df)
+        return einsum('bi,ij,bj->ij', (df_flat, mat, df_flat)) / batch
+
+    @staticmethod
+    def batch_flat(tensor):
+        batch = tensor.size(0)
+        return batch, tensor.view(batch, -1)
