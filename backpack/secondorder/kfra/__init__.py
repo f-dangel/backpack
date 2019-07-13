9ecd5254e6f9b0a7ebdc5b3cd38e232a3323f780
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-07-13 19:29:58 +0200

    (KFRA) No support for pooling, conv, padding

--- backpack/secondorder/kfra/__init__.py
+++ backpack/secondorder/kfra/__init__.py
@@ -1,19 +1,13 @@
 from . import (
     linear,
     activations,
-    # pooling,
     dropout,
-    # conv2d,
     losses,
-    # padding,
 )
 
 EXTENSIONS = [
     *linear.EXTENSIONS,
     *activations.EXTENSIONS,
-    # *pooling.EXTENSIONS,
     *dropout.EXTENSIONS,
-    # *conv2d.EXTENSIONS,
     *losses.EXTENSIONS,
-    # *padding.EXTENSIONS,
 ]
