ca57d7eba5e0d46653de3e282ddaaae8222ac755
Frederik Kunstner <frederik.kunstner@gmail.com>
2019-07-16 20:50:05 +0200

    Make automated tests conform to pass instances instead of classes

--- test/implementation/implementation_bpext.py
+++ test/implementation/implementation_bpext.py
@@ -10,37 +10,37 @@ class BpextImpl(Implementation):
         return list(torch.autograd.grad(self.loss(), self.model.parameters()))
 
     def batch_gradients(self):
-        with backpack(ext.BATCH_GRAD):
+        with backpack(ext.BATCH_GRAD()):
             self.loss().backward()
             batch_grads = [p.grad_batch for p in self.model.parameters()]
         return batch_grads
 
     def batch_l2(self):
-        with backpack(ext.BATCH_L2):
+        with backpack(ext.BATCH_L2()):
             self.loss().backward()
             batch_l2s = [p.batch_l2 for p in self.model.parameters()]
         return batch_l2s
 
     def variance(self):
-        with backpack(ext.VARIANCE):
+        with backpack(ext.VARIANCE()):
             self.loss().backward()
             variances = [p.variance for p in self.model.parameters()]
         return variances
 
     def sgs(self):
-        with backpack(ext.SUM_GRAD_SQUARED):
+        with backpack(ext.SUM_GRAD_SQUARED()):
             self.loss().backward()
             sgs = [p.sum_grad_squared for p in self.model.parameters()]
         return sgs
 
     def diag_ggn(self):
-        with backpack(ext.DIAG_GGN):
+        with backpack(ext.DIAG_GGN()):
             self.loss().backward()
             diag_ggn = [p.diag_ggn for p in self.model.parameters()]
         return diag_ggn
 
     def diag_h(self):
-        with backpack(ext.DIAG_H):
+        with backpack(ext.DIAG_H()):
             self.loss().backward()
             diag_h = [p.diag_h for p in self.model.parameters()]
         return diag_h
