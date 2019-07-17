a6bc182fbfe5e8dc485e4aed281c579353668e44
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-07-17 15:03:37 +0200

    (Test) Add tests for LinearConcat

--- test/layers_test.py
+++ test/layers_test.py
@@ -11,31 +11,27 @@ def data():
     Ds = [20, 10, 3]
 
     X = randn(N, Ds[0])
-    Y = randint(high=Ds[-1], size=(N,))
+    Y = randint(high=Ds[-1], size=(N, ))
 
     manual_seed(0)
     model1 = Sequential(
-        extend(Linear(Ds[0], Ds[1])),
-        extend(Linear(Ds[1], Ds[2]))
-    )
+        extend(Linear(Ds[0], Ds[1])), extend(Linear(Ds[1], Ds[2])))
 
     manual_seed(0)
     model2 = Sequential(
-        extend(LinearConcat(Ds[0], Ds[1])),
-        extend(LinearConcat(Ds[1], Ds[2]))
-    )
+        extend(LinearConcat(Ds[0], Ds[1])), extend(LinearConcat(Ds[1], Ds[2])))
 
     loss = CrossEntropyLoss()
 
     return X, Y, model1, model2, loss
 
 
-def test_LinearCat_forward():
+def test_LinearConcat_forward():
     X, Y, model1, model2, loss = data()
     assert allclose(model1(X), model2(X))
 
 
-def test_LinearCat_backward():
+def test_LinearConcat_backward():
     X, Y, model1, model2, loss = data()
 
     d1 = grad(loss(model1(X), Y), model1.parameters())
--- test/test_problems_linear.py
+++ test/test_problems_linear.py
@@ -1,5 +1,5 @@
 import torch
-from backpack.core.layers import Flatten
+from backpack.core.layers import Flatten, LinearConcat
 from backpack import extend
 from .test_problem import TestProblem
 
@@ -12,27 +12,45 @@ TEST_SETTINGS = {
     "atol": 1e-5
 }
 
-linearlayer = extend(
-    torch.nn.Linear(
-        in_features=TEST_SETTINGS["in_features"],
-        out_features=TEST_SETTINGS["out_features"],
-        bias=TEST_SETTINGS["bias"],
-    ))
-linearlayer2 = extend(
-    torch.nn.Linear(
-        in_features=TEST_SETTINGS["out_features"],
-        out_features=3,
-        bias=TEST_SETTINGS["bias"],
-    ))
-summationLinearLayer = extend(
-    torch.nn.Linear(
-        in_features=TEST_SETTINGS["out_features"], out_features=1, bias=True))
+LINEARS = {
+    'Linear': torch.nn.Linear,
+    'LinearConcat': LinearConcat,
+}
+
+
+def linearlayer(linear_cls):
+    return extend(
+        linear_cls(
+            in_features=TEST_SETTINGS["in_features"],
+            out_features=TEST_SETTINGS["out_features"],
+            bias=TEST_SETTINGS["bias"],
+        ))
+
+
+def linearlayer2(linear_cls):
+    return extend(
+        linear_cls(
+            in_features=TEST_SETTINGS["out_features"],
+            out_features=3,
+            bias=TEST_SETTINGS["bias"],
+        ))
+
+
+def summationLinearLayer(linear_cls):
+    return extend(
+        linear_cls(
+            in_features=TEST_SETTINGS["out_features"],
+            out_features=1,
+            bias=True))
+
+
 input_size = (TEST_SETTINGS["batch"], TEST_SETTINGS["in_features"])
 X = torch.randn(size=input_size)
 
 
-def make_regression_problem():
-    model = torch.nn.Sequential(linearlayer, summationLinearLayer)
+def make_regression_problem(linear_cls):
+    model = torch.nn.Sequential(
+        linearlayer(linear_cls), summationLinearLayer(linear_cls))
 
     Y = torch.randn(size=(model(X).shape[0], 1))
 
@@ -41,8 +59,8 @@ def make_regression_problem():
     return TestProblem(X, Y, model, lossfunc)
 
 
-def make_classification_problem():
-    model = torch.nn.Sequential(linearlayer, Flatten())
+def make_classification_problem(linear_cls):
+    model = torch.nn.Sequential(linearlayer(linear_cls), Flatten())
 
     Y = torch.randint(high=model(X).shape[1], size=(X.shape[0], ))
 
@@ -51,8 +69,9 @@ def make_classification_problem():
     return TestProblem(X, Y, model, lossfunc)
 
 
-def make_2layer_classification_problem():
-    model = torch.nn.Sequential(linearlayer, linearlayer2, Flatten())
+def make_2layer_classification_problem(linear_cls):
+    model = torch.nn.Sequential(
+        linearlayer(linear_cls), linearlayer2(linear_cls), Flatten())
 
     Y = torch.randint(high=model(X).shape[1], size=(X.shape[0], ))
 
@@ -61,8 +80,11 @@ def make_2layer_classification_problem():
     return TestProblem(X, Y, model, lossfunc)
 
 
-TEST_PROBLEMS = {
-    "linear-regression": make_regression_problem(),
-    "linear-classification": make_classification_problem(),
-    "linear-2layer-classification": make_2layer_classification_problem(),
-}
+TEST_PROBLEMS = {}
+for lin_name, lin_cls in LINEARS.items():
+    TEST_PROBLEMS["{}-regression".format(lin_name)] = make_regression_problem(
+        lin_cls)
+    TEST_PROBLEMS["{}-classification".format(
+        lin_name)] = make_classification_problem(lin_cls)
+    TEST_PROBLEMS["{}-2layer-classification".format(
+        lin_name)] = make_2layer_classification_problem(lin_cls)
