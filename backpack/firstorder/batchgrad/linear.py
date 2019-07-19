06ca91519497217945987ee90e3cc642c82f35e6
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-07-19 12:29:03 +0200

    (BatchGrad) Use Remove einsums from Linear(Concat)

--- backpack/firstorder/batchgrad/linear.py
+++ backpack/firstorder/batchgrad/linear.py
@@ -4,26 +4,46 @@ from ..firstorder import FirstOrderExtension
 from ...extensions import BATCH_GRAD
 from ...core.layers import LinearConcat
 
+from ...core.derivatives.linear import (LinearDerivatives,
+                                        LinearConcatDerivatives)
 
-class BatchGradLinear(FirstOrderExtension):
+
+class BatchGradLinear(FirstOrderExtension, LinearDerivatives):
     def __init__(self):
         super().__init__(
             torch.nn.Linear, BATCH_GRAD, params=["bias", "weight"])
 
     def bias(self, module, grad_input, grad_output):
-        return grad_output[0]
+        batch = grad_output[0].shape[0]
+        shape = (batch, *module.bias.shape)
+
+        weight_grad = self.bias_jac_t_mat_prod(
+            module, grad_input, grad_output, grad_output[0], sum_batch=False)
+
+        return weight_grad.view(shape)
 
     def weight(self, module, grad_input, grad_output):
-        return einsum('bi,bj->bij', (grad_output[0], module.input0))
+        batch = grad_output[0].shape[0]
+        shape = (batch, *module.weight.shape)
+
+        weight_grad = self.weight_jac_t_mat_prod(
+            module, grad_input, grad_output, grad_output[0], sum_batch=False)
+
+        return weight_grad.view(shape)
 
 
-class BatchGradLinearConcat(FirstOrderExtension):
+class BatchGradLinearConcat(FirstOrderExtension, LinearConcatDerivatives):
     def __init__(self):
         super().__init__(LinearConcat, BATCH_GRAD, params=["weight"])
 
     def weight(self, module, grad_input, grad_output):
-        input = module.homogeneous_input()
-        return einsum('bi,bj->bij', (grad_output[0], input))
+        batch = grad_output[0].shape[0]
+        shape = (batch, *module.weight.shape)
+
+        weight_grad = self.weight_jac_t_mat_prod(
+            module, grad_input, grad_output, grad_output[0], sum_batch=False)
+
+        return weight_grad.view(shape)
 
 
 EXTENSIONS = [BatchGradLinear(), BatchGradLinearConcat()]
