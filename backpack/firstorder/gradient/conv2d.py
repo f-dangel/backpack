fb81b552b3276140e1656ef6333dcabcb07d9db6
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-07-19 18:34:04 +0200

    (Refactor) Introduce base class for gradient and remove multiple inheritance

--- backpack/firstorder/gradient/conv2d.py
+++ backpack/firstorder/gradient/conv2d.py
@@ -3,50 +3,20 @@ from ...core.layers import Conv2dConcat
 from ...core.derivatives.conv2d import (Conv2DDerivatives,
                                         Conv2DConcatDerivatives)
 from ...extensions import GRAD
-from ..firstorder import FirstOrderExtension
+
+from .base import GradBase
 
 
-class GradConv2d(FirstOrderExtension, Conv2DDerivatives):
+class GradConv2d(GradBase):
     def __init__(self):
-        super().__init__(torch.nn.Conv2d, GRAD, params=["bias", "weight"])
-
-    # TODO: Same code as for batch gradient, but with sum_batch = True
-    def bias(self, module, grad_input, grad_output):
-        batch = grad_output[0].shape[0]
-        shape = module.bias.shape
-
-        grad_out_vec = grad_output[0].contiguous().view(batch, -1)
-
-        bias_grad = self.bias_jac_t_mat_prod(
-            module, grad_input, grad_output, grad_out_vec, sum_batch=True)
-
-        return bias_grad.view(shape)
-
-    # TODO: Same code as for batch gradient, but with sum_batch = True
-    def weight(self, module, grad_input, grad_output):
-        batch = grad_output[0].shape[0]
-        shape = module.weight.shape
-
-        grad_out_vec = grad_output[0].contiguous().view(batch, -1)
-
-        weight_grad = self.weight_jac_t_mat_prod(
-            module, grad_input, grad_output, grad_out_vec, sum_batch=True)
-
-        return weight_grad.view(shape)
+        super().__init__(
+            torch.nn.Conv2d,
+            GRAD,
+            Conv2DDerivatives(),
+            params=["bias", "weight"])
 
 
-class GradConv2dConcat(FirstOrderExtension, Conv2DConcatDerivatives):
+class GradConv2dConcat(GradBase):
     def __init__(self):
-        super().__init__(Conv2dConcat, GRAD, params=["weight"])
-
-    # TODO: Same code as for batch gradient, but with sum_batch = True
-    def weight(self, module, grad_input, grad_output):
-        batch = grad_output[0].shape[0]
-        shape = module.weight.shape
-
-        grad_out_vec = grad_output[0].contiguous().view(batch, -1)
-
-        weight_grad = self.weight_jac_t_mat_prod(
-            module, grad_input, grad_output, grad_out_vec, sum_batch=True)
-
-        return weight_grad.view(shape)
+        super().__init__(
+            Conv2dConcat, GRAD, Conv2DConcatDerivatives(), params=["weight"])
--- backpack/firstorder/gradient/linear.py
+++ backpack/firstorder/gradient/linear.py
@@ -2,43 +2,21 @@ import torch.nn
 from ...core.layers import LinearConcat
 from ...core.derivatives.linear import (LinearDerivatives,
                                         LinearConcatDerivatives)
-from ...utils.utils import einsum
 from ...extensions import GRAD
-from ..firstorder import FirstOrderExtension
+
+from .base import GradBase
 
 
-class GradLinear(FirstOrderExtension, LinearDerivatives):
+class GradLinear(GradBase):
     def __init__(self):
-        super().__init__(torch.nn.Linear, GRAD, params=["bias", "weight"])
-
-    # TODO: Same code as for batch gradient, but with sum_batch = True
-    def bias(self, module, grad_input, grad_output):
-        shape = module.bias.shape
-
-        bias_grad = self.bias_jac_t_mat_prod(
-            module, grad_input, grad_output, grad_output[0], sum_batch=True)
-
-        return bias_grad.view(shape)
-
-    # TODO: Same code as for batch gradient, but with sum_batch = True
-    def weight(self, module, grad_input, grad_output):
-        shape = module.weight.shape
-
-        weight_grad = self.weight_jac_t_mat_prod(
-            module, grad_input, grad_output, grad_output[0], sum_batch=True)
-
-        return weight_grad.view(shape)
+        super().__init__(
+            torch.nn.Linear,
+            GRAD,
+            LinearDerivatives(),
+            params=["bias", "weight"])
 
 
-class GradLinearConcat(FirstOrderExtension, LinearConcatDerivatives):
+class GradLinearConcat(GradBase):
     def __init__(self):
-        super().__init__(LinearConcat, GRAD, params=["weight"])
-
-    # TODO: Same code as for batch gradient, but with sum_batch = True
-    def weight(self, module, grad_input, grad_output):
-        shape = module.weight.shape
-
-        weight_grad = self.weight_jac_t_mat_prod(
-            module, grad_input, grad_output, grad_output[0], sum_batch=True)
-
-        return weight_grad.view(shape)
+        super().__init__(
+            LinearConcat, GRAD, LinearConcatDerivatives(), params=["weight"])
