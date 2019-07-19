e1446da6acc93d14dfedcc03329bba2020c911fe
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-07-19 12:26:29 +0200

    (core) Reuse stuff from Linear in LinearConcat, add sum_batch parameter

--- backpack/core/derivatives/linear.py
+++ backpack/core/derivatives/linear.py
@@ -4,82 +4,108 @@ from torch.nn import Linear
 from .basederivatives import BaseDerivatives
 from ..layers import LinearConcat
 
+from .utils import unsqueeze_if_missing_dim
+
 
 class LinearDerivatives(BaseDerivatives):
     def get_module(self):
         return Linear
 
+    def get_input(self, module):
+        return module.input0
+
+    def get_batch(self, module):
+        return module.input0.size(0)
+
     def hessian_is_zero(self):
         return True
 
+    def get_weight_data(self, module):
+        return module.weight.data
+
     def jac_t_mat_prod(self, module, grad_input, grad_output, mat):
-        d_linear = module.weight.data
+        d_linear = self.get_weight_data(module)
         return einsum('ij,bic->bjc', (d_linear, mat))
 
     def jac_mat_prod(self, module, grad_input, grad_output, mat):
-        d_linear = module.weight.data
+        d_linear = self.get_weight_data(module)
         return einsum('ij,bjc->bic', (d_linear, mat))
 
     def ea_jac_t_mat_jac_prod(self, module, grad_input, grad_output, mat):
-        jac = module.weight
+        jac = self.get_weight_data(module)
         return einsum('ik,ij,jl->kl', (jac, mat, jac))
 
     def weight_jac_mat_prod(self, module, grad_input, grad_output, mat):
-        batch = module.input0.size(0)
+        batch = self.get_batch(module)
         num_cols = mat.size(1)
         shape = tuple(module.weight.size()) + (num_cols, )
 
-        jac_mat = einsum('bj,ijc->bic', (module.input0, mat.view(shape)))
+        jac_mat = einsum('bj,ijc->bic',
+                         (self.get_input(module), mat.view(shape)))
         return jac_mat
 
-    def weight_jac_t_mat_prod(self, module, grad_input, grad_output, mat):
-        batch = module.input0.size(0)
+    @unsqueeze_if_missing_dim(mat_dim=3)
+    def weight_jac_t_mat_prod(self,
+                              module,
+                              grad_input,
+                              grad_output,
+                              mat,
+                              sum_batch=True):
+        batch = self.get_batch(module)
         num_cols = mat.size(2)
 
-        jac_t_mat = einsum('bjc,bi->jic', (mat, module.input0)).contiguous()
-        return jac_t_mat.view(module.weight.numel(), num_cols)
+        equation = 'bjc,bi->jic' if sum_batch is True else 'bjc,bi->bjic'
+
+        jac_t_mat = einsum(equation,
+                           (mat, self.get_input(module))).contiguous()
+
+        sum_shape = [module.weight.numel(), num_cols]
+        shape = sum_shape if sum_batch is True else [batch] + sum_shape
+
+        return jac_t_mat.view(*shape)
 
     def bias_jac_mat_prod(self, module, grad_input, grad_output, mat):
-        batch = module.input0.size(0)
+        batch = self.get_batch(module)
         return mat.unsqueeze(0).expand(batch, -1, -1)
 
-    def bias_jac_t_mat_prod(self, module, grad_input, grad_output, mat):
-        return mat.sum(0)
+    @unsqueeze_if_missing_dim(mat_dim=3)
+    def bias_jac_t_mat_prod(self,
+                            module,
+                            grad_input,
+                            grad_output,
+                            mat,
+                            sum_batch=True):
+        if sum_batch is True:
+            return mat.sum(0)
+        else:
+            return mat
 
 
-class LinearConcatDerivatives(BaseDerivatives):
+class LinearConcatDerivatives(LinearDerivatives):
+    # override
     def get_module(self):
         return LinearConcat
 
-    def hessian_is_zero(self):
-        return True
+    # override
+    def get_input(self, module):
+        """Return homogeneous input, if bias exists """
+        input = super().get_input(module)
+        if module.has_bias():
+            return module.append_ones(input)
+        else:
+            return input
 
-    def jac_t_mat_prod(self, module, grad_input, grad_output, mat):
-        d_linear = module._slice_weight().data
-        return einsum('ij,bic->bjc', (d_linear, mat))
+    # override
+    def get_weight_data(self, module):
+        return module._slice_weight().data
 
-    def jac_mat_prod(self, module, grad_input, grad_output, mat):
-        d_linear = module._slice_weight().data
-        return einsum('ij,bjc->bic', (d_linear, mat))
+    def bias_jac_mat_prod(self, module, grad_input, grad_output, mat):
+        raise RuntimeError("Bias is concatenated with weight matrix")
 
-    def ea_jac_t_mat_jac_prod(self, module, grad_input, grad_output, mat):
-        jac = module._slice_weight().data
-        return einsum('ik,ij,jl->kl', (jac, mat, jac))
-
-    def weight_jac_mat_prod(self, module, grad_input, grad_output, mat):
-        input = module.homogeneous_input()
-
-        num_cols = mat.size(1)
-        shape = tuple(module.weight.size()) + (num_cols, )
-
-        jac_mat = einsum('bj,ijc->bic', (input, mat.view(shape)))
-        return jac_mat
-
-    def weight_jac_t_mat_prod(self, module, grad_input, grad_output, mat):
-        input = module.homogeneous_input()
-
-        batch = module.input0.size(0)
-        num_cols = mat.size(2)
-
-        jac_t_mat = einsum('bjc,bi->jic', (mat, input)).contiguous()
-        return jac_t_mat.view(module.weight.numel(), num_cols)
+    def bias_jac_t_mat_prod(self,
+                            module,
+                            grad_input,
+                            grad_output,
+                            mat,
+                            sum_batch=True):
+        raise RuntimeError("Bias is concatenated with weight matrix")
