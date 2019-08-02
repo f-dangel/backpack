fd948f8511e7fd62cf1126c7cbbe2d3820aad36f
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-08-02 12:59:03 +0200

    (CMP) Remove keyword argument in bias CMP

--- backpack/extensions/curvmatprod/linear.py
+++ backpack/extensions/curvmatprod/linear.py
@@ -1,25 +1,23 @@
+from backpack.core.derivatives.linear import (LinearConcatDerivatives,
+                                              LinearDerivatives)
+
 from .cmpbase import CMPBase
-from backpack.core.derivatives.linear import LinearDerivatives, LinearConcatDerivatives
 
 
 class CMPLinear(CMPBase):
     def __init__(self):
-        super().__init__(
-            derivatives=LinearDerivatives(),
-            params=["weight", "bias"]
-        )
+        super().__init__(derivatives=LinearDerivatives(),
+                         params=["weight", "bias"])
 
     def weight(self, ext, module, g_inp, g_out, backproped):
         CMP_out = backproped
 
         def weight_cmp(mat):
             Jmat = self.derivatives.weight_jac_mat_prod(
-                module, g_inp, g_out, mat
-            )
+                module, g_inp, g_out, mat)
             CJmat = CMP_out(Jmat)
             JTCJmat = self.derivatives.weight_jac_t_mat_prod(
-                module, g_inp, g_out, CJmat
-            )
+                module, g_inp, g_out, CJmat)
             return JTCJmat
 
         return weight_cmp
@@ -27,13 +25,12 @@ class CMPLinear(CMPBase):
     def bias(self, ext, module, g_inp, g_out, backproped):
         CMP_out = backproped
 
-        def bias_cmp(mat, which=None):
-            Jmat = self.derivatives.bias_jac_mat_prod(
-                module, g_inp, g_out, mat)
+        def bias_cmp(mat):
+            Jmat = self.derivatives.bias_jac_mat_prod(module, g_inp, g_out,
+                                                      mat)
             CJmat = CMP_out(Jmat)
             JTCJmat = self.derivatives.bias_jac_t_mat_prod(
-                module, g_inp, g_out, CJmat
-            )
+                module, g_inp, g_out, CJmat)
             return JTCJmat
 
         return bias_cmp
@@ -41,23 +38,18 @@ class CMPLinear(CMPBase):
 
 class CMPLinearConcat(CMPBase):
     def __init__(self):
-        super().__init__(
-            derivatives=LinearConcatDerivatives(),
-            params=["weight"]
-        )
+        super().__init__(derivatives=LinearConcatDerivatives(),
+                         params=["weight"])
 
     def weight(self, ext, module, g_inp, g_out, backproped):
         CMP_out = backproped
 
         def weight_cmp(mat):
             Jmat = self.derivatives.weight_jac_mat_prod(
-                module, g_inp, g_out, mat
-            )
+                module, g_inp, g_out, mat)
             CJmat = CMP_out(Jmat)
             JTCJmat = self.derivatives.weight_jac_t_mat_prod(
-                module, g_inp, g_out, CJmat
-            )
+                module, g_inp, g_out, CJmat)
             return JTCJmat
 
         return weight_cmp
-
