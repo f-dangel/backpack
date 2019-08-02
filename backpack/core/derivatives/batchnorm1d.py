08203c9051a09a3264bb042b226da34cc7d778c4
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-08-02 13:01:21 +0200

    Remove debugging code in BatchNorm derivatives

--- backpack/core/derivatives/batchnorm1d.py
+++ backpack/core/derivatives/batchnorm1d.py
@@ -45,23 +45,12 @@ class BatchNorm1dDerivatives(BaseDerivatives):
         x_hat, var = self.get_normalized_input_and_var(module)
         ivar = 1. / (var + module.eps).sqrt()
 
-        # TODO: Remove DEBUG once the jac_mat is implemented and therefore
-        #       this method is tested within curvmatprod
-        # check if jac_t(g_out) is equal to g_inp
-        DEBUG = False
-        mat_for_jac = g_out[0].unsqueeze(-1) if DEBUG else mat
-
-        dx_hat = einsum('bic,i->bic', (mat_for_jac, module.weight))
+        dx_hat = einsum('bic,i->bic', (mat, module.weight))
 
         jac_t_mat = batch * dx_hat
         jac_t_mat -= dx_hat.sum(0).unsqueeze(0).expand_as(jac_t_mat)
         jac_t_mat -= einsum('bi,sic,si->bic', (x_hat, dx_hat, x_hat))
         jac_t_mat = einsum('bic,i->bic', (jac_t_mat, ivar / batch))
-
-        if DEBUG is True:
-            assert torch.allclose(jac_t_mat, g_inp[0].unsqueeze(-1))
-            print("[DEBUG] Batch Norm:\tjac_t_mat(grad_out) == grad_in")
-            raise Exception
 
         return jac_t_mat
 
@@ -69,8 +58,6 @@ class BatchNorm1dDerivatives(BaseDerivatives):
         input = self.get_input(module)
         mean = input.mean(dim=0)
         var = input.var(dim=0, unbiased=False)
-        # var2 = ((input - input.mean(0))**2).mean(0)
-        # assert torch.allclose(var, var2)
         return (input - mean) / (var + module.eps).sqrt(), var
 
     @jmp_unsqueeze_if_missing_dim(mat_dim=2)
