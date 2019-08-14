6f560cf1e1b924d9a78367b061e0bf50c22cd20f
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-08-14 10:15:13 +0200

    Add extensions DiagGGNExact, DiagGGNMC

--- backpack/extensions/secondorder/__init__.py
+++ backpack/extensions/secondorder/__init__.py
@@ -8,12 +8,14 @@ Those extension make it easier to extract structural or local approximations
 to second-order information, such as
 - `DiagHessian`: The diagonal of the Hessian.
 - `DiagGGN`: The diagonal of the Generalized Gauss-Newton
-  (or Fisher information matrix).
+  (or Fisher information matrix), supports exact computation or sampling.
+  - `DiagGGNExact`: Exact diagonal of the GGN
+  - `DiagGGNMC`: MC-sampled diagonal of the GGN/Fisher
 - `KFAC`, `KFRA`, `KFLR`: Kronecker Block-Diagonal approximations of the
   Generalized Gauss-Newton (or Fisher information matrix).
 - `HBP`: A general framework that encompasses KFAC, KFRA and KFLR.
 """
 
-from .diag_ggn import DiagGGN
+from .diag_ggn import DiagGGN, DiagGGNExact, DiagGGNMC
 from .diag_hessian import DiagHessian
-from .hbp import HBP, KFAC, KFRA, KFLR
+from .hbp import HBP, KFAC, KFLR, KFRA
--- backpack/extensions/secondorder/diag_ggn/__init__.py
+++ backpack/extensions/secondorder/diag_ggn/__init__.py
@@ -1,44 +1,65 @@
+from torch.nn import (AvgPool2d, Conv2d, CrossEntropyLoss, Dropout, Linear,
+                      MaxPool2d, MSELoss, ReLU, Sigmoid, Tanh, ZeroPad2d)
+
+from backpack.core.layers import Conv2dConcat, Flatten, LinearConcat
 from backpack.extensions.backprop_extension import BackpropExtension
-from backpack.core.layers import Conv2dConcat, LinearConcat, Flatten
 from backpack.extensions.secondorder.hbp import LossHessianStrategy
-from torch.nn import Linear, Conv2d, Dropout, MaxPool2d, Tanh, Sigmoid, ReLU, CrossEntropyLoss, MSELoss, AvgPool2d, \
-    ZeroPad2d
-from . import pooling, conv2d, linear, activations, losses, padding, dropout, flatten
+
+from . import (activations, conv2d, dropout, flatten, linear, losses, padding,
+               pooling)
 
 
 class DiagGGN(BackpropExtension):
     VALID_LOSS_HESSIAN_STRATEGIES = [
-        LossHessianStrategy.EXACT,
-        LossHessianStrategy.SAMPLING
+        LossHessianStrategy.EXACT, LossHessianStrategy.SAMPLING
     ]
 
-    def __init__(self, loss_hessian_strategy=LossHessianStrategy.EXACT):
+    def __init__(self,
+                 loss_hessian_strategy=LossHessianStrategy.EXACT,
+                 savefield=None):
+        if savefield is None:
+            savefield = "diag_ggn"
         if loss_hessian_strategy not in self.VALID_LOSS_HESSIAN_STRATEGIES:
             raise ValueError(
                 "Unknown hessian strategy: {}".format(loss_hessian_strategy) +
                 "Valid strategies: [{}]".format(
-                    self.VALID_LOSS_HESSIAN_STRATEGIES
-                )
-            )
+                    self.VALID_LOSS_HESSIAN_STRATEGIES))
 
         self.loss_hessian_strategy = loss_hessian_strategy
-        super().__init__(
-            savefield="diag_ggn",
-            fail_mode="ERROR",
-            module_exts={
-                MSELoss: losses.DiagGGNMSELoss(),
-                CrossEntropyLoss: losses.DiagGGNCrossEntropyLoss(),
-                Linear: linear.DiagGGNLinear(),
-                LinearConcat: linear.DiagGGNLinearConcat(),
-                MaxPool2d: pooling.DiagGGNMaxPool2d(),
-                AvgPool2d: pooling.DiagGGNAvgPool2d(),
-                ZeroPad2d: padding.DiagGGNZeroPad2d(),
-                Conv2d: conv2d.DiagGGNConv2d(),
-                Conv2dConcat: conv2d.DiagGGNConv2dConcat(),
-                Dropout: dropout.DiagGGNDropout(),
-                Flatten: flatten.DiagGGNFlatten(),
-                ReLU: activations.DiagGGNReLU(),
-                Sigmoid: activations.DiagGGNSigmoid(),
-                Tanh: activations.DiagGGNTanh(),
-            }
-        )
+        super().__init__(savefield=savefield,
+                         fail_mode="ERROR",
+                         module_exts={
+                             MSELoss: losses.DiagGGNMSELoss(),
+                             CrossEntropyLoss:
+                             losses.DiagGGNCrossEntropyLoss(),
+                             Linear: linear.DiagGGNLinear(),
+                             LinearConcat: linear.DiagGGNLinearConcat(),
+                             MaxPool2d: pooling.DiagGGNMaxPool2d(),
+                             AvgPool2d: pooling.DiagGGNAvgPool2d(),
+                             ZeroPad2d: padding.DiagGGNZeroPad2d(),
+                             Conv2d: conv2d.DiagGGNConv2d(),
+                             Conv2dConcat: conv2d.DiagGGNConv2dConcat(),
+                             Dropout: dropout.DiagGGNDropout(),
+                             Flatten: flatten.DiagGGNFlatten(),
+                             ReLU: activations.DiagGGNReLU(),
+                             Sigmoid: activations.DiagGGNSigmoid(),
+                             Tanh: activations.DiagGGNTanh(),
+                         })
+
+
+class DiagGGNExact(DiagGGN):
+    """Exact diagonal of the GGN"""
+    def __init__(self, savefield=None):
+        if savefield is None:
+            savefield = "diag_ggn_exact"
+        super().__init__(loss_hessian_strategy=LossHessianStrategy.EXACT,
+                         savefield=savefield)
+
+
+class DiagGGNMC(DiagGGN):
+    """MC-sampled diagonal of the GGN/Fisher."""
+    def __init__(self, savefield=None):
+        if savefield is None:
+            savefield = "diag_ggn_mc"
+        super().__init__(loss_hessian_strategy=LossHessianStrategy.SAMPLING,
+                         savefield=savefield)
