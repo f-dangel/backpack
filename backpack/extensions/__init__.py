29201a652f1aa5b6521a774c32c785b14381e5e6
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-08-14 10:45:05 +0200

    Fix missing import of DiagGGNs

--- backpack/extensions/__init__.py
+++ backpack/extensions/__init__.py
@@ -2,6 +2,7 @@
 BackPACK Extensions
 """
 
-from .firstorder import BatchL2Grad, BatchGrad, SumGradSquared, Variance
-from .secondorder import DiagGGN, DiagHessian, HBP, KFAC, KFRA, KFLR
 from .curvmatprod import CMP
+from .firstorder import BatchGrad, BatchL2Grad, SumGradSquared, Variance
+from .secondorder import (HBP, KFAC, KFLR, KFRA, DiagGGN, DiagGGNExact,
+                          DiagGGNMC, DiagHessian)
