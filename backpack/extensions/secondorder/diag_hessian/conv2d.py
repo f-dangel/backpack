[Bugfix + PerfImprov] Fix the sign issue in DiagH and use improved diagonal extraction code

backpack/extensions/secondorder/diag_hessian/conv2d.py
@@ -10,7 +10,6 @@ class DiagHConv2d(DiagHBaseModule):
     def __init__(self):
         super().__init__(derivatives=Conv2DDerivatives(), params=["bias", "weight"])
 
-    # TODO: Reuse code in ..diaggn.conv2d to extract the diagonal
     def bias(self, ext, module, g_inp, g_out, backproped):
         sqrt_h_outs = backproped["matrices"]
         sqrt_h_outs_signs = backproped["signs"]
@@ -23,22 +22,15 @@ class DiagHConv2d(DiagHBaseModule):
                                       (h_sqrt_view, h_sqrt_view)))
         return h_diag
 
-    # TODO: Reuse code in ..diaggn.conv2d to extract the diagonal
     def weight(self, ext, module, g_inp, g_out, backproped):
         sqrt_h_outs = backproped["matrices"]
         sqrt_h_outs_signs = backproped["signs"]
-        X = convUtils.unfold_func(module)(module.input0).unsqueeze(0)
+        X = convUtils.unfold_func(module)(module.input0)
         h_diag = torch.zeros_like(module.weight)
 
         for h_sqrt, sign in zip(sqrt_h_outs, sqrt_h_outs_signs):
-            num_classes = h_sqrt.size(2)
-            X_repeated = X.expand(num_classes, -1, -1, -1)
-            h_sqrt_view = convUtils.separate_channels_and_pixels(
-                module, h_sqrt)
-            h_diag.add_(
-                einsum('bmlc,cbkl,bmic,cbki->mk',
-                       (h_sqrt_view, X_repeated, h_sqrt_view,
-                        X_repeated)).view_as(module.weight))
+            h_diag_curr = convUtils.extract_weight_diagonal(module, X, h_sqrt)
+            h_diag.add_(sign * h_diag_curr.view_as(module.weight))
         return h_diag
 
 
@@ -46,7 +38,6 @@ class DiagHConv2dConcat(DiagHBaseModule):
     def __init__(self):
         super().__init__(derivatives=Conv2DConcatDerivatives(), params=["weight"])
 
-    # TODO: Reuse code in ..diaggn.conv2d to extract the diagonal
     def weight(self, ext, module, g_inp, g_out, backproped):
         sqrt_h_outs = backproped["matrices"]
         sqrt_h_outs_signs = backproped["signs"]
@@ -55,17 +46,9 @@ class DiagHConv2dConcat(DiagHBaseModule):
         if module.has_bias():
             X = module.append_ones(X)
 
-        X = X.unsqueeze(0)
-
         h_diag = torch.zeros_like(module.weight)
 
         for h_sqrt, sign in zip(sqrt_h_outs, sqrt_h_outs_signs):
-            num_classes = h_sqrt.size(2)
-            X_repeated = X.expand(num_classes, -1, -1, -1)
-            h_sqrt_view = convUtils.separate_channels_and_pixels(
-                module, h_sqrt)
-            h_diag.add_(
-                einsum('bmlc,cbkl,bmic,cbki->mk',
-                       (h_sqrt_view, X_repeated, h_sqrt_view,
-                        X_repeated)).view_as(module.weight))
+            h_diag_curr = convUtils.extract_weight_diagonal(module, X, h_sqrt)
+            h_diag.add_(sign * h_diag_curr.view_as(module.weight))
         return h_diag

backpack/utils/conv.py
@@ -1,4 +1,5 @@
 from torch.nn import Unfold
+from backpack.utils.utils import einsum
 
 
 def unfold_func(module):
@@ -6,7 +7,6 @@ def unfold_func(module):
                   dilation=module.dilation,
                   padding=module.padding,
                   stride=module.stride)
-
 
 def get_weight_gradient_factors(input, grad_out, module):
     batch = input.size(0)
@@ -52,3 +52,12 @@ def check_sizes_output_jac(jmp, module):
             "Size after conv does not match", "Got {}, and {}.",
             "Expected all dimensions to match, except for the first.".format(
                 jmp.size(), module.output_shape))
+
+def extract_weight_diagonal(module, input, grad_output):
+    """
+    input must be the unfolded input to the convolution (see unfold_func)
+    and grad_output the backpropagated gradient
+    """
+    grad_output_viewed = separate_channels_and_pixels(module, grad_output)
+    AX = einsum('bkl,bmlc->cbkm', (input, grad_output_viewed))
+    return (AX ** 2).sum([0, 1]).transpose(0, 1)