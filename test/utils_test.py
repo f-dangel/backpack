d68ac4e5dae1d14c64b899f79d6fe62706ce9a01
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-11-22 11:58:02 +0100

    Refactor matrix_from_kron_facs test using unittest

--- test/utils_test.py
+++ test/utils_test.py
@@ -1,6 +1,7 @@
 """Test of Kronecker utilities."""
 
 import random
+import unittest
 
 import torch
 
@@ -8,76 +9,76 @@ import scipy.linalg
 from backpack.extensions.secondorder import utils as bp_utils
 
 
-# HELPERS
-##############################################################################
-def scipy_matrix_from_two_kron_facs(A, B):
-    return torch.from_numpy(scipy.linalg.kron(A.numpy(), B.numpy()))
+class KroneckerUtilsTest(unittest.TestCase):
+    RUNS = 100
 
+    # Precision of results
+    ATOL = 1e-6
+    RTOL = 1e-5
 
-def scipy_matrix_from_kron_facs(factors):
-    mat = None
-    for factor in factors:
-        if mat is None:
-            assert bp_utils.is_matrix(factor)
-            mat = factor
-        else:
-            mat = scipy_matrix_from_two_kron_facs(mat, factor)
+    # Restriction of dimension and number of factors
+    MIN_DIM = 1
+    MAX_DIM = 5
+    MIN_FACS = 1
+    MAX_FACS = 3
 
-    return mat
+    # HELPERS
+    ##########################################################################
+    def allclose(self, tensor1, tensor2):
+        return torch.allclose(tensor1, tensor2, rtol=self.RTOL, atol=self.ATOL)
 
+    def make_random_kfacs(self, num_facs=None):
+        def random_kfac():
+            def random_dim():
+                return random.randint(self.MIN_DIM, self.MAX_DIM)
 
-def random_matrix(min_dim=1, max_dim=10):
-    def random_int():
-        return random.randint(min_dim, max_dim)
-
-    shape = (random_int(), random_int())
-    return torch.rand(shape)
-
-
-##############################################################################
-ATOL = 1e-6
-RTOL = 1e-5
-
-
-def test_matrix_from_two_kron_facs(random_runs=100, min_dim=1, max_dim=10):
-    """Check matrix from two Kronecker factors with `scipy`."""
-
-    def make_factors():
-        A = random_matrix(min_dim=min_dim, max_dim=max_dim)
-        B = random_matrix(min_dim=min_dim, max_dim=max_dim)
-        return A, B
-
-    for _ in range(random_runs):
-        A, B = make_factors()
-
-        bp_result = bp_utils.matrix_from_two_kron_facs(A, B)
-        sp_result = scipy_matrix_from_two_kron_facs(A, B)
-
-        assert torch.allclose(bp_result, sp_result, atol=ATOL, rtol=RTOL)
-
-
-def test_matrix_from_kron_facs(
-    runs=100, min_num=1, max_num=3, min_dim=1, max_dim=10,
-):
-    """Check matrix from list of Kronecker factors with `scipy`."""
-
-    def make_factors():
-        factors = []
-        num_factors = random.randint(min_num, max_num)
-
-        def random_dim():
-            return random.randint(min_dim, max_dim)
-
-        for _ in range(num_factors):
             shape = (random_dim(), random_dim())
-            factors.append(torch.rand(shape))
+            return torch.rand(shape)
 
-        return factors
+        def random_num_facs():
+            return random.randint(self.MIN_FACS, self.MAX_FACS)
 
-    for _ in range(runs):
-        factors = make_factors()
+        num_facs = num_facs if num_facs is not None else random_num_facs()
+        return [random_kfac() for _ in range(num_facs)]
 
-        bp_result = bp_utils.matrix_from_kron_facs(factors)
-        sp_result = scipy_matrix_from_kron_facs(factors)
+    # SCIPY implementations
+    ##########################################################################
 
-        assert torch.allclose(bp_result, sp_result, atol=ATOL, rtol=RTOL)
+    def scipy_matrix_from_two_kron_facs(self, A, B):
+        return torch.from_numpy(scipy.linalg.kron(A.numpy(), B.numpy()))
+
+    def scipy_matrix_from_kron_facs(self, factors):
+        mat = None
+        for factor in factors:
+            if mat is None:
+                assert bp_utils.is_matrix(factor)
+                mat = factor
+            else:
+                mat = self.scipy_matrix_from_two_kron_facs(mat, factor)
+
+        return mat
+
+    # TESTS
+    ##########################################################################
+
+    def test_matrix_from_two_kron_facs(self):
+        """Check matrix from two Kronecker factors with `scipy`."""
+        NUM_FACS = 2
+
+        for _ in range(self.RUNS):
+            A, B = self.make_random_kfacs(NUM_FACS)
+
+            bp_result = bp_utils.matrix_from_two_kron_facs(A, B)
+            sp_result = self.scipy_matrix_from_two_kron_facs(A, B)
+
+            assert self.allclose(bp_result, sp_result)
+
+    def test_matrix_from_kron_facs(self):
+        """Check matrix from list of Kronecker factors with `scipy`."""
+        for _ in range(self.RUNS):
+            factors = self.make_random_kfacs()
+
+            bp_result = bp_utils.matrix_from_kron_facs(factors)
+            sp_result = self.scipy_matrix_from_kron_facs(factors)
+
+            assert self.allclose(bp_result, sp_result)
