c040136e81a253f8316067eba3c26d23f26d88da
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-12-27 15:47:03 +0100

    Print only shape, not data

--- examples/example_grad.py
+++ examples/example_grad.py
@@ -7,7 +7,7 @@ from utils import load_mnist_data
 B = 64
 X, y = load_mnist_data(B)
 
-print(f"# COMPUTE THE GRADIENT WITH PYTORCH (B={B})")
+print(f"# Gradient with PyTorch (B={B})")
 
 model = Sequential(
     Flatten(),
@@ -22,5 +22,5 @@ for name, param in model.named_parameters():
     print(
         f"\n{name}:",
         f"\n\t.grad.shape: {param.grad.shape}",
-        f"\n\t.grad:       {param.grad}",
+        # f"\n\t.grad:       {param.grad}",
     )
