aa9f1d7c0828c86915c20de38949fb82a78b0bd6
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-12-27 16:42:13 +0100

    Add example: Exact GGN diagonal with BackPACK

---
+++ examples/example_diag_ggn_exact.py
@@ -0,0 +1,36 @@
+"""
+Compute the gradient with PyTorch and the exact GGN diagonal with BackPACK.
+"""
+
+from torch.nn import CrossEntropyLoss, Flatten, Linear, Sequential
+
+from backpack import backpack, extend, extensions
+from utils import load_mnist_data
+
+B = 64
+X, y = load_mnist_data(B)
+
+print(f"# Gradient with PyTorch, exact GGN diagonal with BackPACK (B={B})")
+
+model = Sequential(
+    Flatten(),
+    Linear(784, 10),
+)
+lossfunc = CrossEntropyLoss()
+
+model = extend(model)
+lossfunc = extend(lossfunc)
+
+loss = lossfunc(model(X), y)
+
+with backpack(extensions.DiagGGNExact()):
+    loss.backward()
+
+for name, param in model.named_parameters():
+    print(
+        f"\n{name}:",
+        f"\n\t.grad.shape:           {param.grad.shape}",
+        f"\n\t.diag_ggn_exact.shape: {param.diag_ggn_exact.shape}",
+        # f"\n\t.grad:                 {param.grad}",
+        # f"\n\t.diag_ggn_exact:       {param.diag_ggn_exact}",
+    )
