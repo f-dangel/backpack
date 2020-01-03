b4222821e7b3f9c38ab040c7943df3e6bb2afa96
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2020-01-03 17:13:45 +0100

    Add example: Full GGN computation with GGN-vector-products

---
+++ examples/example_ggn_matrix.py
@@ -0,0 +1,40 @@
+"""
+Compute the full GGN matrix with automatic differentiation.
+"""
+import torch
+from torch.nn import CrossEntropyLoss, Flatten, Linear, Sequential
+from torch.nn.utils.convert_parameters import parameters_to_vector
+
+from backpack.hessianfree.ggnvp import ggn_vector_product
+from backpack.utils.convert_parameters import vector_to_parameter_list
+from utils import load_mnist_data
+
+B = 64
+X, y = load_mnist_data(B)
+
+print("# GGN matrix with automatic differentiation | B =", B)
+
+model = Sequential(Flatten(), Linear(784, 10),)
+lossfunc = CrossEntropyLoss()
+
+output = model(X)
+loss = lossfunc(output, y)
+
+num_params = sum(p.numel() for p in model.parameters())
+ggn = torch.zeros(num_params, num_params)
+
+for i in range(num_params):
+    # GGN-vector product with i.th unit vector yields the i.th row
+    e_i = torch.zeros(num_params)
+    e_i[i] = 1.0
+
+    # convert to model parameter shapes
+    e_i_list = vector_to_parameter_list(e_i, model.parameters())
+    ggn_i_list = ggn_vector_product(loss, output, model, e_i_list)
+
+    ggn_i = parameters_to_vector(ggn_i_list)
+    ggn[i, :] = ggn_i
+
+print("Model parameters: ", num_params)
+print("GGN shape:        ", ggn.shape)
+print("GGN:              ", ggn)
