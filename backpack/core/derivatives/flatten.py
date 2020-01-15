5c0a506ed356bc6b9550eaf990dc6031b0d375c7
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2020-01-15 13:34:46 +0100

    [New index convention] Make Flatten jac_t work

--- backpack/core/derivatives/flatten.py
+++ backpack/core/derivatives/flatten.py
@@ -1,5 +1,8 @@
 from torch.nn import Flatten
 
+from backpack.core.derivatives.utils import jac_t_new_shape_convention
+from backpack.utils.unsqueeze import jmp_unsqueeze_if_missing_dim
+
 from .basederivatives import BaseDerivatives
 
 
@@ -13,8 +16,15 @@ class FlattenDerivatives(BaseDerivatives):
     def ea_jac_t_mat_jac_prod(self, module, g_inp, g_out, mat):
         return mat
 
+    @jmp_unsqueeze_if_missing_dim(mat_dim=3)
+    @jac_t_new_shape_convention
     def jac_t_mat_prod(self, module, g_inp, g_out, mat):
-        return mat
+        new_convention = True
+        if new_convention:
+            return mat
+        else:
+            return mat
 
+    @jmp_unsqueeze_if_missing_dim(mat_dim=3)
     def jac_mat_prod(self, module, g_inp, g_out, mat):
         return mat
