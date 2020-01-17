80911ff73174bf7ea8d7174cedc14e92db2a1a0a
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2020-01-17 19:53:52 +0100

    Introduce eingroup for grouping dimensions

--- backpack/utils/einsum.py
+++ backpack/utils/einsum.py
@@ -6,6 +6,7 @@ Makes it easy to switch to opt_einsum rather than torch's einsum for tests.
 
 import opt_einsum as oe
 import torch
+import numpy
 
 TORCH = "torch"
 OPT_EINSUM = "opt_einsum"
@@ -34,3 +35,28 @@ def einsum(equation, *operands):
     See `backpack.utils.utils.EINSUMS` for supported implementations.
     """
     return EINSUMS[BPEXTS_EINSUM](equation, *operands)
+
+
+def eingroup(equation, operand):
+    """Use einsum notation for grouping dimensions.
+
+    Idea:
+    -----
+    * 'a,b,c->ab,c' to group a and b dimension
+    * 'a,b,c->ba,c' to transpose, then group b and a dimension
+    """
+    split, sep = "->", ","
+
+    in_string = equation.split(split)[0].replace(sep, "")
+    in_shapes = {axis: operand.shape[idx] for idx, axis in enumerate(in_string)}
+
+    out_groups = equation.split(split)[1].split(sep)
+    out_shape = []
+    for group in out_groups:
+        out_shape.append(numpy.prod([in_shapes[axis] for axis in group]))
+
+    einsum_eq = equation.replace(sep, "")
+    print(out_shape)
+    print(einsum_eq)
+
+    return einsum(einsum_eq, operand).view(out_shape)
