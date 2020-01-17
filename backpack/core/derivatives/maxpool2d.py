1abdfbf02fc5d25fd9b274f75f5a9370ba565f01
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2020-01-17 23:35:49 +0100

    [Refactor] Clean MaxPool2dDerivatives

--- backpack/core/derivatives/maxpool2d.py
+++ backpack/core/derivatives/maxpool2d.py
@@ -1,4 +1,4 @@
-from torch import prod, zeros
+from torch import zeros
 from torch.nn import MaxPool2d
 from torch.nn.functional import max_pool2d
 
@@ -7,16 +7,17 @@ from backpack.core.derivatives.utils import (
     jac_mat_prod_accept_vectors,
 )
 
-from backpack.utils import conv as convUtils
 from backpack.core.derivatives.basederivatives import BaseDerivatives
+
+from backpack.utils.einsum import eingroup
 
 
 class MaxPool2DDerivatives(BaseDerivatives):
     def get_module(self):
         return MaxPool2d
 
+    # TODO: Do not recompute but get from forward pass of module
     def get_pooling_idx(self, module):
-        # TODO: Do not recompute but get from forward pass of module
         _, pool_idx = max_pool2d(
             module.input0,
             kernel_size=module.kernel_size,
@@ -35,142 +36,64 @@ class MaxPool2DDerivatives(BaseDerivatives):
               in terms of the approximation and memory costs.
         """
         device = mat.device
-        batch, channels, in_x, in_y = module.input0.size()
-        in_features = channels * in_x * in_y
-        _, _, out_x, out_y = module.output.size()
-        out_features = channels * out_x * out_y
+        N, channels, H_in, W_in = module.input0.size()
+        in_features = channels * H_in * W_in
+        _, _, H_out, W_out = module.output.size()
+        out_features = channels * H_out * W_out
 
-        pool_idx = self.get_pooling_idx(module).view(batch, channels, out_x * out_y)
+        pool_idx = self.get_pooling_idx(module).view(N, channels, H_out * W_out)
         result = zeros(in_features, in_features, device=device)
 
-        for b in range(batch):
+        for b in range(N):
             idx = pool_idx[b, :, :]
             temp = zeros(in_features, out_features, device=device)
             temp.scatter_add_(1, idx, mat)
             result.scatter_add_(0, idx.t(), temp)
-        return result / batch
+        return result / N
 
     def hessian_is_zero(self):
         return True
 
-    # Jacobian-matrix product
     @jac_mat_prod_accept_vectors
     def jac_mat_prod(self, module, g_inp, g_out, mat):
-        new_convention = True
+        mat_as_pool = eingroup("v,n,c,h,w->v,n,c,hw", mat)
+        jmp_as_pool = self.__apply_jacobian_of(module, mat_as_pool)
+        return self.view_like_output(jmp_as_pool, module)
 
-        convUtils.check_sizes_input_jac(mat, module, new_convention=new_convention)
-        mat_as_pool = self.__reshape_for_pooling_in(
-            mat, module, new_convention=new_convention
-        )
-        jmp_as_pool = self.__apply_jacobian_of(
-            module, mat_as_pool, new_convention=new_convention
-        )
-        return self.__reshape_for_matmul(
-            jmp_as_pool, module, new_convention=new_convention
-        )
+    def __apply_jacobian_of(self, module, mat):
+        V, HW_axis = mat.shape[0], 3
+        pool_idx = self.__pool_idx_for_jac(module, V)
+        return mat.gather(HW_axis, pool_idx)
 
-    def __reshape_for_pooling_in(self, mat, module, new_convention=False):
-        batch, channels, in_x, in_y = module.input0.size()
-
-        if new_convention:
-            num_classes = mat.size(0)
-            shape = (num_classes, batch, channels, in_x * in_y)
-        else:
-            num_classes = mat.size(-1)
-            shape = (batch, channels, in_x * in_y, num_classes)
-        return mat.view(shape)
-
-    def __reshape_for_matmul(self, mat, module, new_convention=False):
-        if new_convention:
-            num_columns = mat.size(0)
-            shape = (num_columns,) + tuple(module.output_shape)
-        else:
-            batch = module.output_shape[0]
-            out_features = prod(module.output_shape) / batch
-            num_classes = mat.size(-1)
-            shape = (batch, out_features, num_classes)
-        return mat.view(shape)
-
-    def __apply_jacobian_of(self, module, mat, new_convention=False):
-        batch, channels, out_x, out_y = module.output_shape
-
-        if new_convention:
-            num_classes = mat.shape[0]
-        else:
-            num_classes = mat.shape[-1]
+    def __pool_idx_for_jac(self, module, V):
+        """Manipulated pooling indices ready-to-use in jac(t)."""
 
         pool_idx = self.get_pooling_idx(module)
+        V_axis = 0
+        return (
+            eingroup("n,c,h,w->n,c,hw", pool_idx)
+            .unsqueeze(V_axis)
+            .expand(V, -1, -1, -1)
+        )
 
-        pool_idx = pool_idx.view(batch, channels, out_x * out_y)
-
-        if new_convention:
-            pool_idx = pool_idx.unsqueeze(0).expand(num_classes, -1, -1, -1)
-            return mat.gather(3, pool_idx)
-        else:
-            pool_idx = pool_idx.unsqueeze(-1).expand(-1, -1, -1, num_classes)
-            return mat.gather(2, pool_idx)
-
-    # Transposed Jacobian-matrix product
     @jac_t_mat_prod_accept_vectors
     def jac_t_mat_prod(self, module, g_inp, g_out, mat):
-        new_convention = True
-        convUtils.check_sizes_input_jac_t(mat, module, new_convention=new_convention)
-        mat_as_pool = self.__reshape_for_pooling_out(
-            mat, module, new_convention=new_convention
-        )
-        jmp_as_pool = self.__apply_jacobian_t_of(
-            module, mat_as_pool, new_convention=new_convention
-        )
-        return self.__reshape_for_matmul_t(
-            jmp_as_pool, module, new_convention=new_convention
-        )
+        mat_as_pool = eingroup("v,n,c,h,w->v,n,c,hw", mat)
+        jmp_as_pool = self.__apply_jacobian_t_of(module, mat_as_pool)
+        return self.view_like_input(jmp_as_pool, module)
 
-    def __reshape_for_pooling_out(self, mat, module, new_convention=False):
-        batch, channels, out_x, out_y = module.output_shape
+    def __apply_jacobian_t_of(self, module, mat):
+        V = mat.shape[0]
+        result = self.__zero_for_jac_t(module, V, mat.device)
+        pool_idx = self.__pool_idx_for_jac(module, V)
 
-        if new_convention:
-            num_classes = mat.size(0)
-            shape = (num_classes, batch, channels, out_x * out_y)
-        else:
-            num_classes = mat.size(-1)
-            shape = (batch, channels, out_x * out_y, num_classes)
-        return mat.view(shape)
-
-    def __reshape_for_matmul_t(self, mat, module, new_convention=False):
-        batch = module.output_shape[0]
-
-        if new_convention:
-            in_features_shape = module.input0_shape[1:]
-            num_classes = mat.size(0)
-            shape = (num_classes, batch) + tuple(in_features_shape)
-        else:
-            in_features = module.input0.numel() / batch
-            num_classes = mat.size(-1)
-            shape = (batch, in_features, num_classes)
-
-        return mat.view(shape)
-
-    def __apply_jacobian_t_of(self, module, mat, new_convention=False):
-        batch, channels, out_x, out_y = module.output_shape
-        _, _, in_x, in_y = module.input0.size()
-
-        if new_convention:
-            num_classes = mat.shape[0]
-            shape = (num_classes, batch, channels, in_x * in_y)
-        else:
-            num_classes = mat.shape[-1]
-            shape = (batch, channels, in_x * in_y, num_classes)
-
-        result = zeros(shape, device=mat.device)
-
-        pool_idx = self.get_pooling_idx(module)
-        pool_idx = pool_idx.view(batch, channels, out_x * out_y)
-
-        if new_convention:
-            pool_idx = pool_idx.unsqueeze(0).expand(num_classes, -1, -1, -1)
-            result.scatter_add_(3, pool_idx, mat)
-        else:
-            pool_idx = pool_idx.unsqueeze(-1).expand(-1, -1, -1, num_classes)
-            result.scatter_add_(2, pool_idx, mat)
-
+        HW_axis = 3
+        result.scatter_add_(HW_axis, pool_idx, mat)
         return result
+
+    def __zero_for_jac_t(self, module, V, device):
+        N, C_out, _, _ = module.output_shape
+        _, _, H_in, W_in = module.input0.size()
+
+        shape = (V, N, C_out, H_in * W_in)
+        return zeros(shape, device=device)
