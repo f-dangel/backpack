08bf9d33c61097ca2741e4b3235cce767d3f19ce
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2020-01-17 18:15:31 +0100

    [New index convention] Add documentation for new conventions

--- backpack/core/derivatives/basederivatives.py
+++ backpack/core/derivatives/basederivatives.py
@@ -1,11 +1,73 @@
 class BaseDerivatives:
+    """First- and second-order partial derivatives of a module.
+
+    Shape conventions:
+    ------------------
+    * Batch size: N
+    * Free dimension for vectorization: V
+
+    For vector-processing layers (2d input):
+    * input [N, C_in],  output [N, C_out]
+
+    For image-processing layers (4d input)
+    * Input/output channels: C_in/C_out
+    * Input/output height: H_in/H_out
+    * Input/output width: W_in/W_out
+    * input [N, C_in, H_in, W_in],  output [N, C_out, H_in, W_in]
+
+
+    Definitions:
+    ------------
+    * The Jacobian J is defined as
+        J[n, c, w, ..., ̃n, ̃c, ̃w, ...]
+        = 𝜕output[n, c, w, ...] / 𝜕input[̃n, ̃c, ̃w, ...]
+    * The transposed Jacobian Jᵀ is defined as
+        Jᵀ[̃n, ̃c, ̃w, ..., n, c, w, ...]
+        = 𝜕output[n, c, w, ...] / 𝜕input[̃n, ̃c, ̃w, ...]
+    """
 
     MC_SAMPLES = 1
 
     def jac_mat_prod(self, module, g_inp, g_out, mat):
+        """Apply Jacobian of the output w.r.t. input to a matrix.
+
+        Implicit application of J:
+            result[v, n, c, w, ...]
+            =  ∑_{̃n, ̃c, ̃w} J[n, c, w, ..., ̃n, ̃c, ̃w, ...] mat[̃n, ̃c, ̃w, ...].
+        Parameters:
+        -----------
+        mat: torch.Tensor
+            Matrix the transposed Jacobian will be applied to.
+            Must have shape [V, N, C_in, H_in, ...].
+
+        Returns:
+        --------
+        result: torch.Tensor
+            Transposed-Jacobian-matrix product.
+            Has shape [V, N, C_out, H_out, ...].
+        """
         raise NotImplementedError
 
     def jac_t_mat_prod(self, module, g_inp, g_out, mat):
+        """Apply transposed Jacobian of module output w.r.t. input to a matrix.
+
+        Implicit application of Jᵀ:
+            result[v, ̃n, ̃c, ̃w, ...]
+            = ∑_{n, c, w} Jᵀ[̃n, ̃c, ̃w, ..., n, c, w, ...] mat[v, n, c, w, ...].
+
+        Parameters:
+        -----------
+        mat: torch.Tensor
+            Matrix the Jacobian will be applied to.
+            Must have shape [V, N, C_out, H_out, ...].
+
+        Returns:
+        --------
+        result: torch.Tensor
+            Jacobian-matrix product.
+            Has shape [V, N, C_in, H_in, ...].
+        """
+
         raise NotImplementedError
 
     def ea_jac_t_mat_jac_prod(self, module, g_inp, g_out, mat):
