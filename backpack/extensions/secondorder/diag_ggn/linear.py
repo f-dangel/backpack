503c772300a5aa13661416f111b86d15d829bfd4
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2020-01-20 14:32:08 +0100

    [Refactor] Pull out diagonal extractors for Linear

--- backpack/extensions/secondorder/diag_ggn/linear.py
+++ backpack/extensions/secondorder/diag_ggn/linear.py
@@ -1,6 +1,6 @@
 from backpack.core.derivatives.linear import LinearDerivatives
-from backpack.utils.einsum import einsum
 from backpack.extensions.secondorder.diag_ggn.diag_ggn_base import DiagGGNBaseModule
+import backpack.utils.linear as LinUtils
 
 
 class DiagGGNLinear(DiagGGNBaseModule):
@@ -8,7 +8,7 @@ class DiagGGNLinear(DiagGGNBaseModule):
         super().__init__(derivatives=LinearDerivatives(), params=["bias", "weight"])
 
     def bias(self, ext, module, grad_inp, grad_out, backproped):
-        return einsum("vno->o", backproped ** 2)
+        return LinUtils.extract_bias_diagonal(module, backproped)
 
     def weight(self, ext, module, grad_inp, grad_out, backproped):
-        return einsum("vno,ni->oi", (backproped ** 2, module.input0 ** 2))
+        return LinUtils.extract_weight_diagonal(module, backproped)
--- backpack/extensions/secondorder/diag_hessian/linear.py
+++ backpack/extensions/secondorder/diag_hessian/linear.py
@@ -1,34 +1,30 @@
 import torch
-import torch.nn
+import backpack.utils.linear as LinUtils
 
 from backpack.core.derivatives.linear import LinearDerivatives
-from backpack.utils.einsum import einsum
-
-from .diag_h_base import DiagHBaseModule
+from backpack.extensions.secondorder.diag_hessian.diag_h_base import DiagHBaseModule
 
 
 class DiagHLinear(DiagHBaseModule):
     def __init__(self):
         super().__init__(derivatives=LinearDerivatives(), params=["bias", "weight"])
 
-    # TODO: Reuse code in ..diaggn.linear to extract the diagonal
     def bias(self, ext, module, g_inp, g_out, backproped):
         sqrt_h_outs = backproped["matrices"]
         sqrt_h_outs_signs = backproped["signs"]
-
         h_diag = torch.zeros_like(module.bias)
+
         for h_sqrt, sign in zip(sqrt_h_outs, sqrt_h_outs_signs):
-            h_diag.add_(sign * einsum("cbi->i", (h_sqrt ** 2,)))
-            # h_diag.add_(sign * einsum("bic->i", (h_sqrt ** 2,)))
+            h_diag_curr = LinUtils.extract_bias_diagonal(module, h_sqrt)
+            h_diag.add_(sign * h_diag_curr)
         return h_diag
 
-    # TODO: Reuse code in ..diaggn.linear to extract the diagonal
     def weight(self, ext, module, g_inp, g_out, backproped):
         sqrt_h_outs = backproped["matrices"]
         sqrt_h_outs_signs = backproped["signs"]
-
         h_diag = torch.zeros_like(module.weight)
+
         for h_sqrt, sign in zip(sqrt_h_outs, sqrt_h_outs_signs):
-            h_diag.add_(sign * einsum("cbi,bj->ij", (h_sqrt ** 2, module.input0 ** 2)))
-        # h_diag.add_(sign * einsum("bic,bj->ij", (h_sqrt ** 2, module.input0 ** 2)))
+            h_diag_curr = LinUtils.extract_weight_diagonal(module, h_sqrt)
+            h_diag.add_(sign * h_diag_curr)
         return h_diag
