77b1a1ec5ed1621e2a6964f8ae859643b72f01b0
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2020-01-20 13:27:02 +0100

    [Refactor] Separate summation over pixels and squaring

--- backpack/extensions/secondorder/diag_ggn/conv2d.py
+++ backpack/extensions/secondorder/diag_ggn/conv2d.py
@@ -2,7 +2,7 @@ from backpack.core.derivatives.conv2d import Conv2DDerivatives
 from backpack.utils import conv as convUtils
 from backpack.utils.einsum import einsum
 
-from .diag_ggn_base import DiagGGNBaseModule
+from backpack.extensions.secondorder.diag_ggn.diag_ggn_base import DiagGGNBaseModule
 
 
 class DiagGGNConv2d(DiagGGNBaseModule):
@@ -10,8 +10,9 @@ class DiagGGNConv2d(DiagGGNBaseModule):
         super().__init__(derivatives=Conv2DDerivatives(), params=["bias", "weight"])
 
     def bias(self, ext, module, grad_inp, grad_out, backproped):
-        sqrt_ggn = backproped
-        return einsum("cbijl,cbikm->i", (sqrt_ggn, sqrt_ggn))
+        sqrt_ggn = einsum("vnchw->vnc", backproped)
+        V_axis, N_axis = 0, 1
+        return (sqrt_ggn ** 2).sum([V_axis, N_axis])
 
     def weight(self, ext, module, grad_inp, grad_out, backproped):
         X = convUtils.unfold_func(module)(module.input0)
