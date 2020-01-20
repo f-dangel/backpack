4e0cdfa343b711a7e678e8a30267bd377958bc47
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2020-01-20 13:16:03 +0100

    [Refactor] Clean conv utils

--- backpack/utils/conv.py
+++ backpack/utils/conv.py
@@ -13,26 +13,15 @@ def unfold_func(module):
 
 
 def get_weight_gradient_factors(input, grad_out, module):
-    # batch = input.size(0)
+    # shape [N, C_in * K_x * K_y, H_out * W_out]
     X = unfold_func(module)(input)
-    # dE_dY = grad_out.contiguous().view(batch, module.out_channels, -1)
     dE_dY = eingroup("n,c,h,w->n,c,hw", grad_out)
     return X, dE_dY
 
 
 def separate_channels_and_pixels(module, tensor):
-    """Reshape (v, batch, out_channels, out_x, out_y)
-    into       (v, batch, out_channels, pixels).
-    """
-    # batch, channels, pixels, classes = (
-    #     module.input0.size(0),
-    #     module.out_channels,
-    #     module.output_shape[2] * module.output_shape[3],
-    #     -1,
-    # )
-    # if new_convention:
+    """Reshape (V, N, C, H, W) into (V, N, C, H * W)."""
     return eingroup("v,n,c,h,w->v,n,c,hw", tensor)
-    # return tensor.contiguous().view(classes, batch, channels, pixels)
 
 
 def extract_weight_diagonal(module, input, grad_output):
@@ -41,5 +30,5 @@ def extract_weight_diagonal(module, input, grad_output):
     and grad_output the backpropagated gradient
     """
     grad_output_viewed = separate_channels_and_pixels(module, grad_output)
-    AX = einsum("bkl,cbml->cbkm", (input, grad_output_viewed))
+    AX = einsum("nkl,vnml->vnkm", (input, grad_output_viewed))
     return (AX ** 2).sum([0, 1]).transpose(0, 1)
