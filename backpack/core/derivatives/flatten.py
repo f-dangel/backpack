73760003a6af21bbb2e59310376fbf7ac6bf7293
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2020-01-22 15:23:11 +0100

    [Refactor] Move is_no_op to FlattenDerivatives

--- backpack/core/derivatives/flatten.py
+++ backpack/core/derivatives/flatten.py
@@ -19,3 +19,13 @@ class FlattenDerivatives(BaseDerivatives):
 
     def _jac_mat_prod(self, module, g_inp, g_out, mat):
         return self.view_like_output(mat, module)
+
+    def is_no_op(self, module):
+        """Does flatten add an operation to the computational graph.
+
+        If the input is already flattened, no operation will be added for
+        the `Flatten` layer. This can lead to an intuitive order of backward
+        hook execution, see the discussion at https://discuss.pytorch.org/t/
+        backward-hooks-changing-order-of-execution-in-nn-sequential/12447/4 .
+        """
+        return tuple(module.input0_shape) == tuple(module.output_shape)
--- backpack/extensions/secondorder/diag_ggn/flatten.py
+++ backpack/extensions/secondorder/diag_ggn/flatten.py
@@ -7,17 +7,7 @@ class DiagGGNFlatten(DiagGGNBaseModule):
         super().__init__(derivatives=FlattenDerivatives())
 
     def backpropagate(self, ext, module, grad_inp, grad_out, backproped):
-        if self.is_no_op(module):
+        if self.derivatives.is_no_op(module):
             return backproped
         else:
             return super().backpropagate(ext, module, grad_inp, grad_out, backproped)
-
-    def is_no_op(self, module):
-        """Does flatten add an operation to the computational graph.
-
-        If the input is already flattened, no operation will be added for
-        the `Flatten` layer. This can lead to an intuitive order of backward
-        hook execution, see the discussion at https://discuss.pytorch.org/t/
-        backward-hooks-changing-order-of-execution-in-nn-sequential/12447/4 .
-        """
-        return tuple(module.input0_shape) == tuple(module.output_shape)
