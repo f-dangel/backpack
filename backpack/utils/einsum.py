cd42a6fe66e79aa074b8f6dd7a3ed20b7983af49
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2020-01-25 13:36:10 +0100

    Refactor eingroup, replace debug messages with exceptions

--- backpack/utils/einsum.py
+++ backpack/utils/einsum.py
@@ -57,37 +57,37 @@ def eingroup(equation, operand, dim=None):
 
 
 def _eingroup_preprocess(equation, operand, dim):
+    """Process `eingroup` equation.
+
+    Return the `reshape`s and `einsum` equations that have to
+    be performed.
+    """
     split, sep = "->", ","
 
     def groups(string):
         return string.split(sep)
 
-    def infer(lhs, operand, dim):
-        in_groups = groups(lhs)
-        assert len(in_groups) == len(operand.shape)
+    lhs, rhs = equation.split(split)
+    in_groups, out_groups = groups(lhs), groups(rhs)
 
-        for group, size in zip(in_groups, operand.shape):
-            if len(group) == 1:
-                axis = group[0]
-                if axis in dim.keys():
-                    print("[eingroup] Can infer dim of input group {}".format(group))
-                    assert dim[axis] == size
-                dim[axis] = size
-            else:
-                print("[eingroup] Cannot infer dim of input group {}".format(group))
+    dim = __eingroup_infer(in_groups, operand, dim)
+    in_shape_flat, out_shape = __eingroup_shapes(in_groups, out_groups, dim)
 
-        return dim
+    return in_shape_flat, out_shape, equation.replace(sep, "")
+
+
+def __eingroup_shapes(in_groups, out_groups, dim):
+    """Return shape the input needs to be reshaped, and the output shape"""
 
     def shape(groups, dim):
         return [group_dim(group, dim) for group in groups]
 
     def group_dim(group, dim):
-        return numpy.prod([dim[g] for g in group])
+        try:
+            return numpy.prod([dim[g] for g in group])
+        except KeyError as e:
+            raise KeyError("Unknown dimension for an axis {}".format(e))
 
-    lhs, rhs = equation.split(split)
-    dim = infer(lhs, operand, dim)
-
-    in_groups, out_groups = groups(lhs), groups(rhs)
     out_shape = shape(out_groups, dim)
 
     in_groups_flat = []
@@ -96,7 +96,29 @@ def _eingroup_preprocess(equation, operand, dim):
             in_groups_flat.append(letter)
     in_shape_flat = shape(in_groups_flat, dim)
 
-    return in_shape_flat, out_shape, equation.replace(sep, "")
+    return in_shape_flat, out_shape
+
+
+def __eingroup_infer(in_groups, operand, dim):
+    """Infer the size of each axis."""
+    if not len(in_groups) == len(operand.shape):
+        raise RuntimeError(
+            "Got {} input groups {}, but tensor has {} axes.".format(
+                len(in_groups), in_groups, len(operand.shape)
+            )
+        )
+
+    for group, size in zip(in_groups, operand.shape):
+        if len(group) == 1:
+            axis = group[0]
+            if axis in dim.keys():
+                raise KeyError(
+                    "Can infer dimension of axis {}.".format(axis),
+                    "Remove from dim = {}.".format(dim),
+                )
+            dim[axis] = size
+
+    return dim
 
 
 def try_view(tensor, shape):
