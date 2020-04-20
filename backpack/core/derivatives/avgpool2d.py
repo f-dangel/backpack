Fix KFRA backpropagation for AvgPool2d

backpack/core/derivatives/avgpool2d.py
@@ -5,7 +5,7 @@ import torch.nn
 from torch.nn import AvgPool2d, Conv2d, ConvTranspose2d
 
 from backpack.core.derivatives.basederivatives import BaseDerivatives
-from backpack.utils.ein import eingroup, einsum
+from backpack.utils.ein import eingroup
 
 
 class AvgPool2DDerivatives(BaseDerivatives):
@@ -18,28 +18,21 @@ class AvgPool2DDerivatives(BaseDerivatives):
     # TODO: Require tests
     def ea_jac_t_mat_jac_prod(self, module, g_inp, g_out, mat):
         """Use fact that average pooling can be implemented as conv."""
-        _, channels, in_x, in_y = module.input0.size()
-        in_features = channels * in_x * in_y
-        _, _, out_x, out_y = module.output.size()
-        out_features = channels * out_x * out_y
+        _, C, H_in, W_in = module.input0.size()
+        in_features = C * H_in * W_in
+        _, _, H_out, W_out = module.output.size()
+        out_features = C * H_out * W_out
 
-        # 1) apply conv_transpose to multiply with W^T
-        result = mat.view(channels, out_x, out_y, out_features)
-        result = einsum("cxyf->fcxy", (result,)).contiguous()
-        result = result.view(out_features * channels, 1, out_x, out_y)
-        # result: W^T mat
-        result = self.__apply_jacobian_t_of(module, result)
-        result = result.view(out_features, in_features)
+        mat = mat.view(out_features * C, 1, H_out, W_out)
+        jac_t_mat = self.__apply_jacobian_t_of(module, mat).view(
+            out_features, in_features
+        )
+        mat_t_jac = jac_t_mat.t().reshape(in_features * C, 1, H_out, W_out)
+        jac_t_mat_t_jac = self.__apply_jacobian_t_of(module, mat_t_jac).reshape(
+            in_features, in_features
+        )
 
-        # 2) transpose: mat^T W
-        result = result.t().contiguous()
-
-        # 3) apply conv_transpose
-        result = result.view(in_features * channels, 1, out_x, out_y)
-        result = self.__apply_jacobian_t_of(module, result)
-
-        # 4) transpose to obtain W^T mat W
-        return result.view(in_features, in_features).t()
+        return jac_t_mat_t_jac.t()
 
     def check_exotic_parameters(self, module):
         assert module.count_include_pad, (

test/test_ea_jac_t_mat_jac_prod.py
@@ -2,23 +2,50 @@
 
 H_in →  1/N ∑ₙ Jₙ^T H_out Jₙ
 
+Notes:
+    - `Dropout` cannot be tested,as the `autograd` implementation does a forward
+       pass over each sample, while the `backpack` implementation requires only
+       one forward pass over the batched data. This leads to different outputs,
+       as `Dropout` is not deterministic.
 """
 import pytest
 import torch
-from torch.nn import (  # AvgPool2d,; Dropout,
-    Conv2d,
-    Linear,
-    MaxPool2d,
-    Sigmoid,
-    Tanh,
-    ZeroPad2d,
-)
+from torch.nn import AvgPool2d, Conv2d, Linear, MaxPool2d, Sigmoid, Tanh, ZeroPad2d
 
 from backpack import extend
 from backpack.core.derivatives import derivatives_for
 from backpack.hessianfree.lop import transposed_jacobian_vector_product
 
 from .automated_test import check_sizes, check_values
+
+
+def make_id(layer, input_shape):
+    return "in{}-{}".format(input_shape, layer)
+
+
+torch.manual_seed(0)
+ARGS = "layer,input_shape"
+SETTINGS = [
+    # (layer, input_shape)
+    [Linear(20, 10), (5, 20)],
+    [MaxPool2d(kernel_size=2), (5, 3, 10, 8)],
+    [MaxPool2d(kernel_size=2), (1, 2, 4, 4)],
+    [MaxPool2d(kernel_size=3, stride=2, padding=1), (3, 2, 9, 11)],
+    [AvgPool2d(kernel_size=3), (3, 2, 15, 13)],
+    [AvgPool2d(kernel_size=3, stride=2), (3, 2, 15, 13)],
+    [AvgPool2d(kernel_size=3, stride=2, padding=1), (3, 2, 15, 13)],
+    [Sigmoid(), (6, 20)],
+    [Sigmoid(), (6, 2, 7)],
+    [Tanh(), (6, 20)],
+    [Tanh(), (6, 2, 7)],
+    [Conv2d(2, 3, kernel_size=2), (3, 2, 7, 9)],
+    [Conv2d(2, 3, kernel_size=2, padding=1), (3, 2, 7, 9)],
+    # TODO: ambiguous size if stride > 1
+    # [Conv2d(2, 3, kernel_size=2, padding=1, stride=2), (3, 2, 7, 9)],
+    [Conv2d(2, 3, kernel_size=2, padding=1, stride=1, dilation=2), (3, 2, 7, 9)],
+    [ZeroPad2d(2), (4, 3, 4, 5)],
+]
+IDS = [make_id(layer, input_shape) for (layer, input_shape) in SETTINGS]
 
 
 def get_output_shape(input, layer):
@@ -90,34 +117,6 @@ def generate_data_ea_jac_t_mat_jac_prod(layer, input_shape):
     out_features = get_output_shape(input, layer)[1:].numel()
     mat = torch.rand(out_features, out_features)
     return input, mat
-
-
-def make_id(layer, input_shape):
-    return "in{}-{}".format(input_shape, layer)
-
-
-torch.manual_seed(0)
-ARGS = "layer,input_shape"
-SETTINGS = [
-    # (layer, input_shape)
-    [Linear(20, 10), (5, 20)],
-    [MaxPool2d(kernel_size=2), (5, 3, 10, 8)],
-    [MaxPool2d(kernel_size=2), (1, 2, 4, 4)],
-    [MaxPool2d(kernel_size=3, stride=2, padding=1), (3, 2, 9, 11)],
-    # [AvgPool2d(kernel_size=3), (3, 4, 7, 6)],
-    [Sigmoid(), (6, 20)],
-    [Sigmoid(), (6, 2, 7)],
-    [Tanh(), (6, 20)],
-    [Tanh(), (6, 2, 7)],
-    [Conv2d(2, 3, kernel_size=2), (3, 2, 7, 9)],
-    [Conv2d(2, 3, kernel_size=2, padding=1), (3, 2, 7, 9)],
-    # [Conv2d(2, 3, kernel_size=2, padding=1, stride=2), (3, 2, 7, 9)],
-    [Conv2d(2, 3, kernel_size=2, padding=1, stride=1, dilation=2), (3, 2, 7, 9)],
-    [ZeroPad2d(2), (4, 3, 4, 5)],
-    # not deterministic
-    # [Dropout(0.2), (5, 10, 4)],
-]
-IDS = [make_id(layer, input_shape) for (layer, input_shape) in SETTINGS]
 
 
 @pytest.mark.parametrize(ARGS, SETTINGS, ids=IDS)