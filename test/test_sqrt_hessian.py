d32db3c4a4a1131284376fac37e689135075a979
F. Dangel <fdangel@tuebingen.mpg.de>
2020-04-22 14:23:40 +0200

    Add more tests for MSELoss with reduction 'sum'

--- test/test_sqrt_hessian.py
+++ test/test_sqrt_hessian.py
@@ -77,7 +77,9 @@ SETTINGS = [
     # reduction mean
     [MSELoss(reduction="mean"), (5, 1), regression_targets(shape=(5, 1)), ()],
     [MSELoss(reduction="mean"), (5, 2), regression_targets(shape=(5, 2)), ()],
-    # [MSELoss(reduction="sum"), (5, 1), regression_targets(shape=(5, 1)), ()],
+    # reduction sum
+    [MSELoss(reduction="sum"), (5, 1), regression_targets(shape=(5, 1)), ()],
+    [MSELoss(reduction="sum"), (5, 2), regression_targets(shape=(5, 2)), ()],
     # non-scalar outputs are not supported
     [
         MSELoss(reduction="none"),
