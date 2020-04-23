ba31880ad8ffc461a11c308506385d51d2b54692
fkunstner <frederik.kunstner@gmail.com>
2020-04-22 17:33:14 -0700

    update docs url

--- docs_src/buildweb.sh
+++ docs_src/buildweb.sh
@@ -1,7 +1,4 @@
 cd splash
 bundle exec jekyll build -d "../../docs"
 cd ..
-sphinx-build "rtd" "../docs/rtd" 
 touch ../docs/.nojekyll
-touch ../docs/rtd/.nojekyll
-touch ../docs/rtd/_static/.nojekyll
