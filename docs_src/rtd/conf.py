2a60fdbb11f6e8e7ebeb34d9ac65d9b72f2bd960
fkunstner <frederik.kunstner@gmail.com>
2020-04-24 00:50:07 -0700

    Undo trying to have two sphinx config files

--- docs_src/rtd/conf.py
+++ docs_src/rtd/conf.py
@@ -1,4 +1,4 @@
-# Sphinx Configuration file for local builds
+# Sphinx Configuration file
 #
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
@@ -57,7 +57,7 @@ sphinx_gallery_conf = {
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
