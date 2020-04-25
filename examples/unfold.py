ef44dbd60fe12e293f7f231fe71060be2474484b
F. Dangel <fdangel@tuebingen.mpg.de>
2020-04-25 11:00:50 +0200

    Experiment with embedding Conv(N-1)d in Conv(N)d

--- examples/unfold.py
+++ examples/unfold.py
@@ -23,6 +23,10 @@ Goal:
 
 import torch
 
+###############################################################################
+#      Get the unfolded input with a convolution instead of torch.unfold      #
+###############################################################################
+
 
 def unfold(input, module):
     """Return unfolded input."""
@@ -80,7 +84,7 @@ def unfold_by_conv(input, module):
 
 
 # check
-SETTINGS = [
+UNFOLD_SETTINGS = [
     [torch.nn.Conv2d(1, 1, kernel_size=2, bias=False), (1, 1, 3, 3)],
     [torch.nn.Conv2d(1, 2, kernel_size=2, bias=False), (1, 1, 3, 3)],
     [torch.nn.Conv2d(2, 1, kernel_size=2, bias=False), (1, 2, 3, 3)],
@@ -100,8 +104,7 @@ SETTINGS = [
 ]
 
 print("Unfold check")
-
-for module, in_shape in SETTINGS:
+for module, in_shape in UNFOLD_SETTINGS:
     input = torch.rand(in_shape)
 
     result_unfold = unfold(input, module)
@@ -112,9 +115,15 @@ for module, in_shape in SETTINGS:
     else:
         print("[PASSED] {}".format(module))
 
+###############################################################################
+#             Perform a convolution with the unfolded input matrix            #
+###############################################################################
+
 
 def convolution_with_unfold(input, module):
     """Perform convlution via matrix multiplication."""
+    assert module.bias is None
+
     N, C_in, H_in, W_in = input.shape
     _, _, H_out, W_out = get_output_shape(input, module)
     C_out, _, K_H, K_W = module.weight.shape
@@ -136,7 +145,7 @@ def get_output_shape(input, module):
     return module(input).shape
 
 
-SETTINGS += [
+CONV_SETTINGS = UNFOLD_SETTINGS + [
     [
         torch.nn.Conv2d(
             2, 6, kernel_size=2, padding=1, stride=2, dilation=2, bias=False, groups=2
@@ -164,7 +173,7 @@ SETTINGS += [
 ]
 
 print("Conv via unfold check")
-for module, in_shape in SETTINGS:
+for module, in_shape in CONV_SETTINGS:
     input = torch.rand(in_shape)
 
     result_conv = module(input)
@@ -174,3 +183,205 @@ for module, in_shape in SETTINGS:
         raise ValueError("[FAILED-conv] {}".format(module))
     else:
         print("[PASSED] {}".format(module))
+
+###############################################################################
+#                         Embed Conv(N-1)d in Conv(N)d                        #
+###############################################################################
+
+
+def add_dimension(input, module):
+    """Transform input and module of `Conv(N-1)d` into `Conv(N)d`."""
+    new_input = add_input_dimension(input)
+    new_module = add_module_dimension(module)
+
+    return new_input, new_module
+
+
+def remove_dimension(input, module):
+    """Transform input and module of `Conv(N)d` into `Conv(N-1)d`."""
+    new_input = remove_input_dimension(input)
+    new_module = remove_module_dimension(module)
+
+    return new_input, new_module
+
+
+def add_input_dimension(input):
+    new_input = input.unsqueeze(-1)
+    return new_input
+
+
+def remove_input_dimension(input):
+    new_input = input.squeeze(-1)
+    return new_input
+
+
+def add_module_dimension(module):
+    if isinstance(module, torch.nn.Conv1d):
+        new_module_cls = torch.nn.Conv2d
+    elif isinstance(module, torch.nn.Conv2d):
+        new_module_cls = torch.nn.Conv3d
+    else:
+        raise ValueError("Cannot add dimension to {}".format(module))
+
+    new_out_channels = module.out_channels
+    new_in_channels = module.in_channels
+    new_kernel_size = module.kernel_size + (1,)
+    new_stride = module.stride + (1,)
+    new_padding = module.padding + (0,)
+    new_dilation = module.dilation + (1,)
+    new_groups = module.groups
+    new_bias = module.bias is not None
+    new_padding_mode = module.padding_mode
+
+    new_module = new_module_cls(
+        out_channels=new_out_channels,
+        in_channels=new_in_channels,
+        kernel_size=new_kernel_size,
+        stride=new_stride,
+        padding=new_padding,
+        dilation=new_dilation,
+        groups=new_groups,
+        bias=new_bias,
+        padding_mode=new_padding_mode,
+    )
+
+    new_weight_data = module.weight.data.unsqueeze(-1)
+    new_module.weight.data = new_weight_data
+
+    if new_bias:
+        new_bias_data = module.bias.data
+        new_module.bias.data = new_bias_data
+
+    return new_module
+
+
+def remove_module_dimension(module):
+    if isinstance(module, torch.nn.Conv3d):
+        new_module_cls = torch.nn.Conv2d
+    elif isinstance(module, torch.nn.Conv2d):
+        new_module_cls = torch.nn.Conv1d
+    else:
+        raise ValueError("Cannot remove dimension of {}".format(module))
+
+    new_out_channels = module.out_channels
+    new_in_channels = module.in_channels
+
+    assert module.kernel_size[-1] == 1
+    new_kernel_size = module.kernel_size[:-1]
+
+    assert module.stride[-1] == 1
+    new_stride = module.stride[:-1]
+
+    assert module.padding[-1] == 0
+    new_padding = module.padding[:-1]
+
+    assert module.dilation[-1] == 1
+    new_dilation = module.dilation[:-1]
+
+    new_groups = module.groups
+    new_bias = module.bias is not None
+    new_padding_mode = module.padding_mode
+
+    new_module = new_module_cls(
+        out_channels=new_out_channels,
+        in_channels=new_in_channels,
+        kernel_size=new_kernel_size,
+        stride=new_stride,
+        padding=new_padding,
+        dilation=new_dilation,
+        groups=new_groups,
+        bias=new_bias,
+        padding_mode=new_padding_mode,
+    )
+
+    assert module.weight.shape[-1] == 1
+    new_weight_data = module.weight.data.squeeze(-1)
+    new_module.weight.data = new_weight_data
+
+    if new_bias:
+        new_bias_data = module.bias.data
+        new_module.bias.data = new_bias_data
+
+    return new_module
+
+
+INCREASE_SETTINGS = CONV_SETTINGS + [
+    # 1d convolution
+    ## no bias
+    [torch.nn.Conv1d(1, 1, kernel_size=2, bias=False), (1, 1, 3)],
+    [torch.nn.Conv1d(1, 2, kernel_size=2, bias=False), (1, 1, 3)],
+    [torch.nn.Conv1d(2, 1, kernel_size=2, bias=False), (1, 2, 3)],
+    [torch.nn.Conv1d(2, 2, kernel_size=2, bias=False), (1, 2, 3)],
+    [torch.nn.Conv1d(2, 3, kernel_size=2, bias=False), (3, 2, 11)],
+    [torch.nn.Conv1d(2, 3, kernel_size=2, padding=1, bias=False), (3, 2, 11)],
+    [
+        torch.nn.Conv1d(2, 3, kernel_size=2, padding=1, stride=2, bias=False),
+        (3, 2, 11),
+    ],
+    [
+        torch.nn.Conv1d(
+            2, 3, kernel_size=2, padding=1, stride=2, dilation=2, bias=False
+        ),
+        (3, 2, 11),
+    ],
+    ## with bias
+    [torch.nn.Conv1d(1, 1, kernel_size=2, bias=True), (1, 1, 3)],
+    [torch.nn.Conv1d(1, 2, kernel_size=2, bias=True), (1, 1, 3)],
+    [torch.nn.Conv1d(2, 1, kernel_size=2, bias=True), (1, 2, 3)],
+    [torch.nn.Conv1d(2, 2, kernel_size=2, bias=True), (1, 2, 3)],
+    [torch.nn.Conv1d(2, 3, kernel_size=2, bias=True), (3, 2, 11)],
+    [torch.nn.Conv1d(2, 3, kernel_size=2, padding=1, bias=True), (3, 2, 11)],
+    [torch.nn.Conv1d(2, 3, kernel_size=2, padding=1, stride=2, bias=True), (3, 2, 11),],
+    [
+        torch.nn.Conv1d(
+            2, 3, kernel_size=2, padding=1, stride=2, dilation=2, bias=True
+        ),
+        (3, 2, 11),
+    ],
+    # 2d convolution with bias
+    [torch.nn.Conv2d(1, 1, kernel_size=2, bias=True), (1, 1, 3, 3)],
+    [torch.nn.Conv2d(1, 2, kernel_size=2, bias=True), (1, 1, 3, 3)],
+    [torch.nn.Conv2d(2, 1, kernel_size=2, bias=True), (1, 2, 3, 3)],
+    [torch.nn.Conv2d(2, 2, kernel_size=2, bias=True), (1, 2, 3, 3)],
+    [torch.nn.Conv2d(2, 3, kernel_size=2, bias=True), (3, 2, 11, 13)],
+    [torch.nn.Conv2d(2, 3, kernel_size=2, padding=1, bias=True), (3, 2, 11, 13)],
+    [
+        torch.nn.Conv2d(2, 3, kernel_size=2, padding=1, stride=2, bias=True),
+        (3, 2, 11, 13),
+    ],
+    [
+        torch.nn.Conv2d(
+            2, 3, kernel_size=2, padding=1, stride=2, dilation=2, bias=True
+        ),
+        (3, 2, 11, 13),
+    ],
+]
+
+print("Conv embedding in higher dimension")
+for module, in_shape in INCREASE_SETTINGS:
+    print("[TRY] {}".format(module))
+    input = torch.rand(in_shape)
+
+    result_conv = module(input)
+
+    # in higher dim
+    new_input, new_module = add_dimension(input, module)
+    result_conv2 = remove_input_dimension(new_module(new_input))
+
+    # if not torch.allclose(result_conv, result_conv2, atol=1e-6):
+    # for res1, res2 in zip(result_conv.flatten(), result_conv2.flatten()):
+    # print(res1, "should be", res2, "ratio:", res1 / res2)
+    assert torch.allclose(result_conv, result_conv2, atol=1e-6)
+
+    # adding and removing a dimension are inverses
+    old_input, old_module = remove_dimension(new_input, new_module)
+    assert torch.allclose(old_input, input)
+    result_conv3 = old_module(old_input)
+
+    assert torch.allclose(result_conv, result_conv3)
+
+    print("[PASSED] {}".format(module))
+
+###############################################################################
+#                  Generalize unfold to input data for Conv3d                 #
+###############################################################################
