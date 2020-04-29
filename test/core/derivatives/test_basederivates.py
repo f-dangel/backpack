593f1cead177ecd13344d375710354fe4405e691
sbharadwajj <coolshrisha@gmail.com>
2020-04-29 14:40:51 +0200

    ADD: test_basederivatives.py

--- test/core/derivatives/test_basederivates.py
+++ test/core/derivatives/test_basederivates.py
@@ -9,148 +9,161 @@ import warnings
 import pytest
 import torch
 
-# TODO: Move to a utils file, fix imports
-from ...automated_test import check_sizes, check_values
+
+from backpack import extend
+
+import backpack.core.derivatives.linear
+import backpack.core.derivatives.basederivatives
+from backpack.core.derivatives.basederivatives import BaseDerivatives
+from backpack.core.derivatives import derivatives_for
+from backpack.hessianfree.rop import jacobian_vector_product
 
 
-class DerivativesModule:
-    """ Information required to test a class inheriting from
-    `backpack.core.derivatives.BaseDerivatives`.
+from test.core.derivatives.utils import check_sizes, check_values, get_available_devices
+from test.core.derivatives.test_setup import ALL_CONFIGURATIONS, CONFIGURATION_IDS
 
-    batch_size : N
-    input_shape : [C_in, H_in, W_in, ...]
-
-    Shape of module input → output:
-      [N, C_in, H_in, W_in, ...] → [N, C_out, H_out, W_out, ...]
-
-    """
-
-    def __init__(self, module, N, input_shape, device):
-        self.module = module
-        # TODO: write functionality to get derivative class
-        # from module using backpack.core.derivatives.derivatives_for dict
-        self.derivative = None
-        self.N = N
-        self.input_shape = input_shape
-        self.device = device
-
+torch.manual_seed(123)
+        
 
 class DerivativesImplementation:
-    """Base class for autograd and BackPACK implementations."""
+    """Base class for autograd and BackPACK implementations.
+
+    self.input_mat: The input matrix required for testing is initialized
+    """
 
     def __init__(self, derivatives_module):
-        self.derivatives_module = derivatives_module
+        self.N = derivatives_module.N
+        self.input_shape = derivatives_module.input_shape 
+        self.module = derivatives_module.module
+        self.g_inp, self.g_out = None, None
 
-    def jac_t_mat_prod(self, mat):
+        #Initialize input_mat
+        torch.manual_seed(123)
+        self.input_mat = torch.rand(self.N, *self.input_shape) 
+        
+
+    def jac_t_mat_prod(self, mat):        
         raise NotImplementedError
 
 
-class BackpackDerivativesImplementation(DerivativesImplementation):
-    """Derivative implementations with BackPACK."""
 
-    DUMMY = torch.zeros(1)
+class BackpackDerivativesImplementation(DerivativesImplementation):
+    """Derivative implementations with BackPACK.
+    """
+    def __init__(self, derivatives_module):
+        super().__init__(derivatives_module)
+        self.module = extend(self.module) 
+
+        def get_backpack_class(module):
+            '''
+            Input:
+                module: torch.nn module 
+            Returns:
+                layer_backpack_class = class instance of torch.nn from 'backpack.core.derivatives`
+
+            '''
+            if isinstance(module,type(module)):  
+                layer_backpack_class = derivatives_for[type(module)]         
+                return layer_backpack_class
+
+            raise RuntimeError("No derivative available for {}".format(module))
+
+        self.output_mat = self.module(self.input_mat)
+        
+        layer_backpack_class = get_backpack_class(self.module)
+        self.backpackClass = layer_backpack_class()
+
 
     def jac_t_mat_prod(self, mat):
-        warnings.warn("Using dummy Backpack implementation.")
-        return self.DUMMY
+        '''
+        Input:
+            mat: Matrix with which the jacobian is multiplied.
+                 shape: [V, N, C_in, H_in, W_in]
+        Return:
+            jmp: Jacobian matrix product obatined from backPACK
+
+        '''
+        jmp = self.backpackClass.jac_mat_prod(self.module, self.g_inp, self.g_out,mat)
+        return jmp
 
 
 class AutogradDerivativesImplementation(DerivativesImplementation):
     """Derivative implementations with autograd.
 
     """
+    def __init__(self, derivatives_module):
+        super().__init__(derivatives_module)
 
-    DUMMY = torch.zeros(1)
+    def jac_t_mat_prod_vec(self, input_mat, mat):
+        '''
+        Input:
+            input_mat: input matrix.
+                       shape: [N, C, H_in, W_in]
+                  mat: Matrix with which the jacobian is multiplied.
+                       shape: [N, C_in, H_in, W_in]
+        Returns:
+             jmp_vec: Jacobian matrix product obatined from torch.autograd
+        '''
+        input_mat.requires_grad = True
+        output_mat = self.module(input_mat)
+        jmp_vec = jacobian_vector_product(output_mat, self.input_mat, mat)
+        return jmp_vec[0] #returns as a tuple?
 
-    # TODO: Call transposed Jacobian from backpack.hessianfree
+
     def jac_t_mat_prod(self, mat):
-        warnings.warn("Using dummy Autograd implementation.")
-        return self.DUMMY
+        '''
+        Input:
+            mat: Matrix with which the jacobian is multiplied.
+                 shape: [V, N, C_in, H_in, W_in]
+        Return:
+            jmp: Jacobian matrix product obatined from torch.autograd
 
+        '''
+        V = mat.shape[0]
+        jmp_list = []
 
-# TODO: Move to a utils file
-def get_available_devices():
-    devices = [torch.device("cpu")]
+        for v in range(V):
+            mat_ = mat[v]
+            jmp_mat = self.jac_t_mat_prod_vec(self.input_mat, mat_)
+            jmp_list.append(jmp_mat)
 
-    if torch.cuda.is_available():
-        devices.add(torch.device("cuda"))
+        jmp = torch.stack(jmp_list)
+        return jmp
 
-    return devices
-
-
-def set_up_derivatives_module(setup, device):
-    """Create a DerivativesModule object from setup."""
-    module_cls = setup["module_cls"]
-    module_kwargs = setup["module_kwargs"]
-    N = setup["N"]
-    input_shape = setup["input_shape"]
-
-    module = module_cls(**module_kwargs)
-    derivatives_module_id = "{}-{}-N={}-input_shape={}".format(
-        device, module, N, input_shape
-    )
-
-    return DerivativesModule(module, N, input_shape, device), derivatives_module_id
-
-
-##############################################################################
-# TODO: Move to test_linear.py
-# In general, the files test_sigmoid, test_avgpool2d, ... should contain such
-# a list, which is merged in this file
-
-# Setting up modules for the test, not sure if this is the best way
-SETUPS = [
-    # first setup
-    {
-        "module_cls": torch.nn.Linear,
-        "module_kwargs": {"in_features": 7, "out_features": 3, "bias": True,},
-        "input_shape": (7,),
-        "N": 10,
-    },
-    # second setup
-    {
-        "module_cls": torch.nn.Linear,
-        "module_kwargs": {"in_features": 10, "out_features": 1, "bias": False,},
-        "input_shape": (10,),
-        "N": 1,
-    },
-]
-##############################################################################
-
-##############################################################################
-# create test for each device
-DEVICES = get_available_devices()
-# TODO: Convert to function
-ALL_CONFIGURATIONS = []
-CONFIGURATION_IDS = []
-
-for setup in SETUPS:
-    for device in DEVICES:
-        derivatives_module, derivatives_module_id = set_up_derivatives_module(
-            setup, device
-        )
-
-        ALL_CONFIGURATIONS.append(derivatives_module)
-        CONFIGURATION_IDS.append(derivatives_module_id)
-
-##############################################################################
 
 
 @pytest.mark.parametrize(
-    "derivatives_module", ALL_CONFIGURATIONS, ids=CONFIGURATION_IDS
-)
-def test_jac_t_mat_prod(derivatives_module, V=10):
-    """Test the transposed Jacobian-matrix product. """
-    # TODO: Set up matrix of size [V, N, C_in, H_in, W_in]
-    warnings.warn("This is a dummy.")
-    mat = None
+    "derivatives_module", ALL_CONFIGURATIONS, ids=CONFIGURATION_IDS)
 
+def test_jac_t_mat_prod(derivatives_module, V=10):
+    """Test the transposed Jacobian-matrix product by comparing the 
+        values and sizes obatined by backpack with torch.autograd 
+    
+    mat: Matrix with which the jacobian is multiplied.
+         shape: [V, N, C_in, H_in, W_in]
+                V: vec_cols
+                N: batch_size
+                C_in: Channels
+                H_in, W_in: Height, Width 
+
+
+    """
+    N = derivatives_module.N
+    input_shape = derivatives_module.input_shape    
+
+    torch.manual_seed(123)
+    mat = torch.rand(V, N, *input_shape) 
+
+    
     backpack_res = BackpackDerivativesImplementation(derivatives_module).jac_t_mat_prod(
         mat
     )
+
     autograd_res = AutogradDerivativesImplementation(derivatives_module).jac_t_mat_prod(
         mat
     )
 
     check_sizes(autograd_res, backpack_res)
     check_values(autograd_res, backpack_res)
+
+
