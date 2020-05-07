bacef89edf47a67a3be67afa6dc03aa24cce7f33
F. Dangel <fdangel@tuebingen.mpg.de>
2020-05-07 20:25:15 +0200

    Refactor jac_t, use convolution from functional

--- backpack/core/derivatives/conv_transpose2d.py
+++ backpack/core/derivatives/conv_transpose2d.py
@@ -1,6 +1,7 @@
 """Partial derivatives for `torch.nn.ConvTranspose2d`."""
 
-from torch.nn import Conv2d, ConvTranspose2d
+from torch.nn import ConvTranspose2d
+from torch.nn.functional import conv2d
 
 from backpack.core.derivatives.basederivatives import BaseParameterDerivatives
 from backpack.utils.ein import eingroup
@@ -31,32 +32,21 @@ class ConvTranspose2DDerivatives(BaseParameterDerivatives):
 
     def __jac_t(self, module, mat):
         """Apply ConvTranspose2d backward operation."""
-        _, C_in, H_in, W_in = module.input0.size()
-        _, C_out, H_out, W_out = module.output.size()
         H_axis = 2
         W_axis = 3
+        H_in = module.input0.size(H_axis)
+        W_in = module.input0.size(W_axis)
 
-        conv2d = Conv2d(
-            in_channels=C_out,
-            out_channels=C_in,
-            kernel_size=module.kernel_size,
-            stride=module.stride,
-            padding=module.padding,
-            bias=False,
-            dilation=module.dilation,
-            groups=module.groups,
-        ).to(module.input0.device)
-
-        conv2d.weight.data = module.weight
-
-        # V_N = mat.size(0)
-        # output_size = (V_N, C_in, H_in, W_in)
-
-        # jac_t_mat = (
-        #     conv2d_t(mat, output_size=output_size)
-        #     .narrow(H_axis, 0, H_in)
-        #     .narrow(W_axis, 0, W_in)
-        # )
-
-        jac_t_mat = conv2d(mat).narrow(H_axis, 0, H_in).narrow(W_axis, 0, W_in)
-        return jac_t_mat
+        return (
+            conv2d(
+                mat,
+                module.weight,
+                bias=None,
+                stride=module.stride,
+                padding=module.padding,
+                dilation=module.dilation,
+                groups=module.groups,
+            )
+            .narrow(H_axis, 0, H_in)
+            .narrow(W_axis, 0, W_in)
+        )
