2de7bf3cf3bcf38e4db5bc767d883311c9e4fd69
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2020-05-20 15:43:36 +0200

    Remove double inheritance

--- backpack/extensions/curvmatprod/ggnmp/pooling.py
+++ backpack/extensions/curvmatprod/ggnmp/pooling.py
@@ -3,11 +3,11 @@ from backpack.core.derivatives.maxpool2d import MaxPool2DDerivatives
 from backpack.extensions.curvmatprod.ggnmp.ggnmpbase import GGNMPBase
 
 
-class GGNMPAvgPool2d(GGNMPBase, AvgPool2DDerivatives):
+class GGNMPAvgPool2d(GGNMPBase):
     def __init__(self):
         super().__init__(derivatives=AvgPool2DDerivatives())
 
 
-class GGNMPMaxpool2d(GGNMPBase, MaxPool2DDerivatives):
+class GGNMPMaxpool2d(GGNMPBase):
     def __init__(self):
         super().__init__(derivatives=MaxPool2DDerivatives())
--- backpack/extensions/curvmatprod/hmp/pooling.py
+++ backpack/extensions/curvmatprod/hmp/pooling.py
@@ -3,11 +3,11 @@ from backpack.core.derivatives.maxpool2d import MaxPool2DDerivatives
 from backpack.extensions.curvmatprod.hmp.hmpbase import HMPBase
 
 
-class HMPAvgPool2d(HMPBase, AvgPool2DDerivatives):
+class HMPAvgPool2d(HMPBase):
     def __init__(self):
         super().__init__(derivatives=AvgPool2DDerivatives())
 
 
-class HMPMaxpool2d(HMPBase, MaxPool2DDerivatives):
+class HMPMaxpool2d(HMPBase):
     def __init__(self):
         super().__init__(derivatives=MaxPool2DDerivatives())
