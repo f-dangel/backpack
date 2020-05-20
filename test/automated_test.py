42a6d3b996bd58134083a090b8b17749240d702f
f-dangel <48687646+f-dangel@users.noreply.github.com>
2020-05-20 12:23:53 +0200

    Increase atol for batchnorm hmp

--- test/automated_test.py
+++ test/automated_test.py
@@ -228,8 +228,9 @@ def test_hmp(problem, device):
     autograd_res = AutogradImpl(problem).hmp(matrices)
 
     check_sizes(autograd_res, backpack_res)
-    rtol = 5e-5
-    check_values(autograd_res, backpack_res, rtol=rtol)
+    atol= 5e-4
+    rtol = 5e-4
+    check_values(autograd_res, backpack_res, atol=atol, rtol=rtol)
 
 
 @pytest.mark.parametrize(
