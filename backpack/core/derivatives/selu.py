d58c351a465584feac87dddc8dc5a876eb805a6c
sbharadwajj <coolshrisha@gmail.com>
2020-05-22 17:03:23 +0200

    EDIT: fixed black and flake8

--- backpack/core/derivatives/selu.py
+++ backpack/core/derivatives/selu.py
@@ -6,6 +6,7 @@ from backpack.core.derivatives.elementwise import ElementwiseDerivatives
 
 class SELUDerivatives(ElementwiseDerivatives):
     """Alpha and scale are not input_kwargs"""
+
     alpha = 1.6732632423543772848170429916717
     scale = 1.0507009873554804934193349852946
 
@@ -21,7 +22,9 @@ class SELUDerivatives(ElementwiseDerivatives):
 
         df_SELU = gt(module.input0, 0).float()
         df_SELU[df_SELU == 1] = self.scale
-        df_SELU[df_SELU == 0] = self.scale * self.alpha * exp(module.input0[df_SELU == 0])
+        df_SELU[df_SELU == 0] = (
+            self.scale * self.alpha * exp(module.input0[df_SELU == 0])
+        )
         return df_SELU
 
     def d2f(self, module, g_inp, g_out):
@@ -29,5 +32,7 @@ class SELUDerivatives(ElementwiseDerivatives):
 
         d2f_SELU = gt(module.input0, 0).float()
         d2f_SELU[d2f_SELU == 1] = 0
-        d2f_SELU[d2f_SELU == 0] = self.scale * self.alpha * exp(module.input0[df_SELU == 0])
+        d2f_SELU[d2f_SELU == 0] = (
+            self.scale * self.alpha * exp(module.input0[d2f_SELU == 0])
+        )
         return d2f_SELU
