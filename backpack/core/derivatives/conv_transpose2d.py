49d4448c543a5f588c9380045612a10574e14cae
F. Dangel <fdangel@tuebingen.mpg.de>
2020-05-24 18:54:18 +0200

    Add convTranspose weight_jac_t

--- backpack/core/derivatives/conv_transpose2d.py
+++ backpack/core/derivatives/conv_transpose2d.py
@@ -1,9 +1,11 @@
 """Partial derivatives for `torch.nn.ConvTranspose2d`."""
 
+import torch
 from torch.nn import ConvTranspose2d
 from torch.nn.functional import conv2d, conv_transpose2d
 
 from backpack.core.derivatives.basederivatives import BaseParameterDerivatives
+from backpack.utils.conv_transpose import unfold_by_conv_transpose
 from backpack.utils.ein import eingroup
 
 
@@ -22,53 +24,46 @@ class ConvTranspose2DDerivatives(BaseParameterDerivatives):
 
         return mat.sum(axes)
 
+    def bias_jac_mat_prod(self, module, g_inp, g_out, mat):
+        # TODO Implement with unfold
+        raise NotImplementedError
+
+    def weight_jac_mat_prod(self, module, g_inp, g_out, mat):
+        # TODO Implement with unfold
+        raise NotImplementedError
+
     def _weight_jac_t_mat_prod(self, module, g_inp, g_out, mat, sum_batch=True):
-        """Unintuitive, but faster due to convolution."""
+        """Unintuitive, but faster due to convolution.
+
+        For 1d transpose convolution (x, W) ↦ y with u = unfold(x):
+
+        y[N,G,O,H] = ∑_{I,X} W[I,G,O,X] u[N,I,G,X,H]
+
+        ∇W[n,i,g,o,x] = ∑_{G,O,H} ∂y[n,G,O,H]/∂W[i,g,o,x] ∇y[n,G,O,H]
+                      = ∑_{G,O,H,I,X} ∂(W[I,G,O,X] u[n,I,G,X,H])/∂W[i,g,o,x] ∇y[n,G,O,H]
+                      = ∑_{H} u[n,i,g,x,H]) ∇y[n,g,o,H]
+        """
         V = mat.shape[0]
-        N, C_out, H_out, W_out = module.output_shape
-        _, C_in, _, _ = module.input0_shape
+        G = module.groups
+        N, C_out, H_out, W_out = module.output.shape
 
-        mat = mat.unsqueeze(2).repeat(1, 1, C_in, 1, 1, 1)
-        mat = eingroup("v,n,c,d,w,h->vnc,d,w,h", mat)
+        mat_reshape = mat.reshape(V, N, G, C_out // G, H_out, W_out)
 
-        N_axis = 0
-        input = eingroup("n,c,h,w->nc,h,w", module.input0).unsqueeze(N_axis)
-        input = input.repeat(1, V, 1, 1)
+        C_in = module.input0.shape[1]
+        _, _, K_X, K_Y = module.weight.shape
 
-        assert module.stride == module.dilation
-
-        conv2d_t = ConvTranspose2d(
-            in_channels=V * N * C_in,
-            out_channels=V * N * C_out * C_in,
-            kernel_size=(H_out, W_out),
-            stride=module.dilation,
-            padding=module.padding,
-            bias=False,
-            dilation=module.stride,
-            groups=C_in * N * V,
-        ).to(module.input0.device)
-        print(conv2d_t)
-
-        print("Conv_t weight shape:", conv2d_t.weight.shape)
-        print("Mat shape:", mat.shape)
-        assert conv2d_t.weight.shape == mat.shape
-        conv2d_t.weight.data = mat
-
-        K_H_axis, K_W_axis = 1, 2
-        _, _, K_H, K_W = module.weight.shape
-
-        # something is wrong with the output size
-        output_size = (1, V * N * C_in * C_out, K_H, K_W)
-        grad_weight = conv2d_t(input, output_size=output_size).squeeze(0)
-
-        print(grad_weight.shape)
-
-        grad_weight = grad_weight.narrow(K_H_axis, 0, K_H).narrow(K_W_axis, 0, K_W)
-
-        eingroup_eq = "vnio,x,y->v,{}i,o,x,y".format("" if sum_batch else "n,")
-        return eingroup(
-            eingroup_eq, grad_weight, dim={"v": V, "n": N, "i": C_in, "o": C_out}
+        u = unfold_by_conv_transpose(module.input0, module).reshape(
+            N, C_in // G, G, K_X, K_Y, H_out, W_out
         )
+
+        result_str = "vigoxy" if sum_batch else "vnigoxy"
+        equation = "nigxyhw,vngohw->{}".format(result_str)
+
+        final_shape = (
+            (V, *module.weight.shape) if sum_batch else (V, N, *module.weight.shape)
+        )
+
+        return torch.einsum(equation, u, mat_reshape).reshape(final_shape)
 
     def _jac_t_mat_prod(self, module, g_inp, g_out, mat):
         mat_as_conv = eingroup("v,n,c,h,w->vn,c,h,w", mat)
