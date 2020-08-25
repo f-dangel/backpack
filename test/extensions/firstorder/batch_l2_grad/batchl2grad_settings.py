cacfd6d7cbe97803678f9598df4003ea2ea36a90
f-dangel <48687646+f-dangel@users.noreply.github.com>
2020-08-25 17:45:47 +0200

    Update batchl2grad_settings.py

--- test/extensions/firstorder/batch_l2_grad/batchl2grad_settings.py
+++ test/extensions/firstorder/batch_l2_grad/batchl2grad_settings.py
@@ -5,9 +5,7 @@ but additional custom tests can be defined here by appending it to the list.
 """
 from test.extensions.firstorder.firstorder_settings import FIRSTORDER_SETTINGS
 
-BATCHl2GRAD_SETTINGS = []
+SHARED_SETTINGS = FIRSTORDER_SETTINGS
+LOCAL_SETTINGS = []
 
-SHARED_SETTING = FIRSTORDER_SETTINGS
-LOCAL_SETTING = []
-
-BATCHl2GRAD_SETTINGS = SHARED_SETTING + LOCAL_SETTING
+BATCHl2GRAD_SETTINGS = SHARED_SETTINGS + LOCAL_SETTINGS
