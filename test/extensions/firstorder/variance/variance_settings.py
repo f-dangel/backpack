873335f4cf285853883bd0d3204f5dfb7c07b3be
f-dangel <48687646+f-dangel@users.noreply.github.com>
2020-08-25 17:48:14 +0200

    Update variance_settings.py

--- test/extensions/firstorder/variance/variance_settings.py
+++ test/extensions/firstorder/variance/variance_settings.py
@@ -5,8 +5,6 @@ but additional custom tests can be defined here by appending it to the list.
 """
 from test.extensions.firstorder.firstorder_settings import FIRSTORDER_SETTINGS
 
-VARIANCE_SETTINGS = []
-
 SHARED_SETTINGS = FIRSTORDER_SETTINGS
 LOCAL_SETTINGS = []
 
