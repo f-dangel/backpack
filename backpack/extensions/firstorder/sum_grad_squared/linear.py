ac76a08baccb2f49fd22428e8bb0300592ce823c
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2020-09-08 12:39:15 +0200

    Clean up redundant code in linear SGS, add notes on trick for weights

--- backpack/extensions/firstorder/sum_grad_squared/linear.py
+++ backpack/extensions/firstorder/sum_grad_squared/linear.py
@@ -7,9 +7,14 @@ class SGSLinear(FirstOrderModuleExtension):
     def __init__(self):
         super().__init__(params=["bias", "weight"])
 
-    def bias(self, ext, module, g_inp, g_out, backproped):
-        N_axis = 0
-        return (g_out[0] ** 2).sum(N_axis)
-
     def weight(self, ext, module, g_inp, g_out, backproped):
+        """Compute second moments without expanding individual gradients.
+
+        Overwrites the base class implementation that computes the gradient second
+        moments from individual gradients. This approach is more memory-efficient.
+
+        Note:
+            For details, see page 12 (paragraph about "second moment") of the
+            paper (https://arxiv.org/pdf/1912.10985.pdf).
+        """
         return einsum("ni,nj->ij", (g_out[0] ** 2, module.input0 ** 2))
