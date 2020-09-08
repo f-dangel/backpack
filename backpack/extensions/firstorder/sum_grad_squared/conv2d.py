72f7ade9fd885fbebace92cabe121da989aab400
Shrisha Bharadwaj <coolshrisha@gmail.com>
2020-09-08 12:59:21 +0200

    Refactor SumGrad  (#106)
    Introduce base class for second moment computation via individual gradients.
    
    Add notes on trick for weights of a linear layer

--- backpack/extensions/firstorder/sum_grad_squared/conv2d.py
+++ backpack/extensions/firstorder/sum_grad_squared/conv2d.py
@@ -1,21 +1,7 @@
-from torch import einsum
-
-from backpack.extensions.firstorder.base import FirstOrderModuleExtension
-from backpack.utils import conv as convUtils
+from backpack.core.derivatives.conv2d import Conv2DDerivatives
+from backpack.extensions.firstorder.sum_grad_squared.sgs_base import SGSBase
 
 
-class SGSConv2d(FirstOrderModuleExtension):
+class SGSConv2d(SGSBase):
     def __init__(self):
-        super().__init__(params=["bias", "weight"])
-
-    def bias(self, ext, module, g_inp, g_out, backproped):
-        N_axis = 0
-        return (einsum("nchw->nc", g_out[0]) ** 2).sum(N_axis)
-
-    def weight(self, ext, module, g_inp, g_out, backproped):
-        N_axis = 0
-        X, dE_dY = convUtils.get_weight_gradient_factors(
-            module.input0, g_out[0], module
-        )
-        grad_batch = einsum("nml,nkl->nmk", (dE_dY, X))
-        return (grad_batch ** 2).sum(N_axis).view_as(module.weight)
+        super().__init__(derivatives=Conv2DDerivatives(), params=["bias", "weight"])
