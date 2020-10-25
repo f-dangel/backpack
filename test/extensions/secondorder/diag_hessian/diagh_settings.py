ac24226408eda4c522d172bef5cd1e5c0e566fde
sbharadwajj <coolshrisha@gmail.com>
2020-10-25 23:13:12 +0100

    initial commit

---
+++ test/extensions/secondorder/diag_hessian/diagh_settings.py
@@ -0,0 +1,14 @@
+"""Test configurations to test diag_h
+
+The tests are taken from `test.extensions.secondorder.secondorder_settings`, 
+but additional custom tests can be defined here by appending it to the list.
+"""
+
+from test.extensions.secondorder.secondorder_settings import SECONDORDER_SETTNGS
+
+DiagHESSIAN_SETTINGS = []
+
+SHARED_SETTINGS = SECONDORDER_SETTNGS
+LOCAL_SETTING = []
+
+DiagHESSIAN_SETTINGS = SHARED_SETTINGS + LOCAL_SETTING
