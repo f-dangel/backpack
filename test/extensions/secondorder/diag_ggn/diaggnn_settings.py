dd71e65a65a48970d945b03944aa4d13ee388884
sbharadwajj <coolshrisha@gmail.com>
2020-11-15 17:23:37 +0100

    ADD: changes made for PR

--- test/extensions/secondorder/diag_ggn/diaggnn_settings.py
+++ test/extensions/secondorder/diag_ggn/diaggnn_settings.py
@@ -4,11 +4,11 @@ The tests are taken from `test.extensions.secondorder.secondorder_settings`,
 but additional custom tests can be defined here by appending it to the list.
 """
 
-from test.extensions.secondorder.secondorder_settings import SECONDORDER_SETTNGS
+from test.extensions.secondorder.secondorder_settings import SECONDORDER_SETTINGS
 
 DiagGGN_SETTINGS = []
 
-SHARED_SETTINGS = SECONDORDER_SETTNGS
-LOCAL_SETTING = []
+SHARED_SETTINGS = SECONDORDER_SETTINGS
+LOCAL_SETTINGS = []
 
-DiagGGN_SETTINGS = SHARED_SETTINGS + LOCAL_SETTING
+DiagGGN_SETTINGS = SHARED_SETTINGS + LOCAL_SETTINGS
--- test/extensions/secondorder/diag_hessian/diagh_settings.py
+++ test/extensions/secondorder/diag_hessian/diagh_settings.py
@@ -4,11 +4,11 @@ The tests are taken from `test.extensions.secondorder.secondorder_settings`,
 but additional custom tests can be defined here by appending it to the list.
 """
 
-from test.extensions.secondorder.secondorder_settings import SECONDORDER_SETTNGS
+from test.extensions.secondorder.secondorder_settings import SECONDORDER_SETTINGS
 
 DiagHESSIAN_SETTINGS = []
 
-SHARED_SETTINGS = SECONDORDER_SETTNGS
+SHARED_SETTINGS = SECONDORDER_SETTINGS
 LOCAL_SETTING = []
 
 DiagHESSIAN_SETTINGS = SHARED_SETTINGS + LOCAL_SETTING
