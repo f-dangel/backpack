74dd6254446bca70abb310d02d650d8458970c71
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2020-11-30 19:32:52 +0100

    [DOC] Modify documentation in BatchL2Grad tests

--- test/extensions/firstorder/batch_l2_grad/test_batchl2grad.py
+++ test/extensions/firstorder/batch_l2_grad/test_batchl2grad.py
@@ -1,21 +1,14 @@
-"""Test class for module Batch_l2_grad (L2 norm of batch gradients) 
-from `backpack.core.extensions.firstorder`
+"""Test class for module Batch_l2_grad (squared ℓ₂ norm of batch gradients)"""
 
-Test individual gradients for the following layers:
-- L2 norm of batch gradients of linear layers
-- L2 norm of batch gradients of convolutional layers
-
-"""
 from test.automated_test import check_sizes_and_values
-from test.extensions.problem import make_test_problems
-from test.extensions.implementation.autograd import AutogradExtensions
-from test.extensions.implementation.backpack import BackpackExtensions
 from test.extensions.firstorder.batch_l2_grad.batchl2grad_settings import (
     BATCHl2GRAD_SETTINGS,
 )
+from test.extensions.implementation.autograd import AutogradExtensions
+from test.extensions.implementation.backpack import BackpackExtensions
+from test.extensions.problem import make_test_problems
 
 import pytest
-
 
 PROBLEMS = make_test_problems(BATCHl2GRAD_SETTINGS)
 IDS = [problem.make_id() for problem in PROBLEMS]
@@ -23,7 +16,7 @@ IDS = [problem.make_id() for problem in PROBLEMS]
 
 @pytest.mark.parametrize("problem", PROBLEMS, ids=IDS)
 def test_batch_l2_grad(problem):
-    """Test l2 norm of individual gradients
+    """Test squared ℓ₂ norm of individual gradients.
 
     Args:
         problem (ExtensionsTestProblem): Problem for extension test.
