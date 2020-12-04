0d9767da3f727ef893f48699051fc8bcb42055af
Shrisha Bharadwaj <coolshrisha@gmail.com>
2020-12-04 18:51:29 +0100

    Automate some Conv+activation test settings (#117)
    * ADD: initial setup
    
    * ADD: conv_transpose formula
    
    * FIX: remove formula
    
    * ADD: change firstorder_settings
    
    * FIX: black and flake
    
    * ADD: trial settings
    
    * test
    
    * change in lambda function
    
    * FIX: automate settings creation
    
    * Less white space
    
    Co-authored-by: Felix Dangel <48687646+f-dangel@users.noreply.github.com>

--- test/extensions/secondorder/diag_ggn/diaggnn_settings.py
+++ test/extensions/secondorder/diag_ggn/diaggnn_settings.py
@@ -7,11 +7,12 @@ but additional custom tests can be defined here by appending it to the list.
 from test.extensions.secondorder.secondorder_settings import SECONDORDER_SETTINGS
 from torch.nn import ELU, SELU
 
-from test.extensions.automated_settings import make_simple_cnn_setting
+from test.extensions.automated_settings import make_simple_act_setting
 
 DiagGGN_SETTINGS = []
 
 SHARED_SETTINGS = SECONDORDER_SETTINGS
+
 LOCAL_SETTINGS = []
 
 ###############################################################################
@@ -21,6 +22,6 @@ activations = [ELU, SELU]
 
 for act in activations:
     for bias in [True, False]:
-        LOCAL_SETTINGS.append(make_simple_cnn_setting(act, bias=bias))
+        LOCAL_SETTINGS.append(make_simple_act_setting(act, bias=bias))
 
 DiagGGN_SETTINGS = SHARED_SETTINGS + LOCAL_SETTINGS
