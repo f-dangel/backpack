ce9d5dab54cb8aa67b8f14c687b954a95952ad3e
sbharadwajj <coolshrisha@gmail.com>
2021-01-05 09:27:41 +0100

    ADD: maxpoolnd extension for diag_h with tests

--- test/extensions/automated_settings.py
+++ test/extensions/automated_settings.py
@@ -74,3 +74,51 @@ def make_simple_cnn_setting(input_size, conv_class, conv_params):
     }
 
     return dict_setting
+
+
+def make_simple_pooling_setting(input_size, conv_class, pool_cls, pool_params):
+    """
+    input_size: tuple of input size of (N*C*Image Size)
+    conv_class: convolutional class
+    conv_params: configurations for convolutional class
+    return: simple CNN Network
+
+    This function is used to automatically create a
+    simple CNN Network consisting of CNN & Linear layer
+    for different convolutional layers.
+    It is used to test `test.extensions`.
+    """
+
+    def make_cnn(conv_class, output_size, conv_params, pool_cls, pool_params):
+        """Note: output class size is assumed to be 5"""
+        return torch.nn.Sequential(
+            conv_class(*conv_params),
+            torch.nn.ReLU(),
+            pool_cls(*pool_params),
+            torch.nn.Flatten(),
+            torch.nn.Linear(output_size, 5),
+        )
+
+    def get_output_shape(module, module_params, input, pool, pool_params):
+        """ returns the output shape for a given layer"""
+        output_1 = module(*module_params)(input)
+        output = pool_cls(*pool_params)(output_1)
+        return output.numel() // output.shape[0]
+
+    conv_params = (3, 2, 2)
+    input = torch.rand(input_size)
+    output_size = get_output_shape(
+        conv_class, conv_params, input, pool_cls, pool_params
+    )
+
+    dict_setting = {
+        "input_fn": lambda: torch.rand(input_size),
+        "module_fn": lambda: make_cnn(
+            conv_class, output_size, conv_params, pool_cls, pool_params
+        ),
+        "loss_function_fn": lambda: torch.nn.CrossEntropyLoss(reduction="sum"),
+        "target_fn": lambda: classification_targets((3,), 5),
+        "id_prefix": "automated-simple-cnn",
+    }
+
+    return dict_setting
--- test/extensions/secondorder/diag_hessian/diagh_settings.py
+++ test/extensions/secondorder/diag_hessian/diagh_settings.py
@@ -5,8 +5,19 @@ but additional custom tests can be defined here by appending it to the list.
 """
 
 from test.extensions.secondorder.secondorder_settings import SECONDORDER_SETTINGS
-from torch.nn import LogSigmoid
-from test.extensions.automated_settings import make_simple_act_setting
+from torch.nn import (
+    LogSigmoid,
+    Conv1d,
+    Conv2d,
+    Conv3d,
+    MaxPool1d,
+    MaxPool2d,
+    MaxPool3d,
+)
+from test.extensions.automated_settings import (
+    make_simple_act_setting,
+    make_simple_pooling_setting,
+)
 
 DiagHESSIAN_SETTINGS = []
 
@@ -14,5 +25,24 @@ SHARED_SETTINGS = SECONDORDER_SETTINGS
 LOCAL_SETTINGS = []
 LOCAL_SETTINGS.append(make_simple_act_setting(LogSigmoid, bias=True))
 LOCAL_SETTINGS.append(make_simple_act_setting(LogSigmoid, bias=False))
-
+###############################################################################
+#                         test setting: Pooling Layers                       #
+###############################################################################
+LOCAL_SETTINGS += [
+    make_simple_pooling_setting((3, 3, 7), Conv1d, MaxPool1d, (2, 1)),
+    make_simple_pooling_setting((3, 3, 7), Conv1d, MaxPool1d, (2, 1, 0, 2)),
+    make_simple_pooling_setting(
+        (3, 3, 7), Conv1d, MaxPool1d, (2, 1, 0, 2, False, True)
+    ),
+    make_simple_pooling_setting((3, 3, 11, 11), Conv2d, MaxPool2d, (2, 1)),
+    make_simple_pooling_setting((3, 3, 7, 7), Conv2d, MaxPool2d, (2, 1, 0, 2)),
+    make_simple_pooling_setting(
+        (3, 3, 7, 7), Conv2d, MaxPool2d, (2, 1, 0, 2, False, True)
+    ),
+    make_simple_pooling_setting((3, 3, 7, 7, 7), Conv3d, MaxPool3d, (2, 1)),
+    make_simple_pooling_setting((3, 3, 7, 7, 7), Conv3d, MaxPool3d, (2, 1, 0, 2)),
+    make_simple_pooling_setting(
+        (3, 3, 7, 7, 7), Conv3d, MaxPool3d, (2, 1, 0, 2, False, True)
+    ),
+]
 DiagHESSIAN_SETTINGS = SHARED_SETTINGS + LOCAL_SETTINGS
