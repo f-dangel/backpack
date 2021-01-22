38c7295a6f49f770e9ffa75651b3f1baacbdbd7e
Shrisha Bharadwaj <coolshrisha@gmail.com>
2021-01-22 09:49:25 +0100

    DiagGGN{Exact,MC} support for MaxpoolNd (#125)
    * ADD: test.yaml
    
    * FIX: syntax
    
    * FIX: if-else condition
    
    * FIX: syntax
    
    * syntax
    
    * new syntax for if
    
    * synatax
    
    * remove indentation
    
    * ADD: else-block
    
    * ADD: option for test-light
    
    * FIX: add if condition for test-light
    
    * change if conditon
    
    * ADD: different python versions
    
    * syntax of uses
    
    * fix: matrix syntax
    
    * ADD: coveralls
    
    * FIX: coveralls syntax
    
    * syntax
    
    * coveralls syntax
    
    * FIX: uses
    
    * remove: finish
    
    * change: uses from v1 to development
    
    * remove: uses
    
    * add: uses
    
    * add: uses
    
    * add: uses @ master
    
    * ADDL: run coveralls
    
    * syntax
    
    * remove finish
    
    * GITHUB TOKEN
    
    * github token
    
    * finish
    
    * fix: syntax
    
    * coveralls: finish
    
    * FIX: PR issues
    
    * remove travis
    
    * capitalize character
    
    * initial commit
    
    * ADD: maxpoolnd generalization and tests
    
    * ADD: maxpoolnd extension for diag_h with tests
    
    * removed zeropad1d file
    
    * ADD: diag_ggn extensions for maxpoolnd
    
    * remove zeropad1d
    
    * ADD: ea_jac_t
    
    * FIX: coveralls syntax

--- backpack/extensions/secondorder/diag_ggn/pooling.py
+++ backpack/extensions/secondorder/diag_ggn/pooling.py
@@ -1,11 +1,23 @@
 from backpack.core.derivatives.avgpool2d import AvgPool2DDerivatives
+from backpack.core.derivatives.maxpool1d import MaxPool1DDerivatives
 from backpack.core.derivatives.maxpool2d import MaxPool2DDerivatives
+from backpack.core.derivatives.maxpool3d import MaxPool3DDerivatives
 from backpack.extensions.secondorder.diag_ggn.diag_ggn_base import DiagGGNBaseModule
+
+
+class DiagGGNMaxPool1d(DiagGGNBaseModule):
+    def __init__(self):
+        super().__init__(derivatives=MaxPool1DDerivatives())
 
 
 class DiagGGNMaxPool2d(DiagGGNBaseModule):
     def __init__(self):
         super().__init__(derivatives=MaxPool2DDerivatives())
+
+
+class DiagGGNMaxPool3d(DiagGGNBaseModule):
+    def __init__(self):
+        super().__init__(derivatives=MaxPool3DDerivatives())
 
 
 class DiagGGNAvgPool2d(DiagGGNBaseModule):
