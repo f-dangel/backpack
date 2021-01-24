d56440339bcbd328322dc5d3d0e41d9e723961dc
sbharadwajj <coolshrisha@gmail.com>
2021-01-24 13:39:50 +0100

    fix merge conflicts

--- backpack/extensions/secondorder/diag_hessian/pooling.py
+++ backpack/extensions/secondorder/diag_hessian/pooling.py
@@ -1,10 +1,7 @@
 from backpack.core.derivatives.avgpool1d import AvgPool1DDerivatives
 from backpack.core.derivatives.avgpool2d import AvgPool2DDerivatives
-<<<<<<< HEAD
 from backpack.core.derivatives.avgpool3d import AvgPool3DDerivatives
-=======
 from backpack.core.derivatives.maxpool1d import MaxPool1DDerivatives
->>>>>>> 0994d8159952865a571e6af59bb5297217955c90
 from backpack.core.derivatives.maxpool2d import MaxPool2DDerivatives
 from backpack.core.derivatives.maxpool3d import MaxPool3DDerivatives
 from backpack.extensions.secondorder.diag_hessian.diag_h_base import DiagHBaseModule
@@ -20,15 +17,14 @@ class DiagHAvgPool2d(DiagHBaseModule):
         super().__init__(derivatives=AvgPool2DDerivatives())
 
 
-<<<<<<< HEAD
 class DiagHAvgPool3d(DiagHBaseModule):
     def __init__(self):
         super().__init__(derivatives=AvgPool3DDerivatives())
-=======
+
+
 class DiagHMaxPool1d(DiagHBaseModule):
     def __init__(self):
         super().__init__(derivatives=MaxPool1DDerivatives())
->>>>>>> 0994d8159952865a571e6af59bb5297217955c90
 
 
 class DiagHMaxPool2d(DiagHBaseModule):
