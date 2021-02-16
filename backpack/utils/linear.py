8eac3b64a45578dd836fb145983df91426defcb4
sbharadwajj <coolshrisha@gmail.com>
2021-02-16 11:45:21 +0100

    fix PR issue

--- backpack/utils/linear.py
+++ backpack/utils/linear.py
@@ -1,14 +1,14 @@
 from torch import einsum
 
 
-def extract_weight_diagonal(module, backproped, sum_batch):
+def extract_weight_diagonal(module, backproped, sum_batch=True):
     if sum_batch:
         return einsum("vno,ni->oi", (backproped ** 2, module.input0 ** 2))
     else:
         return einsum("vno,ni->noi", (backproped ** 2, module.input0 ** 2))
 
 
-def extract_bias_diagonal(module, backproped, sum_batch):
+def extract_bias_diagonal(module, backproped, sum_batch=True):
     if sum_batch:
         return einsum("vno->o", backproped ** 2)
     else:
