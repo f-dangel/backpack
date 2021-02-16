7d82f5b5db90f8123344e41a57b5828fcdba5ee4
Shrisha Bharadwaj <coolshrisha@gmail.com>
2021-02-16 08:20:15 +0100

    [OPT] Prefer torch unfold for 2d convolutions (#136)

--- backpack/extensions/secondorder/diag_ggn/convnd.py
+++ backpack/extensions/secondorder/diag_ggn/convnd.py
@@ -12,6 +12,9 @@ class DiagGGNConvND(DiagGGNBaseModule):
         return convUtils.extract_bias_diagonal(module, sqrt_ggn, self.N)
 
     def weight(self, ext, module, grad_inp, grad_out, backproped):
-        X = convUtils.unfold_by_conv(module.input0, module)
+        if self.N == 2:
+            X = convUtils.unfold_func(module)(module.input0)
+        else:
+            X = convUtils.unfold_by_conv(module.input0, module)
         weight_diag = convUtils.extract_weight_diagonal(module, X, backproped, self.N)
         return weight_diag
