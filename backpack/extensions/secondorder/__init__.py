32c957af1faa6fd099c85eaa21ddb5bd06c243fe
sbharadwajj <coolshrisha@gmail.com>
2021-02-23 14:03:21 +0100

    fix black and flake8

--- backpack/extensions/secondorder/__init__.py
+++ backpack/extensions/secondorder/__init__.py
@@ -18,7 +18,13 @@ The implemented extensions are
 - The diagonal of the Hessian :func:`DiagHessian <backpack.extensions.DiagHessian>`
 """
 
-from .diag_ggn import DiagGGNExact, BatchDiagGGN, BatchDiagGGNExact, DiagGGNMC, BatchDiagGGNMC
+from .diag_ggn import (
+    DiagGGNExact,
+    BatchDiagGGN,
+    BatchDiagGGNExact,
+    DiagGGNMC,
+    BatchDiagGGNMC,
+)
 from .diag_hessian import DiagHessian
 from .hbp import HBP, KFAC, KFLR, KFRA
 
