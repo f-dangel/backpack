6ffecff629c47ef7682ebdc07a07033361d73515
tim <tim@gruber-schaefer.de>
2021-04-19 14:03:34 +0200

    test mem of rnn

--- masterThesis/rnn_mem_test.py
+++ masterThesis/rnn_mem_test.py
@@ -1,10 +1,14 @@
 import torch
+import sys
 
+from pytorch_memlab import LineProfiler
 """
 This script test whether the results of all layers are saved 
 during forward pass of an multilayer RNN.
 To this the memory usage of RNN(num_layers) is compared with num_layers*RNN.
 """
+
+
 num_layers = 10
 input_size = 5
 hidden_size = 5
@@ -14,3 +18,12 @@ for i in range(num_layers):
     input_size = input_size if i == 0 else hidden_size
     RNN_list.append(torch.nn.RNN(input_size, hidden_size, 1))
 
+seq_len = 10
+batch_size = 1000
+input_rand = torch.randn(seq_len, batch_size, input_size)
+input_rand.requires_grad = True
+
+RNN_stacked.forward(input_rand)
+
+
+print(sys.getsizeof(RNN_stacked))
