3472f074a8ffec16bc43fad3c06bd70ae160c548
Tim Schäfer <ti.schaefer@student.uni-tuebingen.de>
2021-04-29 14:22:52 +0200

    batch axis in test/extensions

--- test/extensions/implementation/autograd.py
+++ test/extensions/implementation/autograd.py
@@ -11,7 +11,12 @@ class AutogradExtensions(ExtensionsImplementation):
     """Extension implementations with autograd."""
 
     def batch_grad(self):
-        N = self.problem.input.shape[0]
+        """Calculates the individual gradients.
+
+        Returns:
+            list[torch.nn.Tensor]: batch_grads
+        """
+        N = self.problem.input.shape[self.problem.axis_batch]
         batch_grads = [
             torch.zeros(N, *p.size()).to(self.problem.device)
             for p in self.problem.model.parameters()
