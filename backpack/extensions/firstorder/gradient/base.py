73919e96397a32e01ef6d7ae53ae3549c82e5382
Tim Schäfer <ti.schaefer@student.uni-tuebingen.de>
2021-04-30 13:52:49 +0200

    add overwrite check and docs

--- backpack/extensions/firstorder/gradient/base.py
+++ backpack/extensions/firstorder/gradient/base.py
@@ -2,26 +2,33 @@
 from backpack.extensions.firstorder.base import FirstOrderModuleExtension
 
 
-class BatchGradBase(FirstOrderModuleExtension):
+class GradBaseModule(FirstOrderModuleExtension):
     """Passes the calls for the parameters to the derivatives class.
 
-    This class implements functions with method name from params.
-    If child class wants to overwrite these methods,
-    this class needs to check if the method is already implemented.
+    This class implements functions with method names from params.
+
+    If child class wants to overwrite these methods
+    - for example to support an additional external module -
+    it can do so using the interface for parameter "param1"
+    param1(ext, module, g_inp, g_out, bpQuantities):
+        return batch_grads
+    In this case, the method is not overwritten by this class.
     """
 
     def __init__(self, derivatives, params=None):
         """Initializes all methods.
 
+        If the param method has already been defined, it is left unchanged.
+
         Args:
-            derivatives
-                (backpack.core.derivatives.basederivatives.BaseParameterDerivatives):
+            derivatives(backpack.core.derivatives.basederivatives.BaseParameterDerivatives):
                 Derivatives object assigned to self.derivatives.
             params (list[str]): list of strings with parameter names. Defaults to None.
         """
         self.derivatives = derivatives
         for param_str in params:
-            self.__setattr__(param_str, self._make_function(param_str))
+            if not hasattr(self, param_str):
+                self.__setattr__(param_str, self._make_function(param_str))
         super().__init__(params=params)
 
     def _make_function(self, param):
