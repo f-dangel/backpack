d3ce1c1d388871d8baf2bd1920539197d4ab0990
Tim Schäfer <ti.schaefer@student.uni-tuebingen.de>
2021-05-20 11:19:35 +0200

    fix bug, RNN single derivative test

--- test/core/derivatives/problem.py
+++ test/core/derivatives/problem.py
@@ -132,6 +132,10 @@ class DerivativesTestProblem:
         else:
             output = module(input, target)
 
+        if isinstance(output, tuple):
+            # is true for RNN,GRU,LSTM which return tuple (output, ...)
+            output = output[0]
+
         return output.shape
 
     def is_loss(self):
@@ -153,9 +157,9 @@ class DerivativesTestProblem:
         else:
             output = self.module(input)
 
-        """if isinstance(output, tuple):
+        if isinstance(output, tuple):
             # is true for RNN,GRU,LSTM which return tuple (output, ...)
-            output = output[0]"""
+            output = output[0]
 
         return input, output, dict(self.module.named_parameters())
 
