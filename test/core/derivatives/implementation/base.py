655014d8999870d4e94f0cf78b3bd75fa75d47b3
Tim Schäfer <tim@gruber-schaefer.de>
2021-06-08 09:38:34 +0200

    [core] Add RNN derivatives (#156)
    Auxiliary:
    - Improve documentation
    - Move mapping between layers and derivatives from main lib to test suite
    
    ---
    * RNN: derivatives and derivatives test
    
    * additional test for RNN derivatives
    
    * incorporate suggestions
    
    * move RNN CUDA workaround to AutogradDerivatives
    
    * delete unnecessary variable
    
    * fix type hint for hessian
    
    * remove white spaces in einsums
    
    * make format
    
    Co-authored-by: Tim Schäfer <ti.schaefer@student.uni-tuebingen.de>

--- test/core/derivatives/implementation/base.py
+++ test/core/derivatives/implementation/base.py
@@ -1,23 +1,165 @@
-class DerivativesImplementation:
+"""Contains DerivativesImplementation, the base class for autograd and backpack."""
+from abc import ABC, abstractmethod
+
+from torch import Tensor
+
+
+class DerivativesImplementation(ABC):
     """Base class for autograd and BackPACK implementations."""
 
     def __init__(self, problem):
+        """Initialization.
+
+        Args:
+            problem: test problem
+        """
         self.problem = problem
 
-    def jac_mat_prod(self, mat):
+    @abstractmethod
+    def jac_mat_prod(self, mat: Tensor) -> Tensor:
+        """Vectorized product of input-output-Jacobian and a matrix.
+
+        Args:
+            mat: matrix: the vectors along its leading dimension will be multiplied.
+
+        Returns:
+            Tensor representing the result of Jacobian-vector product.
+                product[v] = J @ mat[v]
+        """
         raise NotImplementedError
 
-    def jac_t_mat_prod(self, mat):
+    @abstractmethod
+    def jac_t_mat_prod(self, mat: Tensor) -> Tensor:
+        """Vectorized product of transposed jacobian and matrix.
+
+        Args:
+            mat: matrix: the vectors along its leading dimension will be multiplied.
+
+        Returns:
+            Tensor representing the result of Jacobian-vector product.
+                product[v] = mat[v] @ J
+        """
         raise NotImplementedError
 
-    def weight_jac_t_mat_prod(self, mat, sum_batch):
+    @abstractmethod
+    def weight_jac_t_mat_prod(self, mat: Tensor, sum_batch: bool) -> Tensor:
+        """Product of jacobian and matrix.
+
+        Args:
+            mat: matrix
+            sum_batch: whether to sum along batch axis
+
+        Returns:
+            product
+        """
         raise NotImplementedError
 
-    def bias_jac_t_mat_prod(self, mat, sum_batch):
+    @abstractmethod
+    def bias_jac_t_mat_prod(self, mat: Tensor, sum_batch: bool) -> Tensor:
+        """Product of jacobian and matrix.
+
+        Args:
+            mat: matrix
+            sum_batch: whether to sum along batch axis
+
+        Returns:
+            product
+        """
         raise NotImplementedError
 
-    def weight_jac_mat_prod(self, mat):
+    @abstractmethod
+    def weight_jac_mat_prod(self, mat: Tensor) -> Tensor:
+        """Product of jacobian and matrix.
+
+        Args:
+            mat: matrix
+
+        Returns:
+            product
+        """
         raise NotImplementedError
 
-    def bias_jac_mat_prod(self, mat):
+    @abstractmethod
+    def bias_jac_mat_prod(self, mat: Tensor) -> Tensor:
+        """Product of jacobian and matrix.
+
+        Args:
+            mat: matrix
+
+        Returns:
+            product
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def bias_ih_l0_jac_t_mat_prod(self, mat: Tensor, sum_batch: bool) -> Tensor:
+        """Product of jacobian and matrix.
+
+        Args:
+            mat: matrix
+            sum_batch: whether to sum along batch axis
+
+        Returns:
+            product
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def bias_hh_l0_jac_t_mat_prod(self, mat: Tensor, sum_batch: bool) -> Tensor:
+        """Product of jacobian and matrix.
+
+        Args:
+            mat: matrix
+            sum_batch: whether to sum along batch axis
+
+        Returns:
+            product
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def weight_ih_l0_jac_t_mat_prod(self, mat: Tensor, sum_batch: bool) -> Tensor:
+        """Product of jacobian and matrix.
+
+        Args:
+            mat: matrix
+            sum_batch: whether to sum along batch axis
+
+        Returns:
+            product
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def weight_hh_l0_jac_t_mat_prod(self, mat: Tensor, sum_batch: bool) -> Tensor:
+        """Product of jacobian and matrix.
+
+        Args:
+            mat: matrix
+            sum_batch: whether to sum along batch axis
+
+        Returns:
+            product
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def ea_jac_t_mat_jac_prod(self, mat: Tensor) -> Tensor:
+        """Product of ea jacobian with matrix.
+
+        Args:
+            mat: matrix
+
+        Returns:
+            product
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def sum_hessian(self) -> Tensor:
+        """Sum of hessians.
+
+        Returns:
+            the sum of hessians
+        """
         raise NotImplementedError
