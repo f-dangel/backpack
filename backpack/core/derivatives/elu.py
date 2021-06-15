9ef603c2d6a96859bb140ecd6826a8e0898b22b0
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2021-06-15 13:10:41 +0200

    [ref] Make ELU and SELU derivatives more similar

--- backpack/core/derivatives/elu.py
+++ backpack/core/derivatives/elu.py
@@ -1,4 +1,4 @@
-from torch import exp, gt
+from torch import exp, le, ones_like, zeros_like
 
 from backpack.core.derivatives.elementwise import ElementwiseDerivatives
 
@@ -10,15 +10,18 @@ class ELUDerivatives(ElementwiseDerivatives):
 
     def df(self, module, g_inp, g_out):
         """First ELU derivative: `ELU'(x) = alpha * e^x if x <= 0 else 1`."""
-        pos = gt(module.input0, 0)
-        dtype = module.input0.dtype
+        non_pos = le(module.input0, 0)
 
-        return pos.to(dtype) + module.alpha * pos.logical_not().to(dtype) * exp(
-            module.input0
-        )
+        result = ones_like(module.input0)
+        result[non_pos] = module.alpha * exp(module.input0[non_pos])
+
+        return result
 
     def d2f(self, module, g_inp, g_out):
         """Second ELU derivative: `ELU''(x) = alpha * e^x if x <= 0 else 0`."""
-        return self.df(module, g_inp, g_out) - gt(module.input0, 0).to(
-            module.input0.dtype
-        )
+        non_pos = le(module.input0, 0)
+
+        result = zeros_like(module.input0)
+        result[non_pos] = module.alpha * exp(module.input0[non_pos])
+
+        return result
