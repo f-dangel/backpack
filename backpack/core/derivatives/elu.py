cf017453cb4cd201a3dcb89e6e96ee0e913429be
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2021-06-15 12:40:36 +0200

    [ref] Rewrite ELU derivatives

--- backpack/core/derivatives/elu.py
+++ backpack/core/derivatives/elu.py
@@ -10,10 +10,15 @@ class ELUDerivatives(ElementwiseDerivatives):
 
     def df(self, module, g_inp, g_out):
         """First ELU derivative: `ELU'(x) = alpha * e^x if x <= 0 else 1`."""
-        df_ELU = gt(module.input0, 0).float()
-        df_ELU[df_ELU == 0] = module.alpha * exp(module.input0[df_ELU == 0])
-        return df_ELU
+        pos = gt(module.input0, 0)
+        dtype = module.input0.dtype
+
+        return pos.to(dtype) + module.alpha * pos.logical_not().to(dtype) * exp(
+            module.input0
+        )
 
     def d2f(self, module, g_inp, g_out):
         """Second ELU derivative: `ELU''(x) = alpha * e^x if x <= 0 else 0`."""
-        return self.df(module, g_inp, g_out) - gt(module.input0, 0).float()
+        return self.df(module, g_inp, g_out) - gt(module.input0, 0).to(
+            module.input0.dtype
+        )
