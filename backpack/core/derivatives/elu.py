8d54239b565f8d53cad09cce2152315926fe80b6
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2021-06-15 12:15:30 +0200

    [core] Fix math of ELU second derivative

--- backpack/core/derivatives/elu.py
+++ backpack/core/derivatives/elu.py
@@ -9,11 +9,11 @@ class ELUDerivatives(ElementwiseDerivatives):
         return False
 
     def df(self, module, g_inp, g_out):
-        """First ELU derivative: `ELU'(x) = alpha * e^x if x < 0 else 1`."""
+        """First ELU derivative: `ELU'(x) = alpha * e^x if x <= 0 else 1`."""
         df_ELU = gt(module.input0, 0).float()
         df_ELU[df_ELU == 0] = module.alpha * exp(module.input0[df_ELU == 0])
         return df_ELU
 
     def d2f(self, module, g_inp, g_out):
-        """Second ELU derivative: `ELU''(x) = alpha * e^x if x < 0 else 1`."""
-        return self.df(module, g_inp, g_out)
+        """Second ELU derivative: `ELU''(x) = alpha * e^x if x <= 0 else 0`."""
+        return self.df(module, g_inp, g_out) - gt(module.input0, 0).float()
