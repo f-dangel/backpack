343974999387b5709e42c583bd73bf7ee0c1e7d3
Tim Schäfer <tim@gruber-schaefer.de>
2021-06-17 11:56:11 +0200

    improve adaptive_avg_pool_nd.py

--- backpack/core/derivatives/adaptive_avg_pool_nd.py
+++ backpack/core/derivatives/adaptive_avg_pool_nd.py
@@ -10,14 +10,6 @@ from backpack.core.derivatives.avgpoolnd import AvgPoolNDDerivatives
 class AdaptiveAvgPoolNDDerivatives(AvgPoolNDDerivatives):
     """Implements the derivatives for AdaptiveAvgPool."""
 
-    def __init__(self, N: int):
-        """Initialization.
-
-        Args:
-            N: number of dimensions
-        """
-        super(AdaptiveAvgPoolNDDerivatives, self).__init__(N=N)
-
     def check_parameters(
         self, module: Union[AdaptiveAvgPool1d, AdaptiveAvgPool2d, AdaptiveAvgPool3d]
     ) -> None:
@@ -35,29 +27,20 @@ class AdaptiveAvgPoolNDDerivatives(AvgPoolNDDerivatives):
             NotImplementedError: if the given shapes do not match
         """
         shape_input: Size = module.input0.shape
-        shape_target: Size = module.output.shape
+        shape_output: Size = module.output.shape
 
         # check length of input shape
         if not len(shape_input) == (self.N + 2):
             raise NotImplementedError(
                 f"input must be (batch_size, C, ...) with ... {self.N} dimensions"
             )
-        # check length of target shape
-        if not len(shape_target) == (self.N + 2):
-            raise NotImplementedError(
-                f"shape of target should have {self.N} dimensions"
-            )
 
         # check if input shape is multiple of output shape
-        for n in range(self.N):
-            in_dim: int = shape_input[2 + n]
-            out_dim: int = shape_target[2 + n]
-            if (in_dim % out_dim) != 0:
-                raise NotImplementedError(
-                    "Not supported in BackPACK: Input shape of AdaptiveAvgPool "
-                    "must be multiple of output shape. Only in this case, there is "
-                    "an equivalent AvgPool module."
-                )
+        if any([shape_input[2 + n] % shape_output[2 + n] != 0 for n in range(self.N)]):
+            raise NotImplementedError(
+                f"No equivalent AvgPool (unadaptive): Input shape ({shape_input}) "
+                f"must be multiple of output shape ({shape_output})."
+            )
 
     def get_parameters(
         self, module: Union[AdaptiveAvgPool1d, AdaptiveAvgPool2d, AdaptiveAvgPool3d]
@@ -84,8 +67,9 @@ class AdaptiveAvgPoolNDDerivatives(AvgPoolNDDerivatives):
             out_dim: int = shape_target[2 + n]
             stride.append(in_dim // out_dim)
             kernel_size.append(in_dim - (out_dim - 1) * stride[n])
+        padding: List[int] = [0 for _ in range(self.N)]
 
-        return stride, kernel_size, list((0,) * self.N)
+        return stride, kernel_size, padding
 
 
 class AdaptiveAvgPool1dDerivatives(AdaptiveAvgPoolNDDerivatives):
@@ -93,7 +77,7 @@ class AdaptiveAvgPool1dDerivatives(AdaptiveAvgPoolNDDerivatives):
 
     def __init__(self):
         """Initialization."""
-        super(AdaptiveAvgPool1dDerivatives, self).__init__(N=1)
+        super().__init__(N=1)
 
 
 class AdaptiveAvgPool2dDerivatives(AdaptiveAvgPoolNDDerivatives):
@@ -101,7 +85,7 @@ class AdaptiveAvgPool2dDerivatives(AdaptiveAvgPoolNDDerivatives):
 
     def __init__(self):
         """Initialization."""
-        super(AdaptiveAvgPool2dDerivatives, self).__init__(N=2)
+        super().__init__(N=2)
 
 
 class AdaptiveAvgPool3dDerivatives(AdaptiveAvgPoolNDDerivatives):
@@ -109,4 +93,4 @@ class AdaptiveAvgPool3dDerivatives(AdaptiveAvgPoolNDDerivatives):
 
     def __init__(self):
         """Initialization."""
-        super(AdaptiveAvgPool3dDerivatives, self).__init__(N=3)
+        super().__init__(N=3)
