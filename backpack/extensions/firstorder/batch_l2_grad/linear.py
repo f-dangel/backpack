f5dcd5936a8d9415c44a8dcfce6253556ece7e14
Tim Schäfer <tim@gruber-schaefer.de>
2021-06-22 08:11:34 +0200

    refactor linear.py

--- backpack/extensions/firstorder/batch_l2_grad/linear.py
+++ backpack/extensions/firstorder/batch_l2_grad/linear.py
@@ -1,6 +1,7 @@
 """Contains batch_l2 extension for Linear."""
 from torch import einsum
 
+from backpack.core.derivatives.linear import LinearDerivatives
 from backpack.extensions.firstorder.batch_l2_grad.batch_l2_base import BatchL2Base
 
 
@@ -9,23 +10,7 @@ class BatchL2Linear(BatchL2Base):
 
     def __init__(self):
         """Initialization."""
-        super().__init__(params=["bias", "weight"])
-
-    def bias(self, ext, module, g_inp, g_out, backproped):
-        """batch_l2 for bias.
-
-        Args:
-            ext: backpack extension
-            module: module
-            g_inp: input gradients
-            g_out: output gradients
-            backproped: backpropagation quantities
-
-        Returns:
-            batch_l2 for bias
-        """
-        C_axis = 1
-        return (g_out[0] ** 2).sum(C_axis)
+        super().__init__(params=["bias", "weight"], derivatives=LinearDerivatives())
 
     def weight(self, ext, module, g_inp, g_out, backproped):
         """batch_l2 for weight.
