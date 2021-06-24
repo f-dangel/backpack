eded6b5a92f6788f758d838918e672158cac84a1
Tim Schäfer <tim@gruber-schaefer.de>
2021-06-24 12:28:53 +0200

    move running stats initialization from problem.py to batch_norm_settings.py

--- test/core/derivatives/batch_norm_settings.py
+++ test/core/derivatives/batch_norm_settings.py
@@ -12,8 +12,21 @@ Optional entries:
     "id_prefix" (str): Prefix to be included in the test name.
     "seed" (int): seed for the random number for torch.rand
 """
+from typing import Union
 
 import torch
+from torch.nn import BatchNorm1d, BatchNorm2d, BatchNorm3d
+
+
+def _initialize_training_false(
+    module: Union[BatchNorm1d, BatchNorm2d, BatchNorm3d]
+) -> Union[BatchNorm1d, BatchNorm2d, BatchNorm3d]:
+    module.running_mean = torch.rand_like(module.running_mean)
+    module.running_var = torch.rand_like(module.running_var)
+    module.weight.data = torch.rand_like(module.weight)
+    module.bias.data = torch.rand_like(module.bias)
+    return module.train(False)
+
 
 BATCH_NORM_SETTINGS = [
     {
@@ -33,17 +46,23 @@ BATCH_NORM_SETTINGS = [
         "input_fn": lambda: torch.rand(size=(5, 7, 3, 4, 2)),
     },
     {
-        "module_fn": lambda: torch.nn.BatchNorm1d(num_features=7).train(mode=False),
+        "module_fn": lambda: _initialize_training_false(
+            torch.nn.BatchNorm1d(num_features=7)
+        ),
         "input_fn": lambda: torch.rand(size=(5, 7)),
         "id_prefix": "training=False",
     },
     {
-        "module_fn": lambda: torch.nn.BatchNorm1d(num_features=7).train(mode=False),
+        "module_fn": lambda: _initialize_training_false(
+            torch.nn.BatchNorm1d(num_features=7)
+        ),
         "input_fn": lambda: torch.rand(size=(5, 7, 4)),
         "id_prefix": "training=False",
     },
     {
-        "module_fn": lambda: torch.nn.BatchNorm2d(num_features=7).train(mode=False),
+        "module_fn": lambda: _initialize_training_false(
+            torch.nn.BatchNorm2d(num_features=7)
+        ),
         "input_fn": lambda: torch.rand(size=(5, 7, 3, 4)),
         "id_prefix": "training=False",
     },
