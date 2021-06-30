4f126b0944366671abfb291cecd5a9ff4516e7d3
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2021-06-30 16:44:25 +0200

    [FIX] Check `LSTM` `proj_size` only for `torch>=1.8.0`
    Resolves https://github.com/fKunstner/backpack-discuss/issues/112.

--- backpack/utils/__init__.py
+++ backpack/utils/__init__.py
@@ -1,1 +1,6 @@
 """Contains utility functions."""
+
+from pkg_resources import get_distribution, packaging
+
+TORCH_VERSION = packaging.version.parse(get_distribution("torch").version)
+VERSION_1_8_0 = packaging.version.parse("1.8.0")
