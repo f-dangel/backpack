c19a63e620f932e94fa1371145d5f718e04c7353
Tim Schäfer <tim@gruber-schaefer.de>
2021-07-06 13:41:37 +0200

    Merge: fix diag_ggn_settings.py

--- test/extensions/secondorder/diag_ggn/diag_ggn_settings.py
+++ test/extensions/secondorder/diag_ggn/diag_ggn_settings.py
@@ -10,8 +10,6 @@ Shared settings are taken from `test.extensions.secondorder.secondorder_settings
 Additional local cases can be defined here through ``LOCAL_SETTINGS``.
 """
 from test.core.derivatives.utils import classification_targets, regression_targets
-from test.extensions.automated_settings import make_simple_act_setting
-from test.core.derivatives.utils import regression_targets
 from test.extensions.secondorder.secondorder_settings import SECONDORDER_SETTINGS
 from test.utils.evaluation_mode import initialize_training_false_recursive
 
@@ -24,12 +22,19 @@ from torch.nn import (
     BatchNorm1d,
     BatchNorm2d,
     BatchNorm3d,
+    Conv2d,
+    CrossEntropyLoss,
     Flatten,
+    Linear,
+    MaxPool2d,
     MSELoss,
+    ReLU,
     Sequential,
+    Sigmoid,
 )
 
 from backpack.custom_module import branching
+from backpack.custom_module.branching import ActiveIdentity, Parallel
 from backpack.custom_module.permute import Permute
 from backpack.custom_module.reduce_tuple import ReduceTuple
 
@@ -98,74 +103,74 @@ LOCAL_SETTINGS = [
 # DiagGGNMC, BatchDiagGGNExact
 LOCAL_SETTINGS += [
     {
-        "input_fn": lambda: torch.rand(3, 10),
-        "module_fn": lambda: torch.nn.Sequential(
-            torch.nn.Linear(10, 5),
-            torch.nn.ReLU(),
+        "input_fn": lambda: rand(3, 10),
+        "module_fn": lambda: Sequential(
+            Linear(10, 5),
+            ReLU(),
             # skip connection
-            branching.Parallel(
-                branching.ActiveIdentity(),
-                torch.nn.Linear(5, 5),
+            Parallel(
+                ActiveIdentity(),
+                Linear(5, 5),
             ),
             # end of skip connection
-            torch.nn.Sigmoid(),
-            torch.nn.Linear(5, 4),
+            Sigmoid(),
+            Linear(5, 4),
         ),
-        "loss_function_fn": lambda: torch.nn.CrossEntropyLoss(),
+        "loss_function_fn": lambda: CrossEntropyLoss(),
         "target_fn": lambda: classification_targets((3,), 4),
         "id_prefix": "branching-linear",
     },
     {
-        "input_fn": lambda: torch.rand(4, 2, 6, 6),
-        "module_fn": lambda: torch.nn.Sequential(
-            torch.nn.Conv2d(2, 3, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(),
+        "input_fn": lambda: rand(4, 2, 6, 6),
+        "module_fn": lambda: Sequential(
+            Conv2d(2, 3, kernel_size=3, stride=1, padding=1),
+            ReLU(),
             # skip connection
-            branching.Parallel(
-                branching.ActiveIdentity(),
-                torch.nn.Sequential(
-                    torch.nn.Conv2d(3, 5, kernel_size=3, stride=1, padding=1),
-                    torch.nn.ReLU(),
-                    torch.nn.Conv2d(5, 3, kernel_size=3, stride=1, padding=1),
+            Parallel(
+                ActiveIdentity(),
+                Sequential(
+                    Conv2d(3, 5, kernel_size=3, stride=1, padding=1),
+                    ReLU(),
+                    Conv2d(5, 3, kernel_size=3, stride=1, padding=1),
                 ),
             ),
             # end of skip connection
-            torch.nn.MaxPool2d(kernel_size=3, stride=2),
-            torch.nn.Flatten(),
-            torch.nn.Linear(12, 5),
+            MaxPool2d(kernel_size=3, stride=2),
+            Flatten(),
+            Linear(12, 5),
         ),
-        "loss_function_fn": lambda: torch.nn.CrossEntropyLoss(),
+        "loss_function_fn": lambda: CrossEntropyLoss(),
         "target_fn": lambda: classification_targets((4,), 5),
         "id_prefix": "branching-convolution",
     },
     {
-        "input_fn": lambda: torch.rand(4, 3, 6, 6),
-        "module_fn": lambda: torch.nn.Sequential(
-            torch.nn.Conv2d(3, 2, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(),
+        "input_fn": lambda: rand(4, 3, 6, 6),
+        "module_fn": lambda: Sequential(
+            Conv2d(3, 2, kernel_size=3, stride=1, padding=1),
+            ReLU(),
             # skip connection
-            branching.Parallel(
-                branching.ActiveIdentity(),
-                torch.nn.Sequential(
-                    torch.nn.Conv2d(2, 4, kernel_size=3, stride=1, padding=1),
-                    torch.nn.Sigmoid(),
-                    torch.nn.Conv2d(4, 2, kernel_size=3, stride=1, padding=1),
+            Parallel(
+                ActiveIdentity(),
+                Sequential(
+                    Conv2d(2, 4, kernel_size=3, stride=1, padding=1),
+                    Sigmoid(),
+                    Conv2d(4, 2, kernel_size=3, stride=1, padding=1),
                     branching.Parallel(
                         branching.ActiveIdentity(),
-                        torch.nn.Sequential(
-                            torch.nn.Conv2d(2, 4, kernel_size=3, stride=1, padding=1),
-                            torch.nn.ReLU(),
-                            torch.nn.Conv2d(4, 2, kernel_size=3, stride=1, padding=1),
+                        Sequential(
+                            Conv2d(2, 4, kernel_size=3, stride=1, padding=1),
+                            ReLU(),
+                            Conv2d(4, 2, kernel_size=3, stride=1, padding=1),
                         ),
                     ),
                 ),
             ),
             # end of skip connection
-            torch.nn.MaxPool2d(kernel_size=3, stride=2),
-            torch.nn.Flatten(),
-            torch.nn.Linear(8, 5),
+            MaxPool2d(kernel_size=3, stride=2),
+            Flatten(),
+            Linear(8, 5),
         ),
-        "loss_function_fn": lambda: torch.nn.CrossEntropyLoss(),
+        "loss_function_fn": lambda: CrossEntropyLoss(),
         "target_fn": lambda: classification_targets((4,), 5),
         "id_prefix": "nested-branching-convolution",
     },
