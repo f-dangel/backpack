2600bd460a6467d682b85a21d664db6b3f7f20e7
Tim Schäfer <tim@gruber-schaefer.de>
2021-07-07 08:39:15 +0200

    refactor

--- backpack/custom_module/graph_utils.py
+++ backpack/custom_module/graph_utils.py
@@ -2,16 +2,14 @@
 from copy import deepcopy
 from typing import Set
 
-import torch
-import torch.fx
-from torch.fx import GraphModule
-from torch.nn import Flatten, Module, ReLU
+from torch.fx import Graph, GraphModule, Node, Tracer
+from torch.nn import Flatten, Module
 
 from backpack.custom_module.branching import ActiveIdentity, Branch, SumModule
 from backpack.custom_module.scale_module import ScaleModule
 
 
-class MyCustomTracer(torch.fx.Tracer):
+class BackPackTracer(Tracer):
     """This custom tracer recognizes BackPACK custom modules."""
 
     def is_leaf_module(self, m, module_qualified_name):  # noqa: D102
@@ -33,11 +31,11 @@ def print_table(module: Module) -> None:
     Args:
         module: module to analyze
     """
-    graph_resnet18 = MyCustomTracer().trace(module)
+    graph_resnet18 = BackPackTracer().trace(module)
     graph_resnet18.print_tabular()
 
 
-def convert_module_to_backpack(module: Module) -> Module:
+def convert_module_to_backpack(module: Module) -> GraphModule:
     """Converts all modules to backpack-compatible modules.
 
     Transformations:
@@ -51,17 +49,22 @@ def convert_module_to_backpack(module: Module) -> Module:
     Returns:
         backpack compatible module
     """
+    print("\nMake module BackPACK compatible...")
     module_new = _transform_mul_to_scale_module(module)
     module_new = _transform_flatten_to_module(module_new)
     module_new = _transform_add_to_sum_module(module_new)
     module_new = _transform_inplace_to_normal(module_new)
     module_new = _transform_remove_duplicates(module_new)
+    print("Delete unused modules.")
+    module_new.delete_all_unused_submodules()
+    print("Finished transformation.\n")
     return module_new
 
 
-def _transform_mul_to_scale_module(module: Module) -> Module:
-    print("\nBegin transformation...")
-    graph: torch.fx.Graph = MyCustomTracer().trace(module)
+def _transform_mul_to_scale_module(module: Module) -> GraphModule:
+    print("Begin transformation: <built-in function mul> -> ScaleModule")
+    counter: int = 0
+    graph: Graph = BackPackTracer().trace(module)
     for node in graph.nodes:
         if node.op == "call_function":
             if str(node.target) == "<built-in function mul>":
@@ -72,32 +75,34 @@ def _transform_mul_to_scale_module(module: Module) -> Module:
                     ScaleModule(node.args[1]),
                     (node.args[0],),
                 )
+            counter += 1
 
     graph.lint()
-    # TODO: delete_all_unused_submodules
-    print("End transformation.\n")
-    return torch.fx.GraphModule(module, graph)
+    print(f"Multiplications transformed: {counter}")
+    return GraphModule(module, graph)
 
 
-def _transform_add_to_sum_module(module: Module) -> Module:
-    print("\nBegin transformation...")
-    graph: torch.fx.Graph = MyCustomTracer().trace(module)
+def _transform_add_to_sum_module(module: Module) -> GraphModule:
+    print("Begin transformation: <built-in function add> -> SumModule")
+    counter: int = 0
+    graph: Graph = BackPackTracer().trace(module)
     for node in graph.nodes:
         if node.op == "call_function":
             if str(node.target) == "<built-in function add>":
                 _change_node_to_module(
                     node, "sum_module", module, SumModule(), node.args
                 )
+                counter += 1
 
     graph.lint()
-    # TODO: delete_all_unused_submodules
-    print("End transformation.\n")
-    return torch.fx.GraphModule(module, graph)
+    print(f"Summations transformed: {counter}")
+    return GraphModule(module, graph)
 
 
-def _transform_flatten_to_module(module: Module) -> Module:
-    print("\nBegin transformation...")
-    graph: torch.fx.Graph = MyCustomTracer().trace(module)
+def _transform_flatten_to_module(module: Module) -> GraphModule:
+    print("Begin transformation: <built-in method flatten> -> Flatten")
+    counter: int = 0
+    graph: Graph = BackPackTracer().trace(module)
     for node in graph.nodes:
         if node.op == "call_function":
             if "<built-in method flatten" in str(node.target):
@@ -110,25 +115,35 @@ def _transform_flatten_to_module(module: Module) -> Module:
                     Flatten(start_dim, end_dim),
                     (node.args[0],),
                 )
+                counter += 1
 
     graph.lint()
-    # TODO: delete_all_unused_submodules
-    print("End transformation.\n")
-    return torch.fx.GraphModule(module, graph)
+    print(f"Flatten transformed: {counter}")
+    return GraphModule(module, graph)
 
 
-def _transform_inplace_to_normal(module: Module) -> Module:
-    if isinstance(module, ReLU):
+def _transform_inplace_to_normal(
+    module: Module, initialize_recursion: bool = True
+) -> Module:
+    if initialize_recursion:
+        print("Begin transformation: in-place -> standard")
+        _transform_inplace_to_normal.counter = 0
+    if hasattr(module, "inplace") and module.inplace:
         module.inplace = False
+        _transform_inplace_to_normal.counter += 1
     for child_module in module.children():
-        _transform_inplace_to_normal(child_module)
+        _transform_inplace_to_normal(child_module, initialize_recursion=False)
 
+    if initialize_recursion:
+        print(f"In-place changed: {_transform_inplace_to_normal.counter}")
+        delattr(_transform_inplace_to_normal, "counter")
     return module
 
 
-def _transform_remove_duplicates(module: GraphModule, max_depth: int = 100) -> Module:
-    print("Removing duplicates...")
-    graph: torch.fx.Graph = MyCustomTracer().trace(module)
+def _transform_remove_duplicates(module: Module, max_depth: int = 100) -> GraphModule:
+    print("Begin transformation: remove duplicates")
+    counter = 0
+    graph: Graph = BackPackTracer().trace(module)
     targets: Set[str] = set()
     for node in graph.nodes:
         if node.target in targets:
@@ -147,16 +162,18 @@ def _transform_remove_duplicates(module: GraphModule, max_depth: int = 100) -> M
                 except AttributeError:
                     module.add_submodule(target, new_module)
                     node.target = target
+                    counter += 1
                     break
         else:
             targets.add(node.target)
 
     graph.lint()
-    return torch.fx.GraphModule(module, graph)
+    print(f"Duplicates removed: {counter}")
+    return GraphModule(module, graph)
 
 
 def _change_node_to_module(
-    node: torch.fx.node.Node,
+    node: Node,
     name: str,
     base_module: Module,
     new_module: Module,
