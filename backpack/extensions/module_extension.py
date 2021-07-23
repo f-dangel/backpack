1fa298eaa9b01f103644475aa6d2575db68adb0d
Tim Schäfer <tim@gruber-schaefer.de>
2021-07-23 10:18:11 +0200

    try fix github error

--- backpack/extensions/module_extension.py
+++ backpack/extensions/module_extension.py
@@ -157,7 +157,7 @@ class ModuleExtension:
                 )
             # TODO: discuss whether this can be avoided
             # occurs only in automated tests
-            except NotImplementedError:
+            except (NotImplementedError, RuntimeError):
                 setattr(inp, ext.savefield, attach)
         else:
             setattr(inp, ext.savefield, attach)
