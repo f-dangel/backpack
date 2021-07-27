6d12b065397b06b3eb6aab434643c1187406bbc0
Tim Schäfer <ti.schaefer@student.uni-tuebingen.de>
2021-07-27 14:04:52 +0200

    [ADD] option fail_mode in FirsOrderBackpropExtension to enable custom choice

--- backpack/extensions/firstorder/base.py
+++ backpack/extensions/firstorder/base.py
@@ -22,7 +22,7 @@ class FirstOrderBackpropExtension(BackpropExtension):
         savefield: str,
         module_exts: Dict[Type[Module], ModuleExtension],
         fail_mode: str = FAIL_WARN,
-    ):
+    ):  # noqa: D107
         super().__init__(savefield, module_exts, fail_mode=fail_mode)
 
     def expects_backpropagation_quantities(self) -> bool:  # noqa: D102
