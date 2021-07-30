790a054a9814b00777dbd51b2ae54561b94fad24
Felix Dangel <fdangel@tuebingen.mpg.de>
2021-07-30 16:47:56 +0200

    [DEL] remove no_op logic in Flatten module extensions

--- backpack/extensions/secondorder/diag_ggn/flatten.py
+++ backpack/extensions/secondorder/diag_ggn/flatten.py
@@ -5,9 +5,3 @@ from backpack.extensions.secondorder.diag_ggn.diag_ggn_base import DiagGGNBaseMo
 class DiagGGNFlatten(DiagGGNBaseModule):
     def __init__(self):
         super().__init__(derivatives=FlattenDerivatives())
-
-    def backpropagate(self, ext, module, grad_inp, grad_out, backproped):
-        if self.derivatives.is_no_op(module):
-            return backproped
-        else:
-            return super().backpropagate(ext, module, grad_inp, grad_out, backproped)
