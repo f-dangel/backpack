775623570bf84337bad43101a71b9a2e725216e2
Tim Schäfer <tim@gruber-schaefer.de>
2021-09-01 08:35:15 +0200

    [ADD] reintroduce example_first_order_resnet.py and link to new location

---
+++ docs_src/examples/use_cases/example_first_order_resnet.py
@@ -0,0 +1,8 @@
+r"""First order extensions with a ResNet
+========================================
+"""
+
+# %%
+# This tutorial has moved. Click
+# [here](https://docs.backpack.pt/en/master/use_cases/example_resnet_all_in_one.html)
+# to continue to its new location.
