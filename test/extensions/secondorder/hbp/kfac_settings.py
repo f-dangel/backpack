d20e3d4ffe0af29cfd1b026c63b82a2e196e4885
Jessica Bader <jabader@iastate.edu>
2021-11-23 13:59:40 +0100

    Fixed formatting

--- test/extensions/secondorder/hbp/kfac_settings.py
+++ test/extensions/secondorder/hbp/kfac_settings.py
@@ -1,12 +1,13 @@
 """Define test cases for KFAC."""
 
+from test.core.derivatives.utils import regression_targets
 from test.extensions.secondorder.secondorder_settings import (
     GROUP_CONV_SETTINGS,
     LINEAR_ADDITIONAL_DIMENSIONS_SETTINGS,
 )
-from test.core.derivatives.utils import regression_targets
-from torch.nn import (Sequential, Linear, Flatten, MSELoss, ReLU)
+
 from torch import rand
+from torch.nn import Flatten, Linear, MSELoss, ReLU, Sequential
 
 SHARED_NOT_SUPPORTED_SETTINGS = (
     GROUP_CONV_SETTINGS + LINEAR_ADDITIONAL_DIMENSIONS_SETTINGS
@@ -18,9 +19,12 @@ NOT_SUPPORTED_SETTINGS = SHARED_NOT_SUPPORTED_SETTINGS + LOCAL_NOT_SUPPORTED_SET
 BATCH_SIZE_1_SETTINGS = [
     {
         "input_fn": lambda: rand(1, 7),
-        "module_fn": lambda: Sequential(Linear(7, 3), ReLU(), Flatten(start_dim=1, end_dim=-1), Linear(3, 1)),
+        "module_fn": lambda: Sequential(
+            Linear(7, 3), ReLU(), Flatten(start_dim=1, end_dim=-1), Linear(3, 1)
+        ),
         "loss_function_fn": lambda: MSELoss(reduction="mean"),
         "target_fn": lambda: regression_targets((1, 1)),
         "id_prefix": "one-additional",
     }
 ]
+
--- test/extensions/secondorder/hbp/test_kfac.py
+++ test/extensions/secondorder/hbp/test_kfac.py
@@ -1,15 +1,17 @@
 """Test BackPACK's KFAC extension."""
-import torch
-
+from test.automated_test import check_sizes_and_values
 from test.extensions.implementation.backpack import BackpackExtensions
 from test.extensions.problem import make_test_problems
-from test.extensions.secondorder.hbp.kfac_settings import NOT_SUPPORTED_SETTINGS
-from test.automated_test import check_sizes_and_values
-from test.extensions.secondorder.hbp.kfac_settings import BATCH_SIZE_1_SETTINGS
-from backpack.utils.kroneckers import kfacs_to_mat
-from backpack.hessianfree.ggnvp import ggn_vector_product_from_plist
+from test.extensions.secondorder.hbp.kfac_settings import (
+    BATCH_SIZE_1_SETTINGS,
+    NOT_SUPPORTED_SETTINGS,
+)
 
 import pytest
+import torch
+
+from backpack.hessianfree.ggnvp import ggn_vector_product_from_plist
+from backpack.utils.kroneckers import kfacs_to_mat
 
 NOT_SUPPORTED_PROBLEMS = make_test_problems(NOT_SUPPORTED_SETTINGS)
 NOT_SUPPORTED_IDS = [problem.make_id() for problem in NOT_SUPPORTED_PROBLEMS]
@@ -34,7 +36,7 @@ def test_kfac_not_supported(problem):
 
 @pytest.mark.parametrize("problem", BATCH_SIZE_1_PROBLEMS, ids=BATCH_SIZE_1_IDS)
 def test_kfac_should_approx_ggn_montecarlo(problem):
-    """Check that for batch_size = 1, the K-FAC is the same as the GGN
+    """Check that for batch_size = 1, the K-FAC is the same as the GGN.
 
     Args:
         problem (ExtensionsTestProblem): Test case.
@@ -42,13 +44,11 @@ def test_kfac_should_approx_ggn_montecarlo(problem):
     problem.set_up()
     torch.manual_seed(0)
     # calculate GGN
-    # create ID matrix for each layer
     mat_list = []
     for p in problem.model.parameters():
         mat_list.append(
             torch.eye(p.numel(), device=p.device).reshape(p.numel(), *p.shape)
         )
-    # get output and loss
     outputs = problem.model(problem.input)
     loss = problem.loss_function(outputs, problem.target)
     autograd_res = []
@@ -58,7 +58,9 @@ def test_kfac_should_approx_ggn_montecarlo(problem):
             e_d = mat[i, :]
             GGN_col_i = ggn_vector_product_from_plist(loss, outputs, [layer], e_d)[0]
             ggn_cols.append(GGN_col_i.unsqueeze(0))
-        autograd_res.append(torch.cat(ggn_cols, dim=0).reshape(layer.numel(), layer.numel()))
+        autograd_res.append(
+            torch.cat(ggn_cols, dim=0).reshape(layer.numel(), layer.numel())
+        )
 
     # calculate backpack average
     mc_samples = 200
@@ -71,3 +73,4 @@ def test_kfac_should_approx_ggn_montecarlo(problem):
     check_sizes_and_values(autograd_res, backpack_average_res, atol=1e-1, rtol=1e-1)
 
     problem.tear_down()
+
