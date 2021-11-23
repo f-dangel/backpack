7660589bd3455a0b0fb994312932deb94f878962
Jessica Bader <jabader@iastate.edu>
2021-11-23 11:31:53 +0100

    IP: halfway through first issue

--- test/extensions/secondorder/hbp/kfac_settings.py
+++ test/extensions/secondorder/hbp/kfac_settings.py
@@ -11,3 +11,19 @@ SHARED_NOT_SUPPORTED_SETTINGS = (
 LOCAL_NOT_SUPPORTED_SETTINGS = []
 
 NOT_SUPPORTED_SETTINGS = SHARED_NOT_SUPPORTED_SETTINGS + LOCAL_NOT_SUPPORTED_SETTINGS
+
+# ------------------------------------------------
+from test.core.derivatives.utils import regression_targets
+from torch.nn import (Sequential, Linear, Flatten, MSELoss, ReLU)
+from torch import rand
+BATCH_SIZE_1_SETTINGS = [
+    {
+        "input_fn": lambda: rand(1, 7),
+        "module_fn": lambda: Sequential(Linear(7, 3), ReLU(), Flatten(start_dim=1, end_dim=-1), Linear(3, 1)),
+        "loss_function_fn": lambda: MSELoss(reduction="mean"),
+        "target_fn": lambda: regression_targets((1, 1)),
+        "id_prefix": "one-additional",
+    }
+]
+
+
--- test/extensions/secondorder/hbp/test_kfac.py
+++ test/extensions/secondorder/hbp/test_kfac.py
@@ -1,4 +1,5 @@
 """Test BackPACK's KFAC extension."""
+import torch
 
 from test.extensions.implementation.backpack import BackpackExtensions
 from test.extensions.problem import make_test_problems
@@ -23,3 +24,65 @@ def test_kfac_not_supported(problem):
         BackpackExtensions(problem).kfac()
 
     problem.tear_down()
+
+
+# ------------------------------------------------------------
+from test.automated_test import check_sizes_and_values
+from test.extensions.secondorder.hbp.kfac_settings import BATCH_SIZE_1_SETTINGS
+from backpack.utils.kroneckers import kfacs_to_mat
+BATCH_SIZE_1_PROBLEMS = make_test_problems(BATCH_SIZE_1_SETTINGS)
+BATCH_SIZE_1_IDS = [problem.make_id() for problem in BATCH_SIZE_1_PROBLEMS]
+from backpack.hessianfree.ggnvp import ggn_vector_product, ggn_vector_product_from_plist
+from backpack.utils.convert_parameters import vector_to_parameter_list
+from backpack.utils.examples import _autograd_ggn_exact_columns
+
+@pytest.mark.parametrize("problem", BATCH_SIZE_1_PROBLEMS, ids=BATCH_SIZE_1_IDS)
+def test_kfac_should_approx_ggn_montecarlo(problem):
+    """Check that for batch_size = 1, the K-FAC is the same as the GGN
+
+    Args:
+        problem (ExtensionsTestProblem): Test case.
+    """
+    problem.set_up()
+    torch.manual_seed(0)
+    # calculate GGN
+    # todo remove this and calculate ggn
+    # ============================
+    autograd_res = 0  # todo remove this
+    # create ID matrix for each layer
+    mat_list = []
+    for p in problem.model.parameters():
+        mat_list.append(
+            torch.eye(p.numel(), device=p.device).reshape(p.numel(), *p.shape)
+        )
+    # get output and loss
+    outputs = problem.model(problem.input)
+    loss = problem.loss_function(outputs, problem.target)
+    autograd_res = []
+    for layer, mat in zip(problem.model.parameters(), mat_list):
+        ggn_cols = []
+        for i in range(len(mat[0])):
+            e_d = mat[i, :]
+            GGN_col_i = ggn_vector_product_from_plist(loss, outputs, [layer], e_d)[0]
+            ggn_cols.append(GGN_col_i.unsqueeze(0))
+        autograd_res.append(ggn_cols)
+    # ============================
+
+    # calculate backpack average
+    mc_samples = 200
+    backpack_average_res = []
+    backpack_kfac = BackpackExtensions(problem).kfac(mc_samples)
+    for bpr in backpack_kfac:
+        backpack_average_res.append(kfacs_to_mat(bpr))
+
+    # check the values
+    check_sizes_and_values(autograd_res, backpack_average_res, atol=1e-1, rtol=1e-1)
+
+    problem.tear_down()
+
+
+# ==========================================
+test_kfac_should_approx_ggn_montecarlo(BATCH_SIZE_1_PROBLEMS[0])
+# ==========================================
+
+
