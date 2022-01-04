7492d28ef1e6eda97be5321b9489dc66227ea1f2
Felix Dangel <fdangel@tue.mpg.de>
2022-01-04 09:44:15 -0300

    [REF] Share `Pad` test cases between `DiagH`, `DiagGGN` and `SqrtGGN`

--- test/extensions/secondorder/diag_ggn/diag_ggn_settings.py
+++ test/extensions/secondorder/diag_ggn/diag_ggn_settings.py
@@ -40,7 +40,6 @@ from torch.nn import (
 from backpack import convert_module_to_backpack
 from backpack.custom_module import branching
 from backpack.custom_module.branching import Parallel
-from backpack.custom_module.pad import Pad
 from backpack.custom_module.permute import Permute
 from backpack.custom_module.reduce_tuple import ReduceTuple
 
@@ -281,37 +280,5 @@ LOCAL_SETTINGS += [
     },
 ]
 
-
-###############################################################################
-#                                Custom Pad mod                               #
-###############################################################################
-LOCAL_SETTINGS += [
-    {
-        "input_fn": lambda: rand(5, 3, 4, 4),
-        "module_fn": lambda: Sequential(
-            Conv2d(in_channels=3, out_channels=2, kernel_size=2),
-            Pad((1, 1, 0, 2), mode="constant", value=0.0),
-            Conv2d(in_channels=2, out_channels=2, kernel_size=3, stride=2),
-            Flatten(),
-            Linear(8, 2),
-        ),
-        "loss_function_fn": lambda: MSELoss(reduction="mean"),
-        "target_fn": lambda: regression_targets((5, 2)),
-    },
-    {
-        "input_fn": lambda: rand(5, 3, 4, 4),
-        "module_fn": lambda: Sequential(
-            Conv2d(in_channels=3, out_channels=2, kernel_size=3, padding=1),
-            ReLU(),
-            Pad((1, 1, 2, 0, 1, 2), mode="constant", value=1.0),
-            Conv2d(in_channels=5, out_channels=2, kernel_size=3, stride=2),
-            ReLU(),
-            Flatten(),
-            Linear(8, 3),
-        ),
-        "loss_function_fn": lambda: CrossEntropyLoss(reduction="mean"),
-        "target_fn": lambda: classification_targets((5,), 3),
-    },
-]
 
 DiagGGN_SETTINGS = SHARED_SETTINGS + LOCAL_SETTINGS
--- test/extensions/secondorder/diag_hessian/diagh_settings.py
+++ test/extensions/secondorder/diag_hessian/diagh_settings.py
@@ -4,23 +4,10 @@ The tests are taken from `test.extensions.secondorder.secondorder_settings`,
 but additional custom tests can be defined here by appending it to the list.
 """
 
-from test.core.derivatives.utils import classification_targets, regression_targets
 from test.extensions.automated_settings import make_simple_act_setting
 from test.extensions.secondorder.secondorder_settings import SECONDORDER_SETTINGS
 
-from torch import rand
-from torch.nn import (
-    Conv2d,
-    CrossEntropyLoss,
-    Flatten,
-    Linear,
-    LogSigmoid,
-    MSELoss,
-    ReLU,
-    Sequential,
-)
-
-from backpack.custom_module.pad import Pad
+from torch.nn import LogSigmoid
 
 DiagHESSIAN_SETTINGS = []
 
@@ -28,37 +15,5 @@ SHARED_SETTINGS = SECONDORDER_SETTINGS
 LOCAL_SETTINGS = []
 LOCAL_SETTINGS.append(make_simple_act_setting(LogSigmoid, bias=True))
 LOCAL_SETTINGS.append(make_simple_act_setting(LogSigmoid, bias=False))
-
-###############################################################################
-#                                Custom Pad mod                               #
-###############################################################################
-LOCAL_SETTINGS += [
-    {
-        "input_fn": lambda: rand(5, 3, 4, 4),
-        "module_fn": lambda: Sequential(
-            Conv2d(in_channels=3, out_channels=2, kernel_size=2),
-            Pad((1, 1, 0, 2), mode="constant", value=0.0),
-            Conv2d(in_channels=2, out_channels=2, kernel_size=3, stride=2),
-            Flatten(),
-            Linear(8, 2),
-        ),
-        "loss_function_fn": lambda: MSELoss(reduction="mean"),
-        "target_fn": lambda: regression_targets((5, 2)),
-    },
-    {
-        "input_fn": lambda: rand(5, 3, 4, 4),
-        "module_fn": lambda: Sequential(
-            Conv2d(in_channels=3, out_channels=2, kernel_size=3, padding=1),
-            ReLU(),
-            Pad((1, 1, 2, 0, 1, 2), mode="constant", value=1.0),
-            Conv2d(in_channels=5, out_channels=2, kernel_size=3, stride=2),
-            ReLU(),
-            Flatten(),
-            Linear(8, 3),
-        ),
-        "loss_function_fn": lambda: CrossEntropyLoss(reduction="mean"),
-        "target_fn": lambda: classification_targets((5,), 3),
-    },
-]
 
 DiagHESSIAN_SETTINGS = SHARED_SETTINGS + LOCAL_SETTINGS
