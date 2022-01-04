a84e10f5436dd3608a4911007689d33b93d031e5
Jessica Bader <jabader@iastate.edu>
2022-01-04 10:20:57 +0100

    Updated docstrings to fix doc error

--- backpack/extensions/secondorder/hbp/custom_module.py
+++ backpack/extensions/secondorder/hbp/custom_module.py
@@ -1,3 +1,4 @@
+"""Module extensions for custom properties of HBPBaseModule."""
 from backpack.core.derivatives.sum_module import SumModuleDerivatives
 from backpack.extensions.secondorder.hbp.hbpbase import HBPBaseModule
 
