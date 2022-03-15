b79f59248bfe2f0d0912c4eb8915f6c27c90ad17
jabader97 <51213988+jabader97@users.noreply.github.com>
2022-03-15 19:01:40 +0100

    Typo fix

--- backpack/core/derivatives/mseloss.py
+++ backpack/core/derivatives/mseloss.py
@@ -11,7 +11,7 @@ from backpack.core.derivatives.nll_base import NLLLossDerivatives
 
 
 class MSELossDerivatives(NLLLossDerivatives, ABC):
-    """Partial derivatives for mean square erro loss.
+    """Partial derivatives for mean square error loss.
 
     This comes from the Gaussian distribution.
     """
