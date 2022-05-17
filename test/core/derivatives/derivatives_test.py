e9221ef347677a0594054fad60379a50265f15c7
jabader97 <51213988+jabader97@users.noreply.github.com>
2022-05-17 14:32:46 +0200

    [REF] Changed cross entropy loss to NLL base

--- test/core/derivatives/derivatives_test.py
+++ test/core/derivatives/derivatives_test.py
@@ -73,8 +73,8 @@ CUSTOM_SLICING_MODULE_IDS = [
     problem.make_id() for problem in CUSTOM_SLICING_MODULE_PROBLEMS
 ]
 
-MSE_PROBLEMS = [problem for problem in PROBLEMS if problem.is_mse()]
-MSE_IDS = [problem.make_id() for problem in MSE_PROBLEMS]
+NLL_PROBLEMS = [problem for problem in PROBLEMS if problem.is_nll()]
+NLL_IDS = [problem.make_id() for problem in NLL_PROBLEMS]
 
 SUBSAMPLINGS = [None, [0, 0], [2, 0]]
 SUBSAMPLING_IDS = [f"subsampling={s}".replace(" ", "") for s in SUBSAMPLINGS]
@@ -348,7 +348,7 @@ def test_sqrt_hessian_sampled_squared_approximates_hessian(
 
 
 @mark.parametrize("subsampling", SUBSAMPLINGS, ids=SUBSAMPLING_IDS)
-@mark.parametrize("problem", MSE_PROBLEMS, ids=MSE_IDS)
+@mark.parametrize("problem", NLL_PROBLEMS, ids=NLL_IDS)
 def test_sqrt_hessian_sampled_squared_approximates_hessian_nll(
     problem: DerivativesTestProblem,
     subsampling: Union[List[int], None],
