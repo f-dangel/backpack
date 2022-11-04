19b65cde44e0507bd3e570fb4e1511ec417220df
Felix Dangel <48687646+f-dangel@users.noreply.github.com>
2022-11-04 17:13:18 +0100

    [ADD] Test support for `BCEWithLogitsLoss` in `KFLR` (#284)
    * [ADD] Implement sqrt_hessian for BCEWithLogitsLoss
    
    * [TEST] Add case for sqrt_hessian with non-binary labels
    
    * [DOC] Improve derivation of Hessian square root
    
    * [ADD] Implement manual sampled gradients for `BCEWithLogitsLoss`
    
    * [CI] Add to fully documented files
    
    * [DOC] Fix pydocstyle
    
    * [ADD] `DiagHessian` support for `BCEWithLogitsLoss`
    
    * [CI] Skip `BCEWithLogitsLoss` cases in DiagGGNExactBatch
    
    * [CI] Skip `BCEWithLogitsLoss` for `DiagGGNExact`
    
    * [CI] Skip `BCEWithLogitsLoss` in `SqrtGGN` tests
    
    * [REF] Use `BCEWithLogitsLoss` test cases for Hessian diagonal
    
    * [DEL] Remove skip utilities for `BCEWithLogitsLoss`
    
    * [ADD] Support `BCEWithLogitsLoss` in `(Diag)GGN{Exact,MC}`
    
    * [ADD] Support `BCEWithLogitsLoss` in `SqrtGGN{Exact, MC}` extension
    
    * [DEL] Forgot to extract BCEWithLogitsLoss test cases
    
    * [ADD] Support `BCEWithLogitsLoss` in `KFAC`
    
    * [TEST] Add KFLR test, split KFAC settings to recycle in KFLR tests
    
    Co-authored-by: Felix Dangel <fdangel@tue.mpg.de>

--- test/extensions/secondorder/hbp/kflr_settings.py
+++ test/extensions/secondorder/hbp/kflr_settings.py
@@ -1,5 +1,8 @@
 """Define test cases for KFLR."""
 
+from test.extensions.secondorder.hbp.kfac_settings import (
+    _BATCH_SIZE_1_NO_BRANCHING_SETTINGS,
+)
 from test.extensions.secondorder.secondorder_settings import (
     GROUP_CONV_SETTINGS,
     LINEAR_ADDITIONAL_DIMENSIONS_SETTINGS,
@@ -11,3 +14,5 @@ SHARED_NOT_SUPPORTED_SETTINGS = (
 LOCAL_NOT_SUPPORTED_SETTINGS = []
 
 NOT_SUPPORTED_SETTINGS = SHARED_NOT_SUPPORTED_SETTINGS + LOCAL_NOT_SUPPORTED_SETTINGS
+
+BATCH_SIZE_1_SETTINGS = _BATCH_SIZE_1_NO_BRANCHING_SETTINGS
--- test/extensions/secondorder/hbp/test_kflr.py
+++ test/extensions/secondorder/hbp/test_kflr.py
@@ -1,13 +1,22 @@
 """Test BackPACK's KFLR extension."""
 
+from test.automated_test import check_sizes_and_values
+from test.extensions.implementation.autograd import AutogradExtensions
 from test.extensions.implementation.backpack import BackpackExtensions
-from test.extensions.problem import make_test_problems
-from test.extensions.secondorder.hbp.kflr_settings import NOT_SUPPORTED_SETTINGS
+from test.extensions.problem import ExtensionsTestProblem, make_test_problems
+from test.extensions.secondorder.hbp.kflr_settings import (
+    BATCH_SIZE_1_SETTINGS,
+    NOT_SUPPORTED_SETTINGS,
+)
 
 import pytest
 
+from backpack.utils.kroneckers import kfacs_to_mat
+
 NOT_SUPPORTED_PROBLEMS = make_test_problems(NOT_SUPPORTED_SETTINGS)
 NOT_SUPPORTED_IDS = [problem.make_id() for problem in NOT_SUPPORTED_PROBLEMS]
+BATCH_SIZE_1_PROBLEMS = make_test_problems(BATCH_SIZE_1_SETTINGS)
+BATCH_SIZE_1_IDS = [problem.make_id() for problem in BATCH_SIZE_1_PROBLEMS]
 
 
 @pytest.mark.parametrize("problem", NOT_SUPPORTED_PROBLEMS, ids=NOT_SUPPORTED_IDS)
@@ -21,5 +30,23 @@ def test_kflr_not_supported(problem):
 
     with pytest.raises(RuntimeError):
         BackpackExtensions(problem).kflr()
+
+    problem.tear_down()
+
+
+@pytest.mark.parametrize("problem", BATCH_SIZE_1_PROBLEMS, ids=BATCH_SIZE_1_IDS)
+def test_kflr_equals_ggn(problem: ExtensionsTestProblem):
+    """Check that for batch_size = 1 and linear layers, KFLR is the GGN block.
+
+    Args:
+        problem: Test case.
+    """
+    problem.set_up()
+    autograd_res = AutogradExtensions(problem).ggn_blocks()
+
+    backpack_kflr = BackpackExtensions(problem).kflr()
+    backpack_res = [kfacs_to_mat(kflr) for kflr in backpack_kflr]
+
+    check_sizes_and_values(autograd_res, backpack_res, atol=1e-7, rtol=1e-5)
 
     problem.tear_down()
