4a84b496d046b356b14764842ec4a545c5ae0909
Felix Dangel <fdangel@tue.mpg.de>
2022-11-04 14:27:42 +0100

    [TEST] Add KFLR test, split KFAC settings to recycle in KFLR tests

--- test/extensions/secondorder/hbp/kfac_settings.py
+++ test/extensions/secondorder/hbp/kfac_settings.py
@@ -28,13 +28,36 @@ LOCAL_NOT_SUPPORTED_SETTINGS = []
 
 NOT_SUPPORTED_SETTINGS = SHARED_NOT_SUPPORTED_SETTINGS + LOCAL_NOT_SUPPORTED_SETTINGS
 
-BATCH_SIZE_1_SETTINGS = [
+_BATCH_SIZE_1_NO_BRANCHING_SETTINGS = [
     {
         "input_fn": lambda: rand(1, 7),
         "module_fn": lambda: Sequential(Linear(7, 3), ReLU(), Linear(3, 1)),
         "loss_function_fn": lambda: MSELoss(reduction="mean"),
         "target_fn": lambda: regression_targets((1, 1)),
     },
+    {
+        "input_fn": lambda: rand(1, 5),
+        "module_fn": lambda: Sequential(Linear(5, 4), Sigmoid(), Linear(4, 3)),
+        "loss_function_fn": lambda: CrossEntropyLoss(reduction="mean"),
+        "target_fn": lambda: classification_targets((1,), 3),
+    },
+    {
+        "input_fn": lambda: rand(1, 6),
+        "module_fn": lambda: Sequential(Linear(6, 4), ReLU(), Linear(4, 1)),
+        "loss_function_fn": lambda: BCEWithLogitsLoss(reduction="mean"),
+        "target_fn": lambda: rand(1, 1),
+        "id_prefix": "non-binary-labels",
+    },
+    {
+        "input_fn": lambda: rand(1, 6),
+        "module_fn": lambda: Sequential(Linear(6, 4), ReLU(), Linear(4, 1)),
+        "loss_function_fn": lambda: BCEWithLogitsLoss(reduction="sum"),
+        "target_fn": lambda: classification_targets(size=(1, 1), num_classes=2).float(),
+        "id_prefix": "binary-labels",
+    },
+]
+
+_BATCH_SIZE_1_BRANCHING_SETTINGS = [
     {
         "input_fn": lambda: rand(1, 10),
         "module_fn": lambda: Sequential(
@@ -71,18 +94,8 @@ BATCH_SIZE_1_SETTINGS = [
         "target_fn": lambda: classification_targets((1,), 4),
         "id_prefix": "branching-scalar",
     },
-    {
-        "input_fn": lambda: rand(1, 6),
-        "module_fn": lambda: Sequential(Linear(6, 4), ReLU(), Linear(4, 1)),
-        "loss_function_fn": lambda: BCEWithLogitsLoss(reduction="mean"),
-        "target_fn": lambda: rand(1, 1),
-        "id_prefix": "non-binary-labels",
-    },
-    {
-        "input_fn": lambda: rand(1, 6),
-        "module_fn": lambda: Sequential(Linear(6, 4), ReLU(), Linear(4, 1)),
-        "loss_function_fn": lambda: BCEWithLogitsLoss(reduction="sum"),
-        "target_fn": lambda: classification_targets(size=(1, 1), num_classes=2).float(),
-        "id_prefix": "binary-labels",
-    },
 ]
+
+BATCH_SIZE_1_SETTINGS = (
+    _BATCH_SIZE_1_NO_BRANCHING_SETTINGS + _BATCH_SIZE_1_BRANCHING_SETTINGS
+)
