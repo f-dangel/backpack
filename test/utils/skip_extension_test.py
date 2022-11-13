22cd70d27a456edaabe8e7d772cd9c3116a95ce5
Felix Dangel <fdangel@tue.mpg.de>
2022-11-13 12:56:23 +0100

    [TEST] Add integration test for KFRA

--- test/utils/skip_extension_test.py
+++ test/utils/skip_extension_test.py
@@ -16,3 +16,13 @@ def skip_BCEWithLogitsLoss_non_binary_labels(problem: ExtensionsTestProblem) -> 
         y not in [0, 1] for y in problem.target.flatten()
     ):
         skip("Skipping BCEWithLogitsLoss with non-binary labels")
+
+
+def skip_BCEWithLogitsLoss(problem: ExtensionsTestProblem) -> None:
+    """Skip if case uses BCEWithLogitsLoss as loss function.
+
+    Args:
+        problem: Extension test case.
+    """
+    if isinstance(problem.loss_function, BCEWithLogitsLoss):
+        skip("Skipping BCEWithLogitsLoss")
