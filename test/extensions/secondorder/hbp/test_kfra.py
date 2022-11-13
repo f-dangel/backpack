5e0fb3521790c179dc41fb4528360497620e59b4
Felix Dangel <48687646+f-dangel@users.noreply.github.com>
2022-11-13 13:33:51 +0100

    [DOC] Fully document `HBPConv2d` (#290)
    Add documentation to the `HBP` extension of `Conv2d` layers, which is 
    responsible to compute `KFAC/KFLR/KFRA`. The docstrings draw connections
    to the notation in the [KFC paper](https://arxiv.org/pdf/1602.01407.pdf), and 
    outline important differences, as well as improvements for consistency. 
    Also add a test case for `KFAC, KFLR` for which both approximations become exact.
    
    Note to myself: I made notes how to connect Hessian backpropagation to `KFAC` 
    for convolutions by imposing a Kronecker structure on the backpropagated quantity.
    This concept can also be applied to `KFRA` to achieve more consistency, but is 
    currently not done by the code.
    
    * [DOC] Fully document `HBPConv2d`
    
    * [TEST] KFAC/KFLR for convolution with single output
    
    Convolution layers with a single output behave like linear layers, as
    the weights are not shared over the input.
    
    * [DOC] Polish docstrings
    
    * [TEST] Add integration test for KFRA

--- test/extensions/secondorder/hbp/test_kfra.py
+++ test/extensions/secondorder/hbp/test_kfra.py
@@ -1,21 +1,28 @@
 """Test BackPACK's KFRA extension."""
 
 from test.extensions.implementation.backpack import BackpackExtensions
-from test.extensions.problem import make_test_problems
-from test.extensions.secondorder.hbp.kfra_settings import NOT_SUPPORTED_SETTINGS
+from test.extensions.problem import ExtensionsTestProblem, make_test_problems
+from test.extensions.secondorder.hbp.kfra_settings import (
+    BATCH_SIZE_1_SETTINGS,
+    NOT_SUPPORTED_SETTINGS,
+)
+from test.utils.skip_extension_test import skip_BCEWithLogitsLoss
 
 import pytest
+from torch import Tensor, prod
 
 NOT_SUPPORTED_PROBLEMS = make_test_problems(NOT_SUPPORTED_SETTINGS)
 NOT_SUPPORTED_IDS = [problem.make_id() for problem in NOT_SUPPORTED_PROBLEMS]
+BATCH_SIZE_1_PROBLEMS = make_test_problems(BATCH_SIZE_1_SETTINGS)
+BATCH_SIZE_1_IDS = [problem.make_id() for problem in BATCH_SIZE_1_PROBLEMS]
 
 
 @pytest.mark.parametrize("problem", NOT_SUPPORTED_PROBLEMS, ids=NOT_SUPPORTED_IDS)
-def test_kfra_not_supported(problem):
+def test_kfra_not_supported(problem: ExtensionsTestProblem):
     """Check that the KFRA extension does not allow specific hyperparameters/modules.
 
     Args:
-        problem (ExtensionsTestProblem): Test case.
+        problem: Test case.
     """
     problem.set_up()
 
@@ -23,3 +30,26 @@ def test_kfra_not_supported(problem):
         BackpackExtensions(problem).kfra()
 
     problem.tear_down()
+
+
+@pytest.mark.parametrize("problem", BATCH_SIZE_1_PROBLEMS, ids=BATCH_SIZE_1_IDS)
+def test_kfra_dimensions(problem: ExtensionsTestProblem):
+    """Check that block Hessian approximation of KFRA has correct dimension.
+
+    This test runs KFRA code, but due to the approximations made in KFRA, a case
+    where it becomes exact and can therefore be tested for correct values still
+    needs to be identified.
+
+    Args:
+        problem: Test case.
+    """
+    problem.set_up()
+    skip_BCEWithLogitsLoss(problem)
+
+    backpack_kfra = BackpackExtensions(problem).kfra()
+    for p, p_kfra in zip(problem.trainable_parameters(), backpack_kfra):
+        assert all(kron.dim() == 2 for kron in p_kfra)
+        assert all(kron.shape[0] == kron.shape[1] for kron in p_kfra)
+
+        kron_dims = Tensor([kron_fac.shape[0] for kron_fac in p_kfra])
+        assert p.numel() == prod(kron_dims)
