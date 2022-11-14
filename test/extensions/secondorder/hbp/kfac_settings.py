ba0f78e1b02168ea298381defc4c03df6738e589
Felix Dangel <fdangel@tue.mpg.de>
2022-11-14 11:11:22 +0100

    [TEST] Add cases for `ConvTranspose{1,3}d`

--- test/extensions/secondorder/hbp/kfac_settings.py
+++ test/extensions/secondorder/hbp/kfac_settings.py
@@ -95,6 +95,20 @@ _BATCH_SIZE_1_NO_BRANCHING_SETTINGS = [
     # transpose convolution with single output is a linear layer (no weight
     # sharing across input)
     {
+        "input_fn": lambda: rand(1, 2, 9),
+        "module_fn": lambda: Sequential(
+            ConvTranspose1d(2, 4, 3, padding=5),
+            Sigmoid(),
+            ConvTranspose1d(4, 1, 1),
+            Flatten(),
+        ),
+        "loss_function_fn": lambda: MSELoss(reduction="mean"),
+        "target_fn": lambda: regression_targets((1, 1)),
+        "id_prefix": "convtranspose1d-single-output",
+    },
+    # transpose convolution with single output is a linear layer (no weight
+    # sharing across input)
+    {
         "input_fn": lambda: rand(1, 2, 4, 4),
         "module_fn": lambda: Sequential(
             ConvTranspose2d(2, 3, 2, padding=2),
@@ -104,7 +118,21 @@ _BATCH_SIZE_1_NO_BRANCHING_SETTINGS = [
         ),
         "loss_function_fn": lambda: MSELoss(reduction="mean"),
         "target_fn": lambda: regression_targets((1, 1)),
-        "id_prefix": "convtranspose1d-single-output",
+        "id_prefix": "convtranspose2d-single-output",
+    },
+    # transpose convolution with single output is a linear layer (no weight
+    # sharing across input)
+    {
+        "input_fn": lambda: rand(1, 2, 6, 6, 6),
+        "module_fn": lambda: Sequential(
+            ConvTranspose3d(2, 4, 2, padding=3),
+            Sigmoid(),
+            ConvTranspose3d(4, 1, 1),
+            Flatten(),
+        ),
+        "loss_function_fn": lambda: MSELoss(reduction="mean"),
+        "target_fn": lambda: regression_targets((1, 1)),
+        "id_prefix": "convtranspose3d-single-output",
     },
 ]
 
